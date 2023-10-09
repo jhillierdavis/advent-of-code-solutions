61b97ebe9c90d39011f11e447d70da4b12598d67
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-10-09 20:06:50 +0100

    AOC 2021 Day 12: Initial crude solution for parts 1 and 2

--- aoc-2021/aoc-2021-day-12/solution-in-python3/solution.py
+++ aoc-2021/aoc-2021-day-12/solution-in-python3/solution.py
@@ -1,6 +1,9 @@
 from helpers import fileutils
 
+
 from collections import defaultdict
+
+import copy
 
 def list_copy(orig:list) -> list:
     clone = list()
@@ -9,6 +12,7 @@ def list_copy(orig:list) -> list:
     return clone
 
 paths = set()
+
 def get_path(map, parent, path:list):
     path.append(parent)
     
@@ -18,31 +22,80 @@ def get_path(map, parent, path:list):
             clone = list_copy(path)
             clone.append(child)
             paths.add(str(clone))
-            continue
+            #continue
         elif child.isupper() or child not in path:
             get_path(map, child, list_copy(path))
     return None
 
-
-def count_paths(filename):
+def get_part2_path(map, parent, path:list, small_cave_revist:bool=True):
+    path.append(parent)
     
+    children = map[parent]
+    for child in children:
+        if child == 'end':
+            clone = list_copy(path)
+            clone.append(child)
+            paths.add(str(clone))
+            #continue
+        elif child.isupper():
+            get_part2_path(map, child, list_copy(path), small_cave_revist)
+        elif child not in path:
+            get_part2_path(map, child, list_copy(path), small_cave_revist)
+        elif small_cave_revist == True and 'start' != child and 'end' != child:
+            small_cave_revist = False
+            get_part2_path(map, child, list_copy(path), small_cave_revist)
+            
+    return None
 
+def get_map_of_caves(filename):
     lines = fileutils.get_file_lines(filename)
 
     #map = dict()
     map = defaultdict(set)
 
     for l in lines:
-        print(f"DEBUG: {l}")
+        # print(f"DEBUG: {l}")
         pair = l.split("-")
         map[pair[0]].add(pair[1])
         map[pair[1]].add(pair[0])
+    
+    #print(f"DEBUG: map={map}")
+    return map
 
-    print(f"DEBUG: map={map}")
+def display_paths(paths):
+    lines = [] 
+    for l in paths:
+        print(f"DEBUG: {l}")   
+
+def count_part1_paths(filename):
+    map = get_map_of_caves(filename)
 
     paths.clear()
     get_path(map, 'start', list())
-    for path in paths:
-        print(f"DEBUG: path={path}")
+    
+    display_paths(paths)
         
     return len(paths)
+
+def count_part2_paths(filename):
+    map = get_map_of_caves(filename)
+
+    upaths = set()    
+    for k in map.keys():
+        if k.islower() and k not in ['start', 'end']:
+            paths.clear()
+            print(f"k = {k}")
+            umap = copy.deepcopy(map)
+            values = umap[k]
+            umap[k + '*'] = values
+            for v in values:
+                uvals = umap[v]
+                uvals.add(k + '*')
+                umap[v] = uvals
+            get_path(umap, 'start', list())    
+            for p in paths:                                       
+                upaths.add( p.replace('*', ''))    
+
+    display_paths(upaths)
+
+    return len(upaths)
--- aoc-2021/aoc-2021-day-12/solution-in-python3/test_solution.py
+++ aoc-2021/aoc-2021-day-12/solution-in-python3/test_solution.py
@@ -12,4 +12,17 @@ import solution
     ],    
 )
 def test_part1_solution(filename, expected):
-    assert solution.count_paths(filename) ==  expected
+    assert solution.count_part1_paths(filename) ==  expected
+
+
+@pytest.mark.parametrize(
+    "filename,expected",
+    [
+        pytest.param("puzzle-input-example-1.txt", 36),
+        pytest.param("puzzle-input-example-2.txt", 103),
+        pytest.param("puzzle-input-example-3.txt", 3509),
+        pytest.param("puzzle-input-full.txt", 147848), 
+    ],    
+)
+def test_part2_solution(filename, expected):
+    assert solution.count_part2_paths(filename) ==  expected    
