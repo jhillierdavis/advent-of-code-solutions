93176b30982d72eb2727ea649c696a8a9652e4f7
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-10-12 16:23:10 +0100

    Refactored current solution implementation

--- aoc-2021/aoc-2021-day-13/solution-in-python3/solution.py
+++ aoc-2021/aoc-2021-day-13/solution-in-python3/solution.py
@@ -25,4 +25,50 @@ def create_grid_from_file(filename):
         g.set_symbol(p, '#')
 
     return g
+
+
+def get_fold_input(filename):
+    folds = fileutils.get_lines_after_empty_from_file(filename)
+    #print(f"DEBUG: folds: {folds}")
+    fold = folds[0].split("fold along ",1)[1]
+    return fold.split("=")
+
+
+def process_first_fold_from_filename(filename):
+    g = create_grid_from_file(filename)
+
+    fold = get_fold_input(filename)
+
+    
+    if fold[0] == 'y': # fold vertically       
+        fold_y = int(fold[1])
+
+        #print(f"DEBUG: fold_y: {fold_y}")
+
+        # Upper grid
+        ug = g.get_subgrid_from_origin(g.get_width(), fold_y -1)
+
+        # Lower grid
+        lg = g.get_subgrid_inclusive(point.Point2D(0, fold_y), point.Point2D(g.get_width() - 1, g.get_height() - 1))
+        ilg = lg.get_inverted_vertically()
+        
+        g = ug.merge_symbol(ilg, '#')
+        
+
+    elif fold[0] == 'x': # fold horizontally   
+        fold_x = int(fold[1])
+
+        # Left grid
+        lg = g.get_subgrid_from_origin(fold_x, g.get_height())
+
+        # Right grid
+        rg = g.get_subgrid_inclusive(point.Point2D(fold_x, 0), point.Point2D(g.get_width() - 1, g.get_height() - 1))
+        irg = rg.get_inverted_horizontally()
+
+        g = lg.merge_symbol(irg, '#')        
+    else:
+        print(f"DEBUG: fold[0]={fold[0]} ")
+        raise Exception(f"Unhandled instruction: {fold[0]}")
+
+    return g.count_symbol('#')   
         
--- aoc-2021/aoc-2021-day-13/solution-in-python3/test_solution.py
+++ aoc-2021/aoc-2021-day-13/solution-in-python3/test_solution.py
@@ -1,24 +1,7 @@
 import pytest
 
-from helpers import fileutils, point
+# Local
 import solution
-
-
-def count_dots(grid):
-    count = 0
-    for x in range(grid.set_width()):
-        for y in range(grid.set_height()):
-            value = grid.get_symbol(point.Point2D(x,y))
-            if value == '#':
-                count += 1
-    return count
-
-def get_fold_input(filename):
-    folds = fileutils.get_lines_after_empty_from_file(filename)
-    #print(f"DEBUG: folds: {folds}")
-    fold = folds[0].split("fold along ",1)[1]
-    return fold.split("=")
-
 
 @pytest.mark.parametrize(
     "filename,expected",
@@ -28,37 +11,4 @@ def get_fold_input(filename):
     ],    
 )
 def test_part1_solution(filename, expected):
-    g = solution.create_grid_from_file(filename)
-
-    fold = get_fold_input(filename)
-
-    
-    if fold[0] == 'y': # fold vertically       
-        fold_y = int(fold[1])
-
-        #print(f"DEBUG: fold_y: {fold_y}")
-
-        # Upper grid
-        ug = g.get_subgrid_from_origin(g.get_width(), fold_y -1)
-
-        lg = g.get_subgrid_inclusive(point.Point2D(0, fold_y), point.Point2D(g.get_width() - 1, g.get_height() - 1))
-        ilg = lg.get_inverted_vertically()
-        
-        g = ug.merge_symbol(ilg, '#')
-        
-
-    elif fold[0] == 'x': # fold horizontally   
-        fold_x = int(fold[1])
-
-        # Upper grid
-        lg = g.get_subgrid_from_origin(fold_x, g.get_height())
-
-        rg = g.get_subgrid_inclusive(point.Point2D(fold_x, 0), point.Point2D(g.get_width() - 1, g.get_height() - 1))
-        irg = rg.get_inverted_horizontally()
-
-        g = lg.merge_symbol(irg, '#')        
-    else:
-        print(f"DEBUG: fold[0]={fold[0]} ")
-        raise Exception(f"Unhandled instruction: {fold[0]}")
-
-    assert g.count_symbol('#') == expected
+    assert solution.process_first_fold_from_filename(filename) == expected
