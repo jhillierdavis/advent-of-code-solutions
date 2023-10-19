1587f59045e48630f257f875119c0b45ffc180f9
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-10-19 14:45:01 +0100

    AOC 2021 Day 15: Part 2 - added failing unit test ahead of implementation

---
+++ aoc-2021/aoc-2021-day-15/solution-in-python3/procedural_generation_solution.py
@@ -0,0 +1,5 @@
+from helpers import grid, point
+
+def spawn_grid_from(original_grid):
+    # TODO: Implement
+    return original_grid
--- aoc-2021/aoc-2021-day-15/solution-in-python3/test_solution.py
+++ aoc-2021/aoc-2021-day-15/solution-in-python3/test_solution.py
@@ -1,7 +1,7 @@
 import pytest
 
 # Local
-import solution, dijkstras_algorithm_solution
+import solution, dijkstras_algorithm_solution, procedural_generation_solution
 
 @pytest.mark.parametrize(
     "filename,expected",
@@ -24,3 +24,20 @@ def test_calculate_initial_path_score(filename, expected):
 )
 def test_solution(filename, expected):
     assert dijkstras_algorithm_solution.calcuate_lowest_risk_score(filename) == expected
+
+@pytest.mark.parametrize(
+    "filename,expected",
+    [
+        pytest.param("puzzle-input-example.txt", "puzzle-input-example-part2.txt"),
+    ],    
+)
+def test_procedurally_generated_grid(filename, expected):
+    # Given: original & expected grids
+    original_grid = dijkstras_algorithm_solution.create_chiton_grid_from_file(filename)
+    expected_grid = dijkstras_algorithm_solution.create_chiton_grid_from_file(expected)
+
+    # When: a new grid is generated (based on the original)
+    procedurally_generated_grid = procedural_generation_solution.spawn_grid_from(original_grid)
+
+    # Then: the expected (larger) grid is generated
+    assert procedurally_generated_grid == expected_grid
