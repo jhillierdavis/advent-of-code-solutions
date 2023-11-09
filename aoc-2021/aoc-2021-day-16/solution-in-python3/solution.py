312148ae05b964b331dae95148066b93da2eac11
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-11-09 20:51:45 +0000

    AOC 2021 Day 16: Part 1 implimentation WIP

--- aoc-2021/aoc-2021-day-16/solution-in-python3/solution.py
+++ aoc-2021/aoc-2021-day-16/solution-in-python3/solution.py
@@ -1,3 +1,90 @@
+def parse_hex_string_to_packet_version_sum(hex_string):
+    map = create_hex_to_binary_map()
+    binary_string = hex_to_binary(map, hex_string)
+
+    packet_list = parse_binary_string_to_packet_list(binary_string)
+
+    sum = 0
+    for p in packet_list:
+        sum += get_version_sum_for_packet(p)
+    return sum
+
+
+def get_next_packet_literal(packet_header, binary_string):
+        value = ""
+        i = 6 # Header offset
+        end = False         
+        while not end and i < len(binary_string):
+            if binary_string[i] == "0": # indicates last value packet
+                end = True
+            value += binary_string[i+1:i+5]
+            i += 5
+        packet = PacketLiteral(packet_header, value)
+        return packet
+
+
+def parse_binary_string_to_next_packet(binary_string:str) :
+    if len(binary_string) < 11:
+        return None
+
+    # Determine the packet header
+    packet_header = PacketHeader(binary_string)
+
+    if packet_header.is_literal():
+        packet = get_next_packet_literal(packet_header, binary_string)
+        #print(f"Processing Literal Packet: {packet}")
+        return packet
+    
+    # Operator
+    packet = PacketOperator(packet_header, int(binary_string[6]))
+
+    if packet.get_operator_type_id() == 0:        
+        value = binary_string[7:22]
+        num_bits = int(value, base=2)
+        packet.set_body(binary_string[7:22+num_bits])
+        sub_packets = parse_binary_string_to_packet_list(binary_string[22:22+num_bits])
+        for sp in sub_packets:
+            packet.add_sub_packet(sp)    
+        #print(f"Processing Operator Packet (type 0): {packet}")    
+        return packet
+    
+    if packet.get_operator_type_id() == 1:
+        offset = 18
+        num_packs = int(binary_string[7:offset], base=2)
+        while num_packs > 0:
+            sub_packet = parse_binary_string_to_next_packet(binary_string[offset:])
+            if None == sub_packet:
+                break
+            packet.add_sub_packet(sub_packet)
+            offset += sub_packet.get_length()
+            num_packs -= 1
+        #print(f"Processing Operator Packet (type 1): {packet}")    
+        return packet
+
+    return None
+
+
+def parse_binary_string_to_packet_list(binary_string:str) -> []:
+    packet_list = []
+    offset = 0
+
+    while True:
+        remainder = binary_string[offset:]
+        #print(f"DEBUG: parse_binary_string_to_packet_list: remainder={remainder}")
+
+        if len(remainder) < 11:
+            break
+
+        next_packet = parse_binary_string_to_next_packet(remainder)
+        if not next_packet:
+            break
+
+        offset += next_packet.get_length()
+        packet_list.append(next_packet)
+        
+    return packet_list
+
+
 
 def create_hex_to_binary_map():
     map = {}
@@ -44,11 +131,10 @@ def get_version_sum_for_packet(packet) -> int:
             version_sum += get_version_sum_for_packet(sp)
     return version_sum
 
-
 def get_version_sum(hex_string:str) -> int:
     packet = parse_hex_string_to_packet(hex_string)
     return get_version_sum_for_packet(packet)
-
+        
 
 def parse_binary_string_to_packet(binary_string:str):
 
@@ -107,7 +193,7 @@ def parse_binary_string_to_packet(binary_string:str):
 class PacketHeader():
 
     def __init__(self, binary_string):
-        print(f"DEBUG: Constructing packet header from binary_string: {binary_string}")
+        #print(f"DEBUG: Constructing packet header from binary_string: {binary_string}")
 
         self.bits = binary_string[0:6]
 
@@ -164,12 +250,16 @@ class PacketLiteral:
 
     def get_size(self):
         return self.packet_header.get_size() + len(self.body)
+    
+    def get_length(self):
+        return 1 + self.get_size()
 
     def __str__(self):
         readable = "PacketLiteral: id: " + str(id(self)) \
             + ", header: " + str(self.get_header()) \
             + ", body: " + str(self.get_body()) \
-            + ", value: " + str(self.get_value())     
+            + ", value: " + str(self.get_value()) \
+            + ", length: " + str(self.get_length())
         return readable   
 
 
@@ -199,10 +289,25 @@ class PacketOperator:
     
     def set_body(self, body):
         self.body = body
-    
+
+    def get_body(self):
+        return self.body
+
     def get_size(self):
         return self.packet_header.get_size() + 1 + len(self.body)
-    
+
+    def get_length(self):
+        prefix_length = self.packet_header.get_size() + 1
+        if self.get_operator_type_id() == 0:
+            prefix_length += 15
+        else:
+            prefix_length += 11
+        length_sub_packets = 0
+        for sp in self.get_sub_packets():
+            length_sub_packets += sp.get_length()
+        return prefix_length + length_sub_packets
+
+
     def __repr__(self) -> str:
         return str(self)
 
@@ -210,5 +315,7 @@ class PacketOperator:
         readable = "PacketOperator: id: " + str(id(self)) \
             + ", header: " + str(self.get_header()) \
             + ", operator_type_id: " + str(self.get_operator_type_id()) \
-            + ", number_of_sub_packets: " + str(self.get_number_of_sub_packets())
+            + ", number_of_sub_packets: " + str(self.get_number_of_sub_packets()) \
+            + ", body: " + str(self.get_body()) \
+            + ", length: " + str(self.get_length())
         return readable           
