a9ea1733d36a0f6aed806eda45a5765cf7020bc0
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-11-10 11:56:59 +0000

    AOC 2021 Day 16: Part 1 solution implementation

--- aoc-2021/aoc-2021-day-16/solution-in-python3/solution.py
+++ aoc-2021/aoc-2021-day-16/solution-in-python3/solution.py
@@ -34,6 +34,14 @@ def hex_string_to_binary_string(hex_string):
     return binary_string
 
 
+def get_version_sum_for_packet(packet) -> int:
+    version_sum = packet.get_header().get_version()
+    #print(f"DEBUG: version_sum={version_sum} for {packet}")
+    if packet.get_header().is_operator():
+        for sp in packet.get_sub_packets():
+            version_sum += get_version_sum_for_packet(sp)
+    return version_sum
+
 
 def parse_hex_string_to_packet_version_sum(hex_string):
     map = create_hex_to_binary_map()
@@ -56,7 +64,7 @@ def get_next_packet_literal(packet_header, binary_string):
                 end = True
             value += binary_string[i+1:i+5]
             i += 5
-        packet = PacketLiteral(packet_header, value)
+        packet = PacketLiteral(packet_header, value, i)
         return packet
 
 
@@ -90,10 +98,12 @@ def parse_binary_string_to_next_packet(binary_string:str) :
         num_packs = int(binary_string[7:offset], base=2)
         while num_packs > 0:
             sub_packet = parse_binary_string_to_next_packet(binary_string[offset:])
-            if None == sub_packet:
-                break
-            packet.add_sub_packet(sub_packet)
-            offset += sub_packet.get_length()
+            print(f"DEBUG: Adding sub-packet: {sub_packet}")
+            #if None == sub_packet:
+            #    break
+            offset += sub_packet.get_bit_length()                        
+            packet.add_sub_packet(sub_packet)            
+            print(f"DEBUG: Processing Operator Packet (type 1): {packet} offset={offset} num_packs={num_packs}")
             num_packs -= 1
         #print(f"Processing Operator Packet (type 1): {packet}")    
         return packet
@@ -116,90 +126,10 @@ def parse_binary_string_to_packet_list(binary_string:str) -> []:
         if not next_packet:
             break
 
-        offset += next_packet.get_length()
+        offset += next_packet.get_bit_length()
         packet_list.append(next_packet)
         
     return packet_list
-
-
-
-
-"""
-def parse_hex_string_to_packet(hex_string:str):
-    map = create_hex_to_binary_map()
-    binary_string = hex_to_binary(map, hex_string)
-    
-    print(f"DEBUG: hex_string: {hex_string} binary_string: {binary_string}")
-    return parse_binary_string_to_packet(binary_string)
-"""
-
-def get_version_sum_for_packet(packet) -> int:
-    version_sum = packet.get_header().get_version()
-    print(f"DEBUG: version_sum={version_sum} for {packet}")
-    if packet.get_header().is_operator():
-        for sp in packet.get_sub_packets():
-            version_sum += get_version_sum_for_packet(sp)
-    return version_sum
-
-"""
-def get_version_sum(hex_string:str) -> int:
-    packet = parse_hex_string_to_packet(hex_string)
-    return get_version_sum_for_packet(packet)
-"""        
-"""
-def parse_binary_string_to_packet(binary_string:str):
-
-    if len(binary_string) <= 7:
-        return None
-
-    packet_header = PacketHeader(binary_string)
-    packet = None
-    if packet_header.is_literal():
-        value = ""
-        i = 6
-        end = False         
-        while not end:
-            if binary_string[i] == "0": # indicates last value packet
-                end = True
-            value += binary_string[i+1:i+5]
-            i += 5
-        packet = PacketLiteral(packet_header, value)
-    else:
-        packet = PacketOperator(packet_header, int(binary_string[6]))
-        if packet.get_operator_type_id() == 0:
-
-            value = binary_string[7:22]
-            num_bits = int(value, base=2)
-            packet.set_body(binary_string[7:22+num_bits])
-            first_sub_packet = binary_string[22:22+num_bits]
-            #print(f"DEBUG: first_sub_packet={first_sub_packet}")
-            sub_packet = parse_binary_string_to_packet(first_sub_packet)
-            packet.add_sub_packet(sub_packet)            
-            if sub_packet and sub_packet.get_size() < num_bits:
-                remainder = binary_string[22+sub_packet.get_size()+1:22+num_bits]
-                print(f"DEBUG: first packet remainder={remainder}")
-                sub_packet = parse_binary_string_to_packet(remainder)
-                if sub_packet:
-                    packet.add_sub_packet(sub_packet)            
-            remainder = binary_string[22+num_bits:]
-            print(f"DEBUG: remainder={remainder}")
-            next_packet = parse_binary_string_to_packet(remainder)
-            if next_packet:
-                packet.add_sub_packet(next_packet)
-            
-        else:
-            offset = 18
-            num_packs = int(binary_string[7:offset], base=2)
-            print(f"Number of packs: {num_packs}")
-            for i in range(num_packs):
-                next_packet = parse_binary_string_to_packet(binary_string[offset:])
-                if next_packet:
-                    packet.add_sub_packet(next_packet)                
-                    offset += next_packet.get_size() + 1
-            
-
-    return packet
-"""
 
 
 class PacketHeader():
@@ -218,7 +148,7 @@ class PacketHeader():
     def get_bits(self):
         return self.bits
     
-    def get_size(self):
+    def get_bit_length(self):
         return len(self.bits)
 
     def get_version(self):
@@ -239,6 +169,7 @@ class PacketHeader():
     def __str__(self):
         readable = "PacketHeader: id: " + str(id(self)) \
             + ", bits: " + self.get_bits() \
+            + ", bit_length: " + str(self.get_bit_length()) \
             + ", version: " + str(self.get_version()) \
             + ", type_id: " + str(self.get_type_id())  
            
@@ -247,9 +178,10 @@ class PacketHeader():
 
 class PacketLiteral:
 
-    def __init__(self, packet_header:PacketHeader, body:str):
+    def __init__(self, packet_header:PacketHeader, body:str, bit_length:int):
         self.packet_header = packet_header
         self.body = body
+        self.bit_length = bit_length
 
     def get_header(self):
         return self.packet_header
@@ -260,18 +192,15 @@ class PacketLiteral:
     def get_value(self):
         return int(self.body,2)
 
-    def get_size(self):
-        return self.packet_header.get_size() + len(self.body)
-    
-    def get_length(self):
-        return 1 + self.get_size()
+    def get_bit_length(self):
+        return self.bit_length
 
     def __str__(self):
         readable = "PacketLiteral: id: " + str(id(self)) \
             + ", header: " + str(self.get_header()) \
             + ", body: " + str(self.get_body()) \
             + ", value: " + str(self.get_value()) \
-            + ", length: " + str(self.get_length())
+            + ", bit_length: " + str(self.get_bit_length())
         return readable   
 
 
@@ -305,17 +234,12 @@ class PacketOperator:
     def get_body(self):
         return self.body
 
-    def get_size(self):
-        return self.packet_header.get_size() + 1 + len(self.body)
-
-    def get_length(self):
-        prefix_length = self.packet_header.get_size() + 1
+    def get_bit_length(self):
+        prefix_length = 18
         if self.get_operator_type_id() == 0:
-            prefix_length += 15
-        else:
-            prefix_length += 11
+            prefix_length = 22
         for sp in self.get_sub_packets():
-            prefix_length += sp.get_length()
+            prefix_length += sp.get_bit_length()
         return prefix_length 
 
 
@@ -328,5 +252,5 @@ class PacketOperator:
             + ", operator_type_id: " + str(self.get_operator_type_id()) \
             + ", number_of_sub_packets: " + str(self.get_number_of_sub_packets()) \
             + ", body: " + str(self.get_body()) \
-            + ", length: " + str(self.get_length())
+            + ", bit_length: " + str(self.get_bit_length())
         return readable           
--- aoc-2021/aoc-2021-day-16/solution-in-python3/test_solution.py
+++ aoc-2021/aoc-2021-day-16/solution-in-python3/test_solution.py
@@ -16,19 +16,20 @@ def test_hex_to_binary(hex_str, expected):
     assert solution.hex_to_binary(map, hex_str) == expected
 
 @pytest.mark.parametrize(
-    "hex_string, expected_version, expected_type_id, expected_literal_value_as_binary_string, expected_literal_value",
+    "hex_string, expected_binary_string, expected_version, expected_type_id, expected_bit_length, expected_literal_value_as_binary_string, expected_literal_value",
     [
-        pytest.param("D2FE28", 6, 4, "011111100101", 2021),
-        pytest.param("D2FEA84", 6, 4, "0111111001010001", 32337), 
-        pytest.param("114", 0, 4, "1010", 10), # 000 100 0 1010 0
-        pytest.param("B16", 5, 4, "1011", 11), # 101 100 0 1011 0
-        pytest.param("118", 0, 4, "1100", 12), # 000 100 0 1100 0
-        pytest.param("71A", 3, 4, "1101", 13), # 011 100 0 1101 0
+        pytest.param("D2FE28", "110100101111111000101000", 6, 4, 21, "011111100101", 2021),
+        pytest.param("D2FEA84", "1101001011111110101010000100", 6, 4, 26, "0111111001010001", 32337), 
+        pytest.param("114", "000100010100", 0, 4, 11, "1010", 10), # 000 100 0 1010 0
+        pytest.param("B16", "101100010110", 5, 4, 11, "1011", 11), # 101 100 0 1011 0
+        pytest.param("118", "000100011000", 0, 4, 11, "1100", 12), # 000 100 0 1100 0
+        pytest.param("71A", "011100011010", 3, 4, 11, "1101", 13), # 011 100 0 1101 0
     ],    
 )
-def test_next_literal_packet(hex_string, expected_version, expected_type_id, expected_literal_value_as_binary_string, expected_literal_value):
+def test_next_literal_packet(hex_string, expected_binary_string, expected_version, expected_type_id, expected_bit_length, expected_literal_value_as_binary_string, expected_literal_value):
     # When: packet created from binary srting
     binary_string = solution.hex_string_to_binary_string(hex_string)
+    assert binary_string == expected_binary_string
     packet = solution.parse_binary_string_to_next_packet(binary_string)
     assert type(packet) is solution.PacketLiteral
 
@@ -39,13 +40,16 @@ def test_next_literal_packet(hex_string, expected_version, expected_type_id, exp
     assert header.is_operator() == False
     assert header.get_version() == expected_version
     assert header.get_type_id() == expected_type_id
-    assert header.get_size() == 6
+    assert header.get_bit_length() == 6
 
     # Then: packet body has expected literal attributes
     assert packet.get_body() == expected_literal_value_as_binary_string
     assert packet.get_value() == expected_literal_value
-    assert packet.get_size() == 6 + len(expected_literal_value_as_binary_string)
 
+    # And: literal packet has expected bit length
+    assert packet.get_bit_length() == expected_bit_length
+    
+"""
 @pytest.mark.parametrize(
     "hex_string, expected_version, expected_type_id, expected_literal_value_as_binary_string, expected_literal_value",
     [
@@ -70,34 +74,35 @@ def test_literal_packet(hex_string, expected_version, expected_type_id, expected
     assert header.is_operator() == False
     assert header.get_version() == expected_version
     assert header.get_type_id() == expected_type_id
-    assert header.get_size() == 6
+    assert header.get_bit_length() == 6
 
     # Then: packet body has expected literal attributes
     assert packet.get_body() == expected_literal_value_as_binary_string
     assert packet.get_value() == expected_literal_value
-    assert packet.get_size() == 6 + len(expected_literal_value_as_binary_string)
-
+    assert packet.get_bit_length() == 6 + len(expected_literal_value_as_binary_string)
+"""
 
 @pytest.mark.parametrize(
-    "hex_string, expected_version, expected_type_id, expected_length_type_id, expected_sub_packet_literals",
+    "hex_string, expected_binary_string, expected_version, expected_type_id, expected_length_type_id, expected_bit_length, expected_sub_packet_literals",
     [
-        pytest.param("38006F45291200", 1, 6, 0, [10,20]),
-        pytest.param("EE00D40C823060", 7, 3, 1, [1,2,3])
+        pytest.param("38006F45291200", "00111000000000000110111101000101001010010001001000000000", 1, 6, 0, 49, [10,20]),
+        pytest.param("EE00D40C823060", "11101110000000001101010000001100100000100011000001100000", 7, 3, 1, 51, [1,2,3]),
     ],    
 )
-def test_operator_packet_using_hex(hex_string, expected_version, expected_type_id, expected_length_type_id, expected_sub_packet_literals):
+def test_operator_packet_using_hex(hex_string, expected_binary_string, expected_version, expected_type_id, expected_length_type_id, expected_bit_length, expected_sub_packet_literals):
     binary_string = solution.hex_string_to_binary_string(hex_string)
-    return test_operator_packet_using_binary(binary_string, expected_version, expected_type_id, expected_length_type_id, expected_sub_packet_literals)
+    assert binary_string == expected_binary_string
+    return test_operator_packet_using_binary(binary_string, expected_version, expected_type_id, expected_length_type_id, expected_bit_length, expected_sub_packet_literals)
 
 
 @pytest.mark.parametrize(
-    "binary_string, expected_version, expected_type_id, expected_length_type_id, expected_sub_packet_literals",
+    "binary_string, expected_version, expected_type_id, expected_length_type_id, expected_bit_length, expected_sub_packet_literals",
     [
-        pytest.param("001000100000000010000100011000111000110100", 1, 0, 1, [12,13]),
-        pytest.param("00000000000000000101100001000101010110001011", 0, 0, 0, [10, 11])
+        pytest.param("001000100000000010000100011000111000110100", 1, 0, 1, 40, [12,13]),
+        pytest.param("00000000000000000101100001000101010110001011", 0, 0, 0, 44, [10, 11])
     ],    
 )
-def test_operator_packet_using_binary(binary_string, expected_version, expected_type_id, expected_length_type_id, expected_sub_packet_literals):
+def test_operator_packet_using_binary(binary_string, expected_version, expected_type_id, expected_length_type_id, expected_bit_length, expected_sub_packet_literals):
     # When: packet created from binary srting
     packet = solution.parse_binary_string_to_next_packet(binary_string)
     assert type(packet) is solution.PacketOperator
@@ -109,11 +114,12 @@ def test_operator_packet_using_binary(binary_string, expected_version, expected_
     assert header.is_literal() == False
     assert header.get_version() == expected_version
     assert header.get_type_id() == expected_type_id
-    assert header.get_size() == 6
+    assert header.get_bit_length() == 6
 
     # Then: packet has expected operater attributes
     assert packet.get_operator_type_id() == expected_length_type_id
     assert packet.get_number_of_sub_packets() == len(expected_sub_packet_literals)
+    assert packet.get_bit_length() == expected_bit_length
     #assert packet.get_sub_packet_literals() == expected_sub_packet_literals 
     for sp in packet.get_sub_packets():
         assert type(sp) is solution.PacketLiteral
@@ -127,7 +133,7 @@ def test_operator_packet_using_binary(binary_string, expected_version, expected_
     ],    
 )
 def test_operator_packets_using_binary(binary_string, expected_version, expected_type_id, expected_length_type_id, expected_sub_packet_literals):
-    # When: packet created from binary srting
+    # When: packet created from binary string
     packet = solution.parse_binary_string_to_next_packet(binary_string)
 
     assert type(packet) is solution.PacketOperator
@@ -139,7 +145,7 @@ def test_operator_packets_using_binary(binary_string, expected_version, expected
     assert header.is_literal() == False
     assert header.get_version() == expected_version
     assert header.get_type_id() == expected_type_id
-    assert header.get_size() == 6
+    assert header.get_bit_length() == 6
 
     # Then: packet has expected operater attributes
     assert packet.get_operator_type_id() == expected_length_type_id
@@ -179,7 +185,7 @@ def test_problematic_packet():
         pytest.param("620080001611562C8802118E34", 12), # operator packet (version 3) which contains two sub-packets; each sub-packet is an operator packet that contains two literal values
         pytest.param("C0015000016115A2E0802F182340",23), # has the same structure as the previous example, but the outermost packet uses a different length type ID
         pytest.param("A0016C880162017C3686B18A3D4780", 31), # operator packet that contains an operator packet that contains an operator packet that contains five literal values
-        #pytest.param("2056FA18025A00A4F52AB13FAB6CDA779E1B2012DB003301006A35C7D882200C43289F07A5A192D200C1BC011969BA4A485E63D8FE4CC80480C00D500010F8991E23A8803104A3C425967260020E551DC01D98B5FEF33D5C044C0928053296CDAFCB8D4BDAA611F256DE7B945220080244BE59EE7D0A5D0E6545C0268A7126564732552F003194400B10031C00C002819C00B50034400A70039C009401A114009201500C00B00100D00354300254008200609000D39BB5868C01E9A649C5D9C4A8CC6016CC9B4229F3399629A0C3005E797A5040C016A00DD40010B8E508615000213112294749B8D67EC45F63A980233D8BCF1DC44FAC017914993D42C9000282CB9D4A776233B4BF361F2F9F6659CE5764EB9A3E9007ED3B7B6896C0159F9D1EE76B3FFEF4B8FCF3B88019316E51DA181802B400A8CFCC127E60935D7B10078C01F8B50B20E1803D1FA21C6F300661AC678946008C918E002A72A0F27D82DB802B239A63BAEEA9C6395D98A001A9234EA620026D1AE5CA60A900A4B335A4F815C01A800021B1AE2E4441006A0A47686AE01449CB5534929FF567B9587C6A214C6212ACBF53F9A8E7D3CFF0B136FD061401091719BC5330E5474000D887B24162013CC7EDDCDD8E5E77E53AF128B1276D0F980292DA0CD004A7798EEEC672A7A6008C953F8BD7F781ED00395317AF0726E3402100625F3D9CB18B546E2FC9C65D1C20020E4C36460392F7683004A77DB3DB00527B5A85E06F253442014A00010A8F9106108002190B61E4750004262BC7587E801674EB0CCF1025716A054AD47080467A00B864AD2D4B193E92B4B52C64F27BFB05200C165A38DDF8D5A009C9C2463030802879EB55AB8010396069C413005FC01098EDD0A63B742852402B74DF7FDFE8368037700043E2FC2C8CA00087C518990C0C015C00542726C13936392A4633D8F1802532E5801E84FDF34FCA1487D367EF9A7E50A43E90", 804)
+        pytest.param("2056FA18025A00A4F52AB13FAB6CDA779E1B2012DB003301006A35C7D882200C43289F07A5A192D200C1BC011969BA4A485E63D8FE4CC80480C00D500010F8991E23A8803104A3C425967260020E551DC01D98B5FEF33D5C044C0928053296CDAFCB8D4BDAA611F256DE7B945220080244BE59EE7D0A5D0E6545C0268A7126564732552F003194400B10031C00C002819C00B50034400A70039C009401A114009201500C00B00100D00354300254008200609000D39BB5868C01E9A649C5D9C4A8CC6016CC9B4229F3399629A0C3005E797A5040C016A00DD40010B8E508615000213112294749B8D67EC45F63A980233D8BCF1DC44FAC017914993D42C9000282CB9D4A776233B4BF361F2F9F6659CE5764EB9A3E9007ED3B7B6896C0159F9D1EE76B3FFEF4B8FCF3B88019316E51DA181802B400A8CFCC127E60935D7B10078C01F8B50B20E1803D1FA21C6F300661AC678946008C918E002A72A0F27D82DB802B239A63BAEEA9C6395D98A001A9234EA620026D1AE5CA60A900A4B335A4F815C01A800021B1AE2E4441006A0A47686AE01449CB5534929FF567B9587C6A214C6212ACBF53F9A8E7D3CFF0B136FD061401091719BC5330E5474000D887B24162013CC7EDDCDD8E5E77E53AF128B1276D0F980292DA0CD004A7798EEEC672A7A6008C953F8BD7F781ED00395317AF0726E3402100625F3D9CB18B546E2FC9C65D1C20020E4C36460392F7683004A77DB3DB00527B5A85E06F253442014A00010A8F9106108002190B61E4750004262BC7587E801674EB0CCF1025716A054AD47080467A00B864AD2D4B193E92B4B52C64F27BFB05200C165A38DDF8D5A009C9C2463030802879EB55AB8010396069C413005FC01098EDD0A63B742852402B74DF7FDFE8368037700043E2FC2C8CA00087C518990C0C015C00542726C13936392A4633D8F1802532E5801E84FDF34FCA1487D367EF9A7E50A43E90", 917)
     ],    
 )
 def test_version_sum(hex_string, expected):
