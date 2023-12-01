42d9f2f3160ccb6160d7cb388a9e1535cd23b571
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-12-01 09:36:20 +0000

    AOC 2023: Amended script to limit to existing day's solutions

--- aoc-2023/run-all-day-tests-solution-in-python3.sh
+++ aoc-2023/run-all-day-tests-solution-in-python3.sh
@@ -6,7 +6,7 @@ dir=$PWD
 #./run-tests-solution-in-python3.sh
 
 # Loop through sub-directories of each puzzle day & execute unit tests for each
-for i in {01..16}; do
+for i in {01..01}; do
     # Pad value (if required) to 2 digits (e.g. 07)
     x=$i
     while [ ${#x} -ne 2 ];
