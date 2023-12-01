9ea3ca21d251f565cb0189ffee7d1d9dad87d3cf
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-12-01 08:34:15 +0000

    AOC 2023 Day 01: Refactored implementation

--- aoc-2023/aoc-2023-day-01/solution-in-python3/solution.py
+++ aoc-2023/aoc-2023-day-01/solution-in-python3/solution.py
@@ -1,3 +1,48 @@
+from helpers import fileutils
 
-def solve(input):
-    return False # TODO
+def extract_number_from_string(input, digits_only=True):
+    num_str = ""
+    for i in range(len(input)):
+        ch = input[i]
+        if ch.isnumeric():
+            num_str += ch
+        elif digits_only == False:
+             substr = input[i:]
+             if (substr.startswith('one')):
+                  num_str += '1'
+             if (substr.startswith('two')):
+                  num_str += '2'
+             if (substr.startswith('three')):
+                  num_str += '3'
+             if (substr.startswith('four')):
+                  num_str += '4'
+             if (substr.startswith('five')):
+                  num_str += '5'
+             if (substr.startswith('six')):
+                  num_str += '6'
+             if (substr.startswith('seven')):
+                  num_str += '7'
+             if (substr.startswith('eight')):
+                  num_str += '8'
+             if (substr.startswith('nine')):
+                  num_str += '9'
+
+    length = len(num_str)
+
+    if length == 1:
+           num_str += num_str
+
+    if length > 2:
+           num_str = num_str[0] + num_str[length-1]
+
+    #print(f"DEBUG: {num_str}")
+    return int(num_str)
+
+
+def extract_first_and_last_numbers_from_file(filename, digits_only=True):
+    lines = fileutils.get_file_lines(filename)
+
+    ans = 0
+    for l in lines:
+         ans += extract_number_from_string(l, digits_only)
+    return ans
--- aoc-2023/aoc-2023-day-01/solution-in-python3/test_solution.py
+++ aoc-2023/aoc-2023-day-01/solution-in-python3/test_solution.py
@@ -1,46 +1,6 @@
 import pytest
 
-import solution
-from helpers import fileutils
-
-def extract_number_from_string(input, digits_only=True):
-    num_str = ""
-    for i in range(len(input)):
-        ch = input[i]
-        if ch.isnumeric():
-            num_str += ch
-        elif digits_only == False:
-             substr = input[i:]
-             if (substr.startswith('one')):
-                  num_str += '1'
-             if (substr.startswith('two')):
-                  num_str += '2'
-             if (substr.startswith('three')):
-                  num_str += '3'
-             if (substr.startswith('four')):
-                  num_str += '4'
-             if (substr.startswith('five')):
-                  num_str += '5'
-             if (substr.startswith('six')):
-                  num_str += '6'
-             if (substr.startswith('seven')):
-                  num_str += '7'
-             if (substr.startswith('eight')):
-                  num_str += '8'
-             if (substr.startswith('nine')):
-                  num_str += '9'
-
-    length = len(num_str)
-
-    if length == 1:
-           num_str += num_str
-
-    if length > 2:
-           num_str = num_str[0] + num_str[length-1]
-
-    #print(f"DEBUG: {num_str}")
-    return int(num_str)
-        
+import solution        
 
 @pytest.mark.parametrize(
     "input,expected",
@@ -52,7 +12,7 @@ def extract_number_from_string(input, digits_only=True):
     ],    
 )
 def test_extract_number_from_string(input, expected):
-    assert extract_number_from_string(input) == expected
+    assert solution.extract_number_from_string(input) == expected
 
 @pytest.mark.parametrize(
     "input,expected",
@@ -67,16 +27,7 @@ def test_extract_number_from_string(input, expected):
     ],    
 )
 def test_extract_number_from_string_including_words(input, expected):
-    assert extract_number_from_string(input, False) == expected
-
-def extract_first_and_last_numbers_from_file(filename, digits_only=True):
-    lines = fileutils.get_file_lines(filename)
-
-    ans = 0
-    for l in lines:
-         ans += extract_number_from_string(l, digits_only)
-    return ans     
-
+    assert solution.extract_number_from_string(input, False) == expected
 
 @pytest.mark.parametrize(
     "input,expected",
@@ -86,7 +37,7 @@ def extract_first_and_last_numbers_from_file(filename, digits_only=True):
     ],    
 )
 def test_part1_solution(input, expected):
-    assert extract_first_and_last_numbers_from_file(input) == expected
+    assert solution.extract_first_and_last_numbers_from_file(input) == expected
 
 
 @pytest.mark.parametrize(
@@ -97,4 +48,4 @@ def test_part1_solution(input, expected):
     ],    
 )
 def test_part2_solution(input, expected):
-    assert extract_first_and_last_numbers_from_file(input, False) == expected
+    assert solution.extract_first_and_last_numbers_from_file(input, False) == expected
