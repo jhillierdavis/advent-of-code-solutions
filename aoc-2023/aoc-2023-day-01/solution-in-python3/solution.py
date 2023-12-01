077783cf67290a66760d95812b96fa422de2954d
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-12-01 08:42:02 +0000

    AOC 2023 Day 01: Refactored implementation

--- aoc-2023/aoc-2023-day-01/solution-in-python3/solution.py
+++ aoc-2023/aoc-2023-day-01/solution-in-python3/solution.py
@@ -1,6 +1,24 @@
 from helpers import fileutils
 
+def get_word_to_digit_map():
+     word_to_digit_map = dict()
+
+     word_to_digit_map['one'] = '1'
+     word_to_digit_map['two'] = '2'
+     word_to_digit_map['three'] = '3'
+     word_to_digit_map['four'] = '4'
+     word_to_digit_map['five'] = '5'
+     word_to_digit_map['six'] = '6'
+     word_to_digit_map['seven'] = '7'
+     word_to_digit_map['eight'] = '8'
+     word_to_digit_map['nine'] = '9'
+
+     return word_to_digit_map
+
+
 def extract_number_from_string(input, digits_only=True):
+    word_to_digit_map = get_word_to_digit_map()
+
     num_str = ""
     for i in range(len(input)):
         ch = input[i]
@@ -8,35 +26,22 @@ def extract_number_from_string(input, digits_only=True):
             num_str += ch
         elif digits_only == False:
              substr = input[i:]
-             if (substr.startswith('one')):
-                  num_str += '1'
-             if (substr.startswith('two')):
-                  num_str += '2'
-             if (substr.startswith('three')):
-                  num_str += '3'
-             if (substr.startswith('four')):
-                  num_str += '4'
-             if (substr.startswith('five')):
-                  num_str += '5'
-             if (substr.startswith('six')):
-                  num_str += '6'
-             if (substr.startswith('seven')):
-                  num_str += '7'
-             if (substr.startswith('eight')):
-                  num_str += '8'
-             if (substr.startswith('nine')):
-                  num_str += '9'
+
+             for k in word_to_digit_map.keys():
+                if (substr.startswith(k)):
+                  num_str += word_to_digit_map[k]
+                  break
 
     length = len(num_str)
 
-    if length == 1:
+    if length == 1: # First == last, so duplicate
            num_str += num_str
 
-    if length > 2:
+    if length > 2: # Get first + last
            num_str = num_str[0] + num_str[length-1]
 
     #print(f"DEBUG: {num_str}")
-    return int(num_str)
+    return int(num_str) # Convert to integer value
 
 
 def extract_first_and_last_numbers_from_file(filename, digits_only=True):
@@ -44,5 +49,5 @@ def extract_first_and_last_numbers_from_file(filename, digits_only=True):
 
     ans = 0
     for l in lines:
-         ans += extract_number_from_string(l, digits_only)
+         ans += extract_number_from_string(l.lower(), digits_only)
     return ans
