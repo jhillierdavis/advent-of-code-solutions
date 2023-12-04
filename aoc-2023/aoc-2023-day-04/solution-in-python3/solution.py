4fd657258e16c81e4dada1639aa4181d100aab92
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-12-04 06:31:10 +0000

    AOC 2023 Day 04: Part 2 - initial crude completion

--- aoc-2023/aoc-2023-day-04/solution-in-python3/solution.py
+++ aoc-2023/aoc-2023-day-04/solution-in-python3/solution.py
@@ -2,7 +2,7 @@ from helpers import fileutils
 
 from collections import defaultdict
 
-def solve(filename):
+def solve_part1(filename):
 
     lines = fileutils.get_file_lines(filename)
 
@@ -31,5 +31,52 @@ def solve(filename):
         print(f"DBEUG: {winning_cards} {holding_cards} {points}")
 
         total += points
+
+    return total
+
+
+def solve_part2(filename):
+
+    lines = fileutils.get_file_lines(filename)
+
+    map_points = defaultdict(int)
+
+    total = 0
+    card = 0
+    for l in lines:
+        card += 1
+        map_points[card] += 1
+        
+
+        (c,r) = l.split(':')
+
+        (w,h) = r.split('|')
+
+        #print(f"DBEUG: {w} {h}")
+
+        winning_cards = w.split()
+        holding_cards = h.split()
+
+        
+        matches = 0
+        for hc in holding_cards:
+            for wc in winning_cards:
+                if hc == wc:
+                    matches += 1
+
+        #print(f"DBEUG: {card} {winning_cards} {holding_cards} {matches}")
+
+        for i in range(matches):
+            wins_card = card+i+1
+            #print(f"DEBUG: Card {card} wins {wins_card}")
+            map_points[wins_card] += map_points[card] 
+
+        
+        
+
+    #print(f"DEBUG: {map_points}")
+
+    for v in map_points.values():
+        total += v
 
     return total
--- aoc-2023/aoc-2023-day-04/solution-in-python3/test_solution.py
+++ aoc-2023/aoc-2023-day-04/solution-in-python3/test_solution.py
@@ -6,8 +6,19 @@ import solution
     "input,expected",
     [
         pytest.param('puzzle-input-example.txt', 13),
-        pytest.param('puzzle-input-full.txt', 0), # Not 1538
+        pytest.param('puzzle-input-full.txt', 26914), # Not 1538
     ],    
 )
-def test_solution(input, expected):
-    assert solution.solve(input) == expected
+def test_solution_part1(input, expected):
+    assert solution.solve_part1(input) == expected
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        pytest.param('puzzle-input-example.txt', 30),
+        pytest.param('puzzle-input-full.txt', 13080971),
+    ],    
+)
+def test_solution_part2(input, expected):
+    assert solution.solve_part2(input) == expected    
