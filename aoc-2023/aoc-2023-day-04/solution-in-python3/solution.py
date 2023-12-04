5126838a6c28a7e1144b2af151f7b49f22a0a568
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-12-04 12:21:33 +0000

    AOC 2023 Day 04: Refactored solution - further simplification (based on number of matches)

--- aoc-2023/aoc-2023-day-04/solution-in-python3/solution.py
+++ aoc-2023/aoc-2023-day-04/solution-in-python3/solution.py
@@ -2,10 +2,10 @@ from helpers import fileutils
 
 from collections import defaultdict
 
-def get_cards_from_filename(filename):
+def get_card_matches_from_filename(filename):
     lines = fileutils.get_file_lines(filename)
 
-    list_cards = []
+    list_card_matches = []
     scratchcard = 0
     for l in lines:
         scratchcard += 1
@@ -14,47 +14,35 @@ def get_cards_from_filename(filename):
         winning_cards = w.split()
         holding_cards = h.split()
 
-        list_cards.append( (scratchcard, set(winning_cards), set(holding_cards)) )
+        # Number of card matches = size of intersection (of 2 sets; winning cards & holding cards)
+        matches = len(set(holding_cards) & set(winning_cards))
 
-    return list_cards
+        list_card_matches.append(matches)
+
+    return list_card_matches
 
 
 def solve_part1(filename):
 
-    list_cards = get_cards_from_filename(filename)
+    list_card_matches = get_card_matches_from_filename(filename)
 
     total = 0
-    for entry in list_cards:
-        points = 0
-        winning_cards = entry[1]
-        holding_cards = entry[2]
-
-        # Matches = size of intersection (of 2 sets)
-        matches = len(holding_cards & winning_cards) 
+    for matches in list_card_matches:
         if matches > 0:      
             # When there is a match: points = 2 to power of number of (matches - 1)
-            points = 2**(matches-1) 
-
-        #print(f"DEBUG: {winning_cards} {holding_cards} {points}")
-        total += points
-
+            total += 2**(matches-1)
     return total
 
 
 def solve_part2(filename):
     map_points = defaultdict(int)
 
-    list_cards = get_cards_from_filename(filename) 
-    for entry in list_cards:
-        scratchcard = entry[0]
-        winning_cards = entry[1]
-        holding_cards = entry[2]
+    list_card_matches = get_card_matches_from_filename(filename) 
+    scratchcard = 0
+    for matches in list_card_matches:
+        scratchcard += 1
         map_points[scratchcard] += 1
         
-        # Matches = size of intersection (of 2 sets)
-        matches = len(holding_cards & winning_cards) 
-        #print(f"DEBUG: {card} {winning_cards} {holding_cards} {matches}")
-
         for i in range(matches):
             winning_card = scratchcard + i + 1
             #print(f"DEBUG: Card {card} wins {wins_card}")
