5a5cd061ff1d1164c3baa591e907fe00e328fa96
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-12-05 09:23:37 +0000

    AOC 2023 Day 05: Refactored

--- aoc-2023/aoc-2023-day-05/solution-in-python3/solution.py
+++ aoc-2023/aoc-2023-day-05/solution-in-python3/solution.py
@@ -39,14 +39,11 @@ class MyDefaultDict(defaultdict):
         return key
 
 def get_location_for_seed_from_filename(filename, seed):
-    seed_lines = fileutils.get_lines_before_empty_from_file(filename)
+    map_of_maps = get_map_of_maps_from_filename(filename)
+    seed_to_location_map = get_seed_to_location_map_from_seed_list(map_of_maps, [seed])
+    return seed_to_location_map[seed]
 
-    #print(f"DEBUG: seed_lines={seed_lines}")
-    (s,v) = seed_lines[0].split(':')    
-    seed_list = v.split()
-    #print(f"DEBUG: seed_list={seed_list}")
-
-
+def get_map_of_maps_from_filename(filename):
     map_lines = fileutils.get_lines_after_empty_from_file(filename)
     #print(f"DEBUG: {map_lines}={map_lines}")
 
@@ -58,114 +55,47 @@ def get_location_for_seed_from_filename(filename, seed):
     temperature_to_humidity_map = AdjusterMap()
     humidity_to_location_map = AdjusterMap()
     
+    map_of_maps = {}
 
     map = None
     for l in map_lines:
         values = l.split()
 
+        
+
         if values[0] == 'seed-to-soil':
             map = seed_to_soil_map
+            map_of_maps[values[0]] = map
             continue
 
         if values[0] == 'soil-to-fertilizer':
             map = soil_to_fertilizer_map
+            map_of_maps[values[0]] = map
             continue
 
         if values[0] == 'fertilizer-to-water':
             map = fertilizer_to_water_map
+            map_of_maps[values[0]] = map
             continue
 
         if values[0] == 'water-to-light':
             map = water_to_light_map
+            map_of_maps[values[0]] = map
             continue
 
         if values[0] == 'light-to-temperature':
             map = light_to_temperature_map
+            map_of_maps[values[0]] = map
             continue
 
         if values[0] == 'temperature-to-humidity':
             map = temperature_to_humidity_map
+            map_of_maps[values[0]] = map
             continue
 
         if values[0] == 'humidity-to-location':
             map = humidity_to_location_map
-            continue
-
-        values = l.split()
-        #print(f"DEBUG: values={values}")
-        map.add_adjuster(int(values[0]),int(values[1]),int(values[2]))
-
-
-    soil = seed_to_soil_map.get_value(seed)
-    #print(f"DEBUG: soil={soil}")
-
-    fertilizer = soil_to_fertilizer_map.get_value(soil)
-    #print(f"DEBUG: fertilizer={fertilizer}")
-
-    water = fertilizer_to_water_map.get_value(fertilizer)
-    #print(f"DEBUG: water={water}")
-
-    light = water_to_light_map.get_value(water)
-    #print(f"DEBUG: light={light}")
-
-    temperature = light_to_temperature_map.get_value(light)
-    #print(f"DEBUG: temperature={temperature}")
-
-    humidity = temperature_to_humidity_map.get_value(temperature)
-    #print(f"DEBUG: humidity={humidity}")
-
-    location = humidity_to_location_map.get_value(humidity)
-    #print(f"DEBUG: location={location}")
-
-
-    return location
-
-def get_nearest_location_for_seeds_from_filename(filename):
-    seed_list = get_seed_list_from_filename(filename)
-
-    map_lines = fileutils.get_lines_after_empty_from_file(filename)
-    #print(f"DEBUG: {map_lines}={map_lines}")
-
-    seed_to_soil_map = AdjusterMap()
-    soil_to_fertilizer_map = AdjusterMap()
-    fertilizer_to_water_map = AdjusterMap()
-    water_to_light_map = AdjusterMap()
-    light_to_temperature_map = AdjusterMap()
-    temperature_to_humidity_map = AdjusterMap()
-    humidity_to_location_map = AdjusterMap()
-    
-    
-
-    map = None
-    for l in map_lines:
-        values = l.split()
-
-        if values[0] == 'seed-to-soil':
-            map = seed_to_soil_map
-            continue
-
-        if values[0] == 'soil-to-fertilizer':
-            map = soil_to_fertilizer_map
-            continue
-
-        if values[0] == 'fertilizer-to-water':
-            map = fertilizer_to_water_map
-            continue
-
-        if values[0] == 'water-to-light':
-            map = water_to_light_map
-            continue
-
-        if values[0] == 'light-to-temperature':
-            map = light_to_temperature_map
-            continue
-
-        if values[0] == 'temperature-to-humidity':
-            map = temperature_to_humidity_map
-            continue
-
-        if values[0] == 'humidity-to-location':
-            map = humidity_to_location_map
+            map_of_maps[values[0]] = map
             continue
 
         values = l.split()
@@ -173,35 +103,43 @@ def get_nearest_location_for_seeds_from_filename(filename):
         #for i in range(int(values[2])):
         #    map[int(values[1])+i] = int(values[0])+i
         map.add_adjuster(int(values[0]),int(values[1]),int(values[2]))
+    
+    return map_of_maps
 
-
+def get_seed_to_location_map_from_seed_list(map_of_maps, seed_list):
     seed_to_location_map = defaultdict(int)
 
     for seed in seed_list:
-        soil = seed_to_soil_map.get_value(int(seed))
+        soil = map_of_maps['seed-to-soil'].get_value(int(seed))
         #print(f"DEBUG: soil={soil}")
 
-        fertilizer = soil_to_fertilizer_map.get_value(soil)
+        fertilizer = map_of_maps['soil-to-fertilizer'].get_value(soil)
         #print(f"DEBUG: fertilizer={fertilizer}")
 
-        water = fertilizer_to_water_map.get_value(fertilizer)
+        water = map_of_maps['fertilizer-to-water'].get_value(fertilizer)
         #print(f"DEBUG: water={water}")
 
-        light = water_to_light_map.get_value(water)
+        light = map_of_maps['water-to-light'].get_value(water)
         #print(f"DEBUG: light={light}")
 
-        temperature = light_to_temperature_map.get_value(light)
+        temperature = map_of_maps['light-to-temperature'].get_value(light)
         #print(f"DEBUG: temperature={temperature}")
 
-        humidity = temperature_to_humidity_map.get_value(temperature)
+        humidity = map_of_maps['temperature-to-humidity'].get_value(temperature)
         #print(f"DEBUG: humidity={humidity}")
 
-        location = humidity_to_location_map.get_value(humidity)
+        location = map_of_maps['humidity-to-location'].get_value(humidity)
         #print(f"DEBUG: location={location}")
 
         seed_to_location_map[seed] = int(location)
 
+    return seed_to_location_map
 
+
+def get_nearest_location_for_seeds_from_filename(filename):
+    seed_list = get_seed_list_from_filename(filename)
+    map_of_maps = get_map_of_maps_from_filename(filename)
+    seed_to_location_map = get_seed_to_location_map_from_seed_list(map_of_maps, seed_list)
     return min(seed_to_location_map.values())
 
 def get_seed_list_from_filename(filename):
@@ -226,86 +164,6 @@ def get_seed_list_as_range_pairs_from_filename(filename):
 
 def solve_part2(filename):
     seed_list = get_seed_list_as_range_pairs_from_filename(filename)
-
-    map_lines = fileutils.get_lines_after_empty_from_file(filename)
-    #print(f"DEBUG: {map_lines}={map_lines}")
-
-    seed_to_soil_map = AdjusterMap()
-    soil_to_fertilizer_map = AdjusterMap()
-    fertilizer_to_water_map = AdjusterMap()
-    water_to_light_map = AdjusterMap()
-    light_to_temperature_map = AdjusterMap()
-    temperature_to_humidity_map = AdjusterMap()
-    humidity_to_location_map = AdjusterMap()
-    
-    
-
-    map = None
-    for l in map_lines:
-        values = l.split()
-
-        if values[0] == 'seed-to-soil':
-            map = seed_to_soil_map
-            continue
-
-        if values[0] == 'soil-to-fertilizer':
-            map = soil_to_fertilizer_map
-            continue
-
-        if values[0] == 'fertilizer-to-water':
-            map = fertilizer_to_water_map
-            continue
-
-        if values[0] == 'water-to-light':
-            map = water_to_light_map
-            continue
-
-        if values[0] == 'light-to-temperature':
-            map = light_to_temperature_map
-            continue
-
-        if values[0] == 'temperature-to-humidity':
-            map = temperature_to_humidity_map
-            continue
-
-        if values[0] == 'humidity-to-location':
-            map = humidity_to_location_map
-            continue
-
-        values = l.split()
-        #print(f"DEBUG: values={values}")
-        #for i in range(int(values[2])):
-        #    map[int(values[1])+i] = int(values[0])+i
-        map.add_adjuster(int(values[0]),int(values[1]),int(values[2]))
-
-
-    seed_to_location_map = defaultdict(int)
-
-    for seed in seed_list:
-        soil = seed_to_soil_map.get_value(int(seed))
-        #print(f"DEBUG: soil={soil}")
-
-        fertilizer = soil_to_fertilizer_map.get_value(soil)
-        #print(f"DEBUG: fertilizer={fertilizer}")
-
-        water = fertilizer_to_water_map.get_value(fertilizer)
-        #print(f"DEBUG: water={water}")
-
-        light = water_to_light_map.get_value(water)
-        #print(f"DEBUG: light={light}")
-
-        temperature = light_to_temperature_map.get_value(light)
-        #print(f"DEBUG: temperature={temperature}")
-
-        humidity = temperature_to_humidity_map.get_value(temperature)
-        #print(f"DEBUG: humidity={humidity}")
-
-        location = humidity_to_location_map.get_value(humidity)
-        #print(f"DEBUG: location={location}")
-
-        seed_to_location_map[seed] = int(location)
-
-
+    map_of_maps = get_map_of_maps_from_filename(filename)
+    seed_to_location_map = get_seed_to_location_map_from_seed_list(map_of_maps, seed_list)
     return min(seed_to_location_map.values())
-
-
