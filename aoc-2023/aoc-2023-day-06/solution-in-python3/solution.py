263f53a8aac4c9ba200622afd121badb5a0770d7
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-12-06 14:01:28 +0000

    AOC 2023 Day 06: Refactored - added explanatory comments, notes etc.

--- aoc-2023/aoc-2023-day-06/solution-in-python3/solution.py
+++ aoc-2023/aoc-2023-day-06/solution-in-python3/solution.py
@@ -2,7 +2,7 @@ from helpers import fileutils
 
 from collections import defaultdict
 
-def calculate_wins_part1(time, distance):
+def calculate_wins_using_brute_force_approach(time, distance):
     wins = 0
     for hold in range(time):
         length = hold * (time - hold)
@@ -12,17 +12,27 @@ def calculate_wins_part1(time, distance):
     return wins
 
 
-def calculate_wins_part2(time, distance):
+def calculate_wins_using_optimised_brute_force_approach(time, distance):
     wins = 0
 
+    # Optimise to start win search from (existing max. distance / time) available
     start = int(distance / time )
 
+    # Optimise to only search half the available time (as symmetric results)
     for hold in range(start, 1 + int(time/2)):
         length = hold * (time - hold)
         #print(f"hold={hold} length={length}")
         if length > distance:
             wins += 1
-    wins *= 2
+    
+    # Adjust to cater for fact that only found half the (symmetric) answers
+    wins *= 2 
+
+    # Adjust answer if half-time availble is even
     if 0 == time % 2:        
         wins -= 1
+
     return wins
+
+def calculate_wins_using_binary_search_approach(time, distance):
+    return 0 # TODO
--- aoc-2023/aoc-2023-day-06/solution-in-python3/test_solution.py
+++ aoc-2023/aoc-2023-day-06/solution-in-python3/test_solution.py
@@ -13,11 +13,25 @@ import solution
         pytest.param(75, 1279, 22),
         pytest.param(94, 1216, 63), # 31 * 57 * 22 * 63 = 2449062
         pytest.param(71530, 940200, 71503),
-        #pytest.param(52947594, 426137412791216, 0),
+        #pytest.param(52947594, 426137412791216, 0), # Too slow!
     ],    
 )
-def test_solution(time, distance, combinations):
-    assert solution.calculate_wins_part1(time, distance) == combinations    
+def test_wins_using_brute_force_approach(time, distance, combinations):
+    assert solution.calculate_wins_using_brute_force_approach(time, distance) == combinations    
+
+@pytest.mark.parametrize(
+    "values, expected",
+    [
+        pytest.param([(7, 9), (15, 40), (30, 200)], 288), # 4 * 8 * 9 = 288
+        pytest.param([(52, 426), (94, 1374), (75, 1279), (94, 1216)], 2449062),  # 31 * 57 * 22 * 63 = 2449062
+    ],    
+)
+def test_solution_part1(values, expected):
+    ans = 1
+    for v in values:
+        ans *= solution.calculate_wins_using_optimised_brute_force_approach(v[0], v[1])
+    assert ans == expected
+
 
 @pytest.mark.parametrize(
     "time, distance, combinations",
@@ -25,13 +39,14 @@ def test_solution(time, distance, combinations):
         pytest.param(7, 9, 4),
         pytest.param(15, 40, 8),
         pytest.param(30, 200, 9),
-        pytest.param(52, 426, 31),  # 4 * 8 * 9 = 288
+        pytest.param(52, 426, 31),  
         pytest.param(94, 1374, 57),
         pytest.param(75, 1279, 22),
-        pytest.param(94, 1216, 63), # 31 * 57 * 22 * 63 = 2449062
+        pytest.param(94, 1216, 63),
         pytest.param(71530, 940200, 71503),
         pytest.param(52947594, 426137412791216, 33149631),
     ],    
 )
 def test_solution_part2(time, distance, combinations):
-    assert solution.calculate_wins_part2(time, distance) == combinations    
+    assert solution.calculate_wins_using_optimised_brute_force_approach(time, distance) == combinations    
+
