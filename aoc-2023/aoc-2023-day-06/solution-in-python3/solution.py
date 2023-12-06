afd45910b8c27bd3b2c591b4f2ec5e91fc8a4468
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-12-06 06:03:26 +0000

    AOC 2023 Day 06: Part 2 completion

--- aoc-2023/aoc-2023-day-06/solution-in-python3/solution.py
+++ aoc-2023/aoc-2023-day-06/solution-in-python3/solution.py
@@ -2,11 +2,27 @@ from helpers import fileutils
 
 from collections import defaultdict
 
-def solve(time, distance):
+def calculate_wins_part1(time, distance):
     wins = 0
     for hold in range(time):
         length = hold * (time - hold)
-        print(f"{length}")
+        #print(f"{length}")
         if length > distance:
             wins += 1
+    return wins
+
+
+def calculate_wins_part2(time, distance):
+    wins = 0
+
+    start = int(distance / time )
+
+    for hold in range(start, 1 + int(time/2)):
+        length = hold * (time - hold)
+        #print(f"hold={hold} length={length}")
+        if length > distance:
+            wins += 1
+    wins *= 2
+    if 0 == time % 2:        
+        wins -= 1
     return wins
--- aoc-2023/aoc-2023-day-06/solution-in-python3/test_solution.py
+++ aoc-2023/aoc-2023-day-06/solution-in-python3/test_solution.py
@@ -17,5 +17,21 @@ import solution
     ],    
 )
 def test_solution(time, distance, combinations):
-    assert solution.solve(time, distance) == combinations    
+    assert solution.calculate_wins_part1(time, distance) == combinations    
 
+@pytest.mark.parametrize(
+    "time, distance, combinations",
+    [
+        pytest.param(7, 9, 4),
+        pytest.param(15, 40, 8),
+        pytest.param(30, 200, 9),
+        pytest.param(52, 426, 31),  # 4 * 8 * 9 = 288
+        pytest.param(94, 1374, 57),
+        pytest.param(75, 1279, 22),
+        pytest.param(94, 1216, 63), # 31 * 57 * 22 * 63 = 2449062
+        pytest.param(71530, 940200, 71503),
+        pytest.param(52947594, 426137412791216, 33149631),
+    ],    
+)
+def test_solution_part2(time, distance, combinations):
+    assert solution.calculate_wins_part2(time, distance) == combinations    
