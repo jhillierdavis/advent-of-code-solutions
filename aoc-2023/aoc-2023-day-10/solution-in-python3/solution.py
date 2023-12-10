c25c36c2456458e0cbb07bf65573fa61cab334e1
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-12-10 08:44:59 +0000

    AOC 2023 Day 10: Part1 - work in progress

--- aoc-2023/aoc-2023-day-10/solution-in-python3/solution.py
+++ aoc-2023/aoc-2023-day-10/solution-in-python3/solution.py
@@ -14,22 +14,22 @@ S is the starting position of the animal; there is a pipe on this tile, but your
 """
 
 def can_move_right(current, next):
-    if (current == '-' or current == 'L' or current == 'F') and next in ['-', '7', 'J']: # right -> right, down, up
+    if (current == 'S' or current == '-' or current == 'L' or current == 'F') and next in ['-', '7', 'J']: # right -> right, down, up
         return True
     return False
 
 def can_move_down(current, next):
-    if (current == '|' or current == 'F' or current == '7') and next in ['|', 'L', 'J' ]: # down -> down, right, left
+    if (current == 'S' or current == '|' or current == 'F' or current == '7') and next in ['|', 'L', 'J' ]: # down -> down, right, left
         return True
     return False
 
 def can_move_up(current, next):
-    if (current == '|' or current == 'J' or current == 'L') and next in ['|', 'F' , '7']: # up -> up, right, left
+    if (current == 'S' or  current == '|' or current == 'J' or current == 'L') and next in ['|', 'F' , '7']: # up -> up, right, left
         return True
     return False
 
 def can_move_left(current, next):
-    if (current == '-' or current == 'J' or current == '7') and next in ['-', 'L', 'F']: # left -> left, up, down
+    if (current == 'S' or current == '-' or current == 'J' or current == '7') and next in ['-', 'L', 'F']: # left -> left, up, down
         return True
     return False
 
@@ -44,23 +44,51 @@ def can_move_from_to(current, next):
         return True
     return False
 
+def can_move_from_point_to_point(g, cp, np):
+    cps = g.get_symbol(cp)
+    nps = g.get_symbol(np)
+
+    print(f"DEBUG: cps={cps} nps={nps} cp={cp} nps={nps}")
+
+    if nps == 'S':
+        return True
+
+    if cp.get_x() - np.get_x() == 1:  # Moving left
+        return can_move_left(cps, nps)
+
+    if cp.get_x() - np.get_x() == -1:  # Moving right
+        return can_move_right(cps, nps)
+    
+    if cp.get_y() - np.get_y() == 1:  # Moving up
+        return can_move_up(cps, nps)
+
+    if cp.get_y() - np.get_y() == -1:  # Moving down
+        return can_move_down(cps, nps)
+    
+    return False
+
 
 def get_max_loop_distance(g, sp):
+    sp_symbol = g.get_symbol(sp)
+
     distance_set = set()
     n_set = g.get_cardinal_point_neighbours(sp)
     for n in n_set:
-        symbol = g.get_symbol(sp)
-        if symbol != '.':
+        if can_move_from_point_to_point(g, sp, n):
+            n_symbol = g.get_symbol(n)
+            print(f"DEBUG: Can move: {sp_symbol} -> {n_symbol}")
+
             count = get_count_loop_length(g,sp,n,[])
             distance_set.add((1 + count) / 2)
-    return max(distance_set)
+    return max(distance_set) if len(distance_set) > 0 else 0
 
 def get_count_loop_length(g, fp, cp, path):
     #print(f"DEBUG: current_point={cp} path={path}")
 
     if cp == fp:
         count = len(path)
-        print(f"DEBUG: At finish point after count={count} with path={path}")
+        #print(f"DEBUG: At finish point after count={count}")
+        #print(f"DEBUG: Start to finish path={path}")
         return count
 
     cp_symbol = g.get_symbol(cp)
@@ -75,13 +103,16 @@ def get_count_loop_length(g, fp, cp, path):
         if n in path:
             continue
 
-        n_symbol = g.get_symbol(n)
-        if not can_move_from_to(cp_symbol, n_symbol):
+        if not can_move_from_point_to_point(g, cp, n):
             print(f"DEBUG: Cannot move from current={cp_symbol} to next={n_symbol} !")
             continue
-                
+
+        #print(f"DEBUG: Can move: {cp_symbol} -> {n_symbol}")
+
+        # Store the current path (if not already done)    
         if not cp in path:
             path.append(cp)   
+
         count = get_count_loop_length(g, fp, n, path)  
         count_list.append(count)
         
