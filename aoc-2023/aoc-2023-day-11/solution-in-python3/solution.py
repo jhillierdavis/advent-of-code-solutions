c40743477d7de54fe7c1657747abdf928e49a4d3
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-12-11 07:21:04 +0000

    AOC 2023 Day 11: Part 1 - initial crude implementation

--- aoc-2023/aoc-2023-day-11/solution-in-python3/solution.py
+++ aoc-2023/aoc-2023-day-11/solution-in-python3/solution.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import math
 
 from helpers import fileutils, grid, point
 
@@ -50,8 +51,86 @@ def get_empty_columns_from_filename(filename):
     print(f"DEBUG: empty_cols={empty_cols}")
     return empty_cols
 
+def get_offset_values(orig_values):
+    offset_values = []
+    offset = 0    
+    for entry in orig_values:        
+        offset_values.append(entry + offset)
+        offset += 1
+    return offset_values
+
+
+def get_lines_with_duplicated_rows(lines, target_rows):
+    new_lines = []
+    index = 0
+    for l in lines:
+        new_lines.append(l)
+        if index in target_rows: # Duplicate line
+            new_lines.append(l)
+        index += 1
+    return new_lines
+
+def get_lines_with_duplicated_cols(lines, target_cols):
+    new_lines = []    
+    for l in lines:    
+        nl = ""    
+        for i in range(len(l)):
+            chr = l[i]
+            nl += chr
+            if i in target_cols: # Duplicate
+                nl += chr
+        #print(f"DEBUG: nl={nl} l={l}")                
+        new_lines.append(nl)
+    return new_lines
+
+
+def get_expanded_grid_from_filename(filename):
+    g = get_grid_from_filename(filename)
+
+    lines = grid.grid_to_lines(g)
+    #print(f"DEBUG: grid lines={lines}")
+
+    empty_rows = get_rows_without_galaxies(g)
+    empty_cols = get_columns_without_galaxies(g)
+
+    #offset_rows = get_offset_values(empty_rows)
+    #print(f"DEBUG: offset_rows={offset_rows}")
+
+    new_lines = get_lines_with_duplicated_rows(lines, empty_rows)
+    assert len(new_lines) == len(lines) + len(empty_rows)
+
+    new_lines = get_lines_with_duplicated_cols(new_lines, empty_cols)        
+    #print(f"DEBUG: new_lines={new_lines}")
+
+    return grid.lines_to_grid(new_lines)
+
+
+def get_galaxy_locations(g):
+    locations = []
+    for x in range(g.get_width()):
+        for y in range(g.get_height()):
+            p = point.Point2D(x,y)
+            if g.get_symbol(p) == '#':
+                locations.append(p)
+    return locations
+    
+def get_galaxy_pair_shortest_distances(g):
+    locations = get_galaxy_locations(g)
+    distances = []
+
+    for lp1 in locations:
+        for lp2 in locations:
+            if lp1 == lp2:
+                break
+            distance = abs(lp1.get_x() - lp2.get_x()) + abs(lp1.get_y() - lp2.get_y())
+            print(f"Galaxy location={lp1} distance={distance}")
+            distances.append(distance)
+    return distances
+
 
 def solve(filename):
-    g = get_grid_from_filename(filename)
     
-    return 0 # TODO
+    g = get_expanded_grid_from_filename(filename)
+    
+    distances = get_galaxy_pair_shortest_distances(g)
+    return sum(distances)
--- aoc-2023/aoc-2023-day-11/solution-in-python3/test_solution.py
+++ aoc-2023/aoc-2023-day-11/solution-in-python3/test_solution.py
@@ -1,5 +1,7 @@
 import pytest
 
+from helpers import grid
+
 import solution
 
 @pytest.mark.parametrize(
@@ -11,6 +13,7 @@ import solution
 def test_get_empty_rows_from_filename(filename, expected):
     assert solution.get_empty_rows_from_filename(filename) == expected
 
+
 @pytest.mark.parametrize(
     "filename,expected",
     [
@@ -21,11 +24,43 @@ def test_get_empty_columns_from_filename(filename, expected):
     assert solution.get_empty_columns_from_filename(filename) == expected
 
 
+@pytest.mark.parametrize(
+    "filename,expected",
+    [
+        pytest.param("puzzle-input-example.txt", "puzzle-input-example-expanded.txt"),
+    ],    
+)
+def test_matches_expanded(filename, expected):
+    g_orig = solution.get_grid_from_filename(filename)
+    g_exp = solution.get_grid_from_filename(expected)
+    assert grid.grid_to_lines(g_orig) != grid.grid_to_lines(g_exp)
+
+    g_actual = solution.get_expanded_grid_from_filename(filename)
+
+    # TODO: Implement grid equality!
+    assert grid.grid_to_lines(g_actual) != grid.grid_to_lines(g_orig)
+    assert grid.grid_to_lines(g_actual) == grid.grid_to_lines(g_exp)
+
 
 @pytest.mark.parametrize(
     "filename,expected",
     [
-        pytest.param("puzzle-input-example.txt", -1),
+        pytest.param("puzzle-input-example.txt", 9),
+    ],    
+)
+def test_solution(filename, expected):
+    g = solution.get_expanded_grid_from_filename(filename)
+
+    locations = solution.get_galaxy_locations(g)
+
+    assert len(locations) == expected
+
+
+@pytest.mark.parametrize(
+    "filename,expected",
+    [
+        pytest.param("puzzle-input-example.txt", 374),
+        pytest.param("puzzle-input-full.txt", 0), # 9693756
     ],    
 )
 def test_solution(filename, expected):
