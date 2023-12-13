43eed90454245e3b73fcb7e6a23725692a56fe1c
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-12-13 06:31:52 +0000

    AOC 2023 Day 13: Parts 1 & 2 - initial crude solution

--- aoc-2023/aoc-2023-day-13/solution-in-python3/solution.py
+++ aoc-2023/aoc-2023-day-13/solution-in-python3/solution.py
@@ -1,7 +1,90 @@
 from collections import defaultdict
 
-from helpers import fileutils
+from helpers import fileutils, grid, point
 
-def solve(filename):
+def get_grids_from(filename):
     lines = fileutils.get_file_lines(filename)
-    return 0 # TODO
+
+    grids  = []
+
+    glines =[]
+    for l in lines:
+        if len(l.strip()) == 0 :
+            if len(glines) > 0:
+                g = grid.lines_to_grid(glines)
+                grids.append(g)
+                glines.clear()
+            continue
+        glines.append(l)
+
+    if len(glines) > 0:
+        g = grid.lines_to_grid(glines)
+        grids.append(g)
+        glines.clear()
+
+    return grids
+
+def get_symmetry_row(g, target_mismatch:int=0):
+    height = g.get_height()
+    width = g.get_width()
+
+    for row in range(height-1):
+        mismatch = 0
+        for offset in range(height):
+            left = row - offset
+            right = row + 1 + offset
+
+            if left >= 0 and right < height:
+                # Check similarity of row (point by point)
+                for rp in range(width):
+                    if g.get_symbol(point.Point2D(rp, left)) != g.get_symbol(point.Point2D(rp, right)):
+                        mismatch += 1
+
+        if mismatch == target_mismatch:
+            location = row + 1
+            print(f"DEBUG: Vertical symmetry found at {location}")
+            return location
+        
+    return 0 # Symmetry mot found
+
+def get_symmetry_column(g, target_mismatch:int=0):
+    height = g.get_height()
+    width = g.get_width()
+
+    for column in range(width-1):
+        mismatch = 0
+        for offset in range(width):
+            top = column - offset
+            bottom = column + 1 + offset
+
+            if top >= 0 and bottom < width:
+                # Check similarity of column (point by point)
+                for cp in range(height):
+                    if g.get_symbol(point.Point2D(top, cp)) != g.get_symbol(point.Point2D(bottom, cp)):
+                        mismatch += 1
+
+        if mismatch == target_mismatch:
+            location = column + 1
+            print(f"DEBUG: Horizontal symmetry found at {location}")
+            return location
+        
+    return 0  # Symmetry mot found 
+
+
+def solve(filename, target_mismatch:int=0):
+    grids = get_grids_from(filename)
+
+    total = 0
+    for g in grids:
+        #print(f"DEBUG: Finding relection point for grid")
+        total += get_symmetry_column(g, target_mismatch)
+        total += get_symmetry_row(g, target_mismatch) * 100        
+
+    return total
+
+
+def solve_part1(filename):
+    return solve(filename)
+
+def solve_part2(filename):
+    return solve(filename, 1)
--- aoc-2023/aoc-2023-day-13/solution-in-python3/test_solution.py
+++ aoc-2023/aoc-2023-day-13/solution-in-python3/test_solution.py
@@ -5,8 +5,33 @@ import solution
 @pytest.mark.parametrize(
     "filename,expected",
     [
-        pytest.param("puzzle-input-example.txt", -1),
+        pytest.param("puzzle-input-example.txt", 2),
+        pytest.param("puzzle-input-full.txt", 100),
     ],    
 )
-def test_solution(filename, expected):
-    assert solution.solve(filename) == expected
+def test_get_grids_from(filename, expected):
+    grids = solution.get_grids_from(filename)
+
+    assert len(grids) == expected
+
+
+@pytest.mark.parametrize(
+    "filename,expected",
+    [
+        pytest.param("puzzle-input-example.txt", 405),
+        pytest.param("puzzle-input-full.txt", 30487),
+    ],    
+)
+def test_solve_part1(filename, expected):
+    assert solution.solve_part1(filename) == expected
+
+
+@pytest.mark.parametrize(
+    "filename,expected",
+    [
+        pytest.param("puzzle-input-example.txt", 400),
+        pytest.param("puzzle-input-full.txt", 31954),
+    ],    
+)
+def test_solve_part2(filename, expected):
+    assert solution.solve_part2(filename) == expected    
