43526da8e51be6d922b6eea2224f38adedf47732
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-12-14 12:23:27 +0000

    AOC 2023 Day 14: Part 2 - work in progress

--- aoc-2023/aoc-2023-day-14/solution-in-python3/solution.py
+++ aoc-2023/aoc-2023-day-14/solution-in-python3/solution.py
@@ -26,7 +26,7 @@ def calculate_load_from(filename):
     return calculate_load_to_north(g)
 
 def shift_rounded_rocks_north(g):
-
+    count = 0
     for col in range(1, g.get_height()):
         for row in range(g.get_width()):
             p = point.Point2D(row, col)            
@@ -37,14 +37,115 @@ def shift_rounded_rocks_north(g):
                 if s_north == '.':
                     g.set_symbol(p_north, 'O')
                     g.set_symbol(p, '.')
-    #print()
-    #grid.display_grid(g)
-    #print()
-            
+                    count += 1
+    return count
+
+def shift_rounded_rocks_south(g):
+    count = 0
+    for col in range(g.get_height()-1):
+        for row in range(g.get_width()):
+            p = point.Point2D(row, col)            
+            s = g.get_symbol(p)
+            if s == 'O':
+                p_north = point.Point2D(row, col+1)
+                s_north =  g.get_symbol(p_north)
+                if s_north == '.':
+                    g.set_symbol(p_north, 'O')
+                    g.set_symbol(p, '.')
+                    count += 1
+    return count
+
+def shift_rounded_rocks_west(g):
+    count = 0
+    for col in range(g.get_height()):
+        for row in range(1, g.get_width()):
+            p = point.Point2D(row, col)            
+            s = g.get_symbol(p)
+            if s == 'O':
+                p_next = point.Point2D(row-1, col)
+                s_next =  g.get_symbol(p_next)
+                if s_next == '.':
+                    g.set_symbol(p_next, 'O')
+                    g.set_symbol(p, '.')
+                    count += 1
+    return count
+
+def shift_rounded_rocks_east(g):
+    count = 0
+    for col in range(g.get_height()):
+        for row in range(g.get_width()-1):
+            p = point.Point2D(row, col)            
+            s = g.get_symbol(p)
+            if s == 'O':
+                p_next = point.Point2D(row+1, col)
+                s_next =  g.get_symbol(p_next)
+                if s_next == '.':
+                    g.set_symbol(p_next, 'O')
+                    g.set_symbol(p, '.')
+                    count += 1
+    return count
 
 def solve_part1(filename):
     lines  = fileutils.get_file_lines(filename)
     g = grid.lines_to_grid(lines)
-    for i in range(g.get_height()):
+    for i in range(g.get_height()-1):
         shift_rounded_rocks_north(g)     
+    return calculate_load_to_north(g)
+
+def get_repeating_sequence_in(L):
+    '''Reduce the input list to a list of all repeated integers in the list.'''
+    return [item for item in list(set(L)) if L.count(item) > 1]
+
+
+def is_repeating_sequence_in(list_of_numbers):
+    repeated_sequence = get_repeating_sequence_in(list_of_numbers)
+    if len(repeated_sequence) > 0:
+        print(f"DEBUG: repeated_set={repeated_sequence}")
+        return True
+    return False
+
+def solve_part2(filename):
+    lines  = fileutils.get_file_lines(filename)
+    g = grid.lines_to_grid(lines)
+    
+
+    counts = []
+    max = 1000000000
+    found_repeating_sequence = False
+    spin = 0
+    while spin < max:
+        spin += 1
+        count = 0
+        for i in range(g.get_height()-1):
+            count += shift_rounded_rocks_north(g)     
+
+        for i in range(g.get_width()-1):
+            count += shift_rounded_rocks_west(g)     
+
+        for i in range(g.get_height()-1):
+            count += shift_rounded_rocks_south(g)     
+
+        for i in range(g.get_width()-1):
+            count += shift_rounded_rocks_east(g)     
+
+        #print(f"DEBUG: spin={spin} count={count}")
+        if not found_repeating_sequence:
+            repeating_sequence = get_repeating_sequence_in(counts)
+            repeating_sequence_length = len(repeating_sequence)
+        
+            if spin > 200 and repeating_sequence_length > 1:
+                load = calculate_load_to_north(g)
+                remainder = max - spin
+                offset = remainder % repeating_sequence_length
+                print(f"DEBUG: cycle at spin: {spin} load={load} repeating_sequence_length={repeating_sequence_length} repeating_sequence={repeating_sequence} remainder={remainder} offset={offset}")
+                spin = max - offset
+                print(f"DEBUG: fast forward to spin={spin}!")
+                found_repeating_sequence = True
+
+            counts.append(count)
+        else:
+            load = calculate_load_to_north(g)
+            print(f"DEBUG: spin={spin} load={load}")
+
+
     return calculate_load_to_north(g)
--- aoc-2023/aoc-2023-day-14/solution-in-python3/test_solution.py
+++ aoc-2023/aoc-2023-day-14/solution-in-python3/test_solution.py
@@ -21,12 +21,37 @@ def test_count_rounded_rocks_from(filename, expected):
 def test_calculate_load_from(filename, expected):
     assert solution.calculate_load_from(filename) == expected    
 
+
 @pytest.mark.parametrize(
     "filename,expected",
     [
         pytest.param("puzzle-input-example.txt", 136),
-        pytest.param("puzzle-input-full.txt", 113456),
+        #pytest.param("puzzle-input-full.txt", 113456),
     ],    
 )
 def test_solve_part1(filename, expected):
-    assert solution.solve_part1(filename) == expected        
+    assert solution.solve_part1(filename) == expected  
+
+
+@pytest.mark.parametrize(
+    "list_of_numbers,expected",
+    [
+        pytest.param([1,2,3], False),
+        pytest.param([1,2,3,1,2,3], True),
+        pytest.param([33841, 35099, 34910, 34913, 34817, 34739, 34641, 34474, 34443, 34417, 34416, 34320, 34276, 34180, 34137, 34093, 34059, 33952, 34012, 33931, 33826, 33809, 33733, 33671, 33655, 33564, 33503, 33514, 33478, 33491, 33540, 33414, 33397, 33393, 33318, 33237, 33287, 33280, 33243, 33261, 33283, 33255, 33216, 33215, 33209, 33242, 33235, 33157, 33154, 33121, 33083, 33051, 33045, 32993, 32939, 32918, 32875, 32788, 32833, 32818, 32776, 32771, 32779, 32749, 32719, 32692, 32616, 32613, 32629, 32634, 32625, 32642, 32556, 32509, 32537, 32497, 32491, 32462, 32456, 32423, 32438, 32448, 32475, 32498, 32476, 32509, 32497, 32475, 32495, 32503, 32496, 32500, 32521, 32501, 32506, 32477, 32471, 32495, 32504, 32495, 32513, 32521, 32484, 32500, 32487, 32474, 32489, 32520, 32495, 32496, 32515, 32494, 32503, 32481, 32490, 32489, 32503, 32489, 32506, 32518, 32488, 32519, 32481, 32473, 32483, 32513, 32492, 32500, 32534, 32488, 32502, 32475, 32483, 32486, 32507, 32508, 32500, 32517, 32482, 32512, 32478, 32477, 32502, 32507, 32491, 32494, 32527, 32485, 32506, 32494, 32477, 32485, 32501], True)
+    ],    
+)
+def test_is_repeating_sequence_in(list_of_numbers, expected):
+    assert solution.is_repeating_sequence_in(list_of_numbers) == expected
+
+
+
+@pytest.mark.parametrize(
+    "filename,expected",
+    [
+        pytest.param("puzzle-input-example.txt", 64),
+        pytest.param("puzzle-input-full.txt", 0), # < 118755
+    ],    
+)
+def test_solve_part1(filename, expected):
+    assert solution.solve_part2(filename) == expected
