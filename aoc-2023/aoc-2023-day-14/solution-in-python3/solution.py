245fc8d59a11a3ab809216b4b5bbde573172c4fb
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-12-14 06:48:41 +0000

    AOC 2023 Day 14: Part 1 - initial crude solution

--- aoc-2023/aoc-2023-day-14/solution-in-python3/solution.py
+++ aoc-2023/aoc-2023-day-14/solution-in-python3/solution.py
@@ -1,7 +1,50 @@
 from collections import defaultdict
 
-from helpers import fileutils
+from helpers import fileutils, grid, point
 
-def solve(filename):
-    lines = fileutils.get_file_lines(filename)
-    return 0 # TODO
+
+
+def count_rounded_rocks_from(filename):
+    lines  = fileutils.get_file_lines(filename)
+    g = grid.lines_to_grid(lines)
+
+    return g.count_symbol('O')
+
+def calculate_load_to_north(g):
+    g = g.get_inverted_vertically()
+    #grid.display_grid(g)
+
+    total_load = 0
+    coords = g.get_matching_symbol_coords('O')
+    for c in  coords:
+        total_load += 1 + c.get_y()
+    return total_load
+
+def calculate_load_from(filename):
+    lines  = fileutils.get_file_lines(filename)
+    g = grid.lines_to_grid(lines)
+    return calculate_load_to_north(g)
+
+def shift_rounded_rocks_north(g):
+
+    for col in range(1, g.get_height()):
+        for row in range(g.get_width()):
+            p = point.Point2D(row, col)            
+            s = g.get_symbol(p)
+            if s == 'O':
+                p_north = point.Point2D(row, col-1)
+                s_north =  g.get_symbol(p_north)
+                if s_north == '.':
+                    g.set_symbol(p_north, 'O')
+                    g.set_symbol(p, '.')
+    #print()
+    #grid.display_grid(g)
+    #print()
+            
+
+def solve_part1(filename):
+    lines  = fileutils.get_file_lines(filename)
+    g = grid.lines_to_grid(lines)
+    for i in range(g.get_height()):
+        shift_rounded_rocks_north(g)     
+    return calculate_load_to_north(g)
--- aoc-2023/aoc-2023-day-14/solution-in-python3/test_solution.py
+++ aoc-2023/aoc-2023-day-14/solution-in-python3/test_solution.py
@@ -5,8 +5,28 @@ import solution
 @pytest.mark.parametrize(
     "filename,expected",
     [
-        pytest.param("puzzle-input-example.txt", -1),
+        pytest.param("puzzle-input-example-slid-north.txt", 18),
     ],    
 )
-def test_solution(filename, expected):
-    assert solution.solve(filename) == expected
+def test_count_rounded_rocks_from(filename, expected):
+    assert solution.count_rounded_rocks_from(filename) == expected
+
+
+@pytest.mark.parametrize(
+    "filename,expected",
+    [
+        pytest.param("puzzle-input-example-slid-north.txt", 136),
+    ],    
+)
+def test_calculate_load_from(filename, expected):
+    assert solution.calculate_load_from(filename) == expected    
+
+@pytest.mark.parametrize(
+    "filename,expected",
+    [
+        pytest.param("puzzle-input-example.txt", 136),
+        pytest.param("puzzle-input-full.txt", 113456),
+    ],    
+)
+def test_solve_part1(filename, expected):
+    assert solution.solve_part1(filename) == expected        
