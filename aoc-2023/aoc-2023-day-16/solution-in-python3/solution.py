51795432d36a552fd891ff691e458280a8c30d14
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-12-15 16:20:54 +0000

    AOC 2023 Day 16: Initial file set updated

--- aoc-2023/aoc-2023-day-16/solution-in-python3/solution.py
+++ aoc-2023/aoc-2023-day-16/solution-in-python3/solution.py
@@ -2,6 +2,11 @@ from collections import defaultdict
 
 from helpers import fileutils
 
-def solve(filename):
+def solve_part1(filename):
     lines = fileutils.get_file_lines(filename)
     return 0 # TODO
+
+def solve_part2(filename):
+    lines = fileutils.get_file_lines(filename)
+    return 0 # TODO
+
--- aoc-2023/aoc-2023-day-16/solution-in-python3/test_solution.py
+++ aoc-2023/aoc-2023-day-16/solution-in-python3/test_solution.py
@@ -6,7 +6,18 @@ import solution
     "filename,expected",
     [
         pytest.param("puzzle-input-example.txt", -1),
+        #pytest.param("puzzle-input-full.txt", -1),
     ],    
 )
-def test_solution(filename, expected):
-    assert solution.solve(filename) == expected
+def test_solve_part1(filename, expected):
+    assert solution.solve_part1(filename) == expected
+
+@pytest.mark.parametrize(
+    "filename,expected",
+    [
+        #pytest.param("puzzle-input-example.txt", -1),
+        #pytest.param("puzzle-input-full.txt", -1),
+    ],    
+)
+def test_solve_part2(filename, expected):
+    assert solution.solve_part2(filename) == expected    
