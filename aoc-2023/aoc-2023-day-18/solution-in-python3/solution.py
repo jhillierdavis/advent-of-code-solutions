3aecd96b12dd2318ff806031a8ac5d1e1685eb0e
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-12-17 20:51:43 +0000

    AOC 2023 Day 18: Initial file set

---
+++ aoc-2023/aoc-2023-day-18/solution-in-python3/solution.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+from helpers import fileutils
+
+def solve_part1(filename):
+    lines = fileutils.get_file_lines(filename)
+    return 0 # TODO
+
+def solve_part2(filename):
+    lines = fileutils.get_file_lines(filename)
+    return 0 # TODO
+
