95b10dc3de766c7ca36f3702163f369184c83860
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-12-19 06:09:49 +0000

    AOC Day 19: Part 1 - initial crude implementation

--- aoc-2023/aoc-2023-day-19/solution-in-python3/solution.py
+++ aoc-2023/aoc-2023-day-19/solution-in-python3/solution.py
@@ -1,10 +1,92 @@
-#from collections import defaultdict
+from collections import defaultdict
 
 from helpers import fileutils
 
+
+def get_workflow_mappings_from(filename):
+    workflow_mapping = {}
+
+    workflow_lines = fileutils.get_lines_before_empty_from_file(filename)
+
+    for wl in workflow_lines:
+        
+        name, instructions = wl.split('{')
+        #print(f"DEBUG: name={name} instructions={instructions}")
+        instruction_list = instructions[:-1].split(',')
+        workflow_mapping[name] = instruction_list
+    
+    print(f"DEBUG: workflow_mapping={workflow_mapping}")
+    return workflow_mapping
+
+def get_part_mapping_from(part_input):
+    part_mapping = defaultdict(int)
+
+    s = part_input[1:-1] # Remove parenthesis
+    ratings = s.split(',')
+    for r in ratings:
+        category, value = r.split("=")
+        part_mapping[category] = int(value)
+
+    print(f"DEBUG: part_mapping={part_mapping}")
+    return part_mapping
+
+def process_next_workflow_instruction(workflow_mapping, part_mapping, next):
+    instructions = workflow_mapping[next]
+    for ins in instructions:
+        if '>' in ins:
+            w,d = ins.split(':')
+            category,value = w.split('>')
+            value = int(value)
+            if part_mapping[category] > value:
+                next = d
+                break                
+        elif '<' in ins:
+            w,d = ins.split(':')
+            category,value = w.split('<')
+            value = int(value)
+            if part_mapping[category] < value:
+                next = d
+                break
+        else:
+            next = ins
+            break
+        
+    #print(f"DEBUG: next={next}")
+    return next
+
+
+def workflow_processing_for_part(workflow_mappings, part_input):
+    part_mapping = get_part_mapping_from(part_input)
+    return workflow_processing_for_part_mapping(workflow_mappings, part_mapping)
+
+
+def workflow_processing_for_part_mapping(workflow_mappings, part_mapping):
+    next = 'in'
+    while 'R' != next and 'A' != next:
+        next = process_next_workflow_instruction(workflow_mappings, part_mapping, next)
+    return next
+
+
 def solve_part1(filename):
-    lines = fileutils.get_file_lines(filename)
-    return 0 # TODO
+    workflow_mappings = get_workflow_mappings_from(filename)
+
+    part_lines = fileutils.get_lines_after_empty_from_file(filename)
+
+    sum = 0
+    for pl in part_lines:
+        x = 0; m = 0; a = 0; s = 0
+        print(f"DEBUG: pl={pl}")
+        part_mapping = get_part_mapping_from(pl)
+        if 'A' == workflow_processing_for_part_mapping(workflow_mappings, part_mapping):
+            x = part_mapping['x']
+            m = part_mapping['m']
+            a = part_mapping['a']
+            s = part_mapping['s']
+            subtotal = x + m + a + s
+            print(f"DEBUG: subtotal={subtotal} x={x} m={m} a={a} s={s}")
+            sum += subtotal
+
+    return sum
 
 def solve_part2(filename):
     lines = fileutils.get_file_lines(filename)
--- aoc-2023/aoc-2023-day-19/solution-in-python3/test_solution.py
+++ aoc-2023/aoc-2023-day-19/solution-in-python3/test_solution.py
@@ -3,10 +3,27 @@ import pytest
 import solution
 
 @pytest.mark.parametrize(
+    "filename,part,expected",
+    [
+        pytest.param("puzzle-input-example.txt","{x=787,m=2655,a=1222,s=2876}", 'A'),
+        pytest.param("puzzle-input-example.txt","{x=1679,m=44,a=2067,s=496}", 'R'),
+        pytest.param("puzzle-input-example.txt","{x=2036,m=264,a=79,s=2244}", 'A'),
+        pytest.param("puzzle-input-example.txt","{x=2461,m=1339,a=466,s=291}", 'R'),
+        pytest.param("puzzle-input-example.txt","{x=2127,m=1623,a=2188,s=1013}", 'A'),        
+        #pytest.param("puzzle-input-full.txt", -1),
+    ],    
+)
+def test_workflow_processing_for_part(filename, part, expected):
+    workflow_mapping = solution.get_workflow_mappings_from(filename)
+
+    assert solution.workflow_processing_for_part(workflow_mapping, part) == expected
+
+
+@pytest.mark.parametrize(
     "filename,expected",
     [
-        pytest.param("puzzle-input-example.txt", -1),
-        #pytest.param("puzzle-input-full.txt", -1),
+        pytest.param("puzzle-input-example.txt", 19114),
+        pytest.param("puzzle-input-full.txt", -1),
     ],    
 )
 def test_solve_part1(filename, expected):
