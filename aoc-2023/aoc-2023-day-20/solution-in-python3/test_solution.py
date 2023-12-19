0cbb3e8212240be24d137a95867ad37add61b59a
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-12-19 18:19:31 +0000

    AOC 2023 Day 20: Initial file set

---
+++ aoc-2023/aoc-2023-day-20/solution-in-python3/test_solution.py
@@ -0,0 +1,23 @@
+import pytest
+
+import solution
+
+@pytest.mark.parametrize(
+    "filename,expected",
+    [
+        pytest.param("puzzle-input-example.txt", -1),
+        #pytest.param("puzzle-input-full.txt", -1),
+    ],    
+)
+def test_solve_part1(filename, expected):
+    assert solution.solve_part1(filename) == expected
+
+@pytest.mark.parametrize(
+    "filename,expected",
+    [
+        #pytest.param("puzzle-input-example.txt", -1),
+        #pytest.param("puzzle-input-full.txt", -1),
+    ],    
+)
+def test_solve_part2(filename, expected):
+    assert solution.solve_part2(filename) == expected    
