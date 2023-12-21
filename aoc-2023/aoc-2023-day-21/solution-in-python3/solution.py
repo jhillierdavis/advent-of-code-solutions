632408dff83716c6834ce8510af6e3d1d03ce741
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-12-21 06:25:05 +0000

    AOC 2023 Day 21: Part 1 - initial crude implementation

--- aoc-2023/aoc-2023-day-21/solution-in-python3/solution.py
+++ aoc-2023/aoc-2023-day-21/solution-in-python3/solution.py
@@ -1,10 +1,66 @@
-#from collections import defaultdict
+from collections import defaultdict, deque
 
-from helpers import fileutils
+from helpers import fileutils, grid
 
-def solve_part1(filename):
+
+
+def count_neighbours(g, cp):
+    #print(f"DEBUG: cp={cp}")
+    neighbours = g.get_cardinal_point_neighbours(cp)
+    #print(f"DEBUG: neighbours={neighbours}")
+
+    count = 0
+    for n in neighbours:
+        if g.get_symbol(n) != '#':
+            count += 1
+    return count
+
+def get_cardinal_point_neighbours_at_distance(g, cp, d, coords, visited):
+    #print(f"DEBUG: d={d} {cp}")
+    if d == 0:
+        coords.add(cp)
+        return
+    
+    neighbours = g.get_cardinal_point_neighbours(cp)
+    for n in neighbours:
+        if (n,d) in visited:
+            continue
+        else:
+            visited.add((n,d))
+
+        if g.get_symbol(n) != '#':    
+            #coords.add(n)        
+            get_cardinal_point_neighbours_at_distance(g, n, d -1, coords, visited)            
+    return
+        
+    
+
+
+def solve_part1(filename, steps):
     lines = fileutils.get_file_lines(filename)
-    return 0 # TODO
+
+    g = grid.lines_to_grid(lines)
+
+    #grid.display_grid(g)
+
+    matches = g.get_matching_symbol_coords('S')
+    cp = matches[0]
+#    print(f"DEBUG: cp={cp}")
+#    neighbours = g.get_cardinal_point_neighbours(cp)
+#    print(f"DEBUG: neighbours={neighbours}")
+
+ 
+#    for n in neighbours:
+#        if g.get_symbol(n) != '#':
+#            count += 1
+    
+    coords = set()
+    visited = set()
+    get_cardinal_point_neighbours_at_distance(g, cp, steps, coords, visited)
+    print(f"DEBUG: coords={coords}")
+    count = len(coords)
+    return count
+
 
 def solve_part2(filename):
     lines = fileutils.get_file_lines(filename)
--- aoc-2023/aoc-2023-day-21/solution-in-python3/test_solution.py
+++ aoc-2023/aoc-2023-day-21/solution-in-python3/test_solution.py
@@ -3,14 +3,17 @@ import pytest
 import solution
 
 @pytest.mark.parametrize(
-    "filename,expected",
+    "filename,steps,expected",
     [
-        pytest.param("puzzle-input-example.txt", -1),
-        #pytest.param("puzzle-input-full.txt", -1),
+        pytest.param("puzzle-input-example.txt", 1, 2),
+        pytest.param("puzzle-input-example.txt", 2, 4),
+        #pytest.param("puzzle-input-example.txt", 3, 6),
+        pytest.param("puzzle-input-example.txt", 6, 16),
+        pytest.param("puzzle-input-full.txt", 64, -1), # 241 too low
     ],    
 )
-def test_solve_part1(filename, expected):
-    assert solution.solve_part1(filename) == expected
+def test_solve_part1(filename, steps, expected):
+    assert solution.solve_part1(filename, steps) == expected
 
 @pytest.mark.parametrize(
     "filename,expected",
