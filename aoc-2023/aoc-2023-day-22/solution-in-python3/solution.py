40dc2f27e44ad2c088b6eb0d52774d664f34f09e
jhillierdavis <jhillierdavis@users.noreply.github.com>
2023-12-22 12:16:43 +0000

    AOC 2023 Day 22: Part 1 - work in progress

--- aoc-2023/aoc-2023-day-22/solution-in-python3/solution.py
+++ aoc-2023/aoc-2023-day-22/solution-in-python3/solution.py
@@ -1,10 +1,174 @@
 #from collections import defaultdict
 
-from helpers import fileutils
+from helpers import fileutils, strutils
+
+class Block():
+    def __init__(self, begin:(int,int,int), end:(int,int,int)):
+        self.begin = begin
+        self.end=end
+
+    def is_grounded(self) -> bool:
+        return True if self.begin[2] == 1 or self.end[2] == 1 else False
+    
+    """
+    def is_supporting(self, other) -> bool:
+        if 1 != other.begin[2] - self.end[2]:
+            print(f"DEBUG: Not directly above: {self} {other}")
+            return False
+
+        is_x_overlap = (other.begin[0] <= self.begin[0] and other.begin[0] <= self.end[0]) or (other.end[0] >= self.begin[0] and other.end[0] >= self.end[0])
+        is_y_overlap = (other.begin[1] >= self.begin[1] and other.begin[1] <= self.end[1]) or (other.end[1] >= self.begin[1] and other.end[1] <= self.end[1])
+        print(f"DEBUG: Overlap x {is_x_overlap}: y {is_y_overlap} for {self} {other}")
+
+        return True if is_x_overlap and is_y_overlap else False
+    """
+
+    def is_supporting(self, other) -> bool:
+        cloned_other = other.clone()
+        cloned_other.decrement_z()
+
+        cubes_self = self.get_set_of_cubes()
+        cubes_cloned_other = cloned_other.get_set_of_cubes()
+
+        intersection = cubes_self.intersection(cubes_cloned_other)
+        #print(f"DEBUG: {intersection} {self} {cloned_other} {other}")
+        return len(intersection) > 0
+    
+    def is_neighbour(self, other) -> bool:
+        if self.begin[2] == other.begin[2] and self.end[2] == other.end[2]:
+            return True
+        return False
+
+
+    def get_set_of_cubes(self):
+        cubes = set()
+
+        if self.begin[0] == self.end[0] and self.begin[1] == self.end[1]:
+            for z in range(self.begin[2], 1 + self.begin[2]):
+                cubes.add((self.begin[0], self.end[1], z))
+        elif self.begin[0] == self.end[0] and self.begin[2] == self.end[2]:
+            for y in range(self.begin[1], 1 + self.end[1]):
+                cubes.add((self.begin[0], y, self.begin[2]))
+        elif self.begin[1] == self.end[1] and self.begin[2] == self.end[2]:
+            for x in range(self.begin[0], 1 + self.end[0]):
+                cubes.add((x, self.begin[1], self.begin[2]))
+        else:
+            assert False
+
+        return cubes
+        
+
+    
+    def is_supported_by(self, other) -> bool:
+        return other.is_supporting(self)
+
+    def is_supported_by_any_of(self, other_blocks) -> bool:
+        for other in other_blocks:
+            if self == other:
+                continue
+            if self.is_supported_by(other):
+                return True
+        return False
+
+
+    def decrement_z(self):
+        self.begin = (self.begin[0], self.begin[1], self.begin[2] -1)
+        self.end = (self.end[0], self.end[1], self.end[2] -1)
+    
+    def __str__(self) -> str:
+        return f"Block(id={id(self)} , begin: {self.begin}, end: {self.end})"
+    
+    def __repr__(self) -> str:
+        return str(self)
+    
+    def __eq__(self,other) -> bool:
+        return self.begin == other.begin and self.end == other.end
+    
+    def __hash__(self):
+        return hash(self.begin * 17) + hash(self.end * 19)
+    
+    def __lt__(self, other) -> bool:
+        # E.g. for Heapq: to handle "Handle duplicate point values -> TypeError: '<' not supported between instances of 'Point2D' and 'Point2D'"
+        return self.begin < other.begin or self.end < other.end
+    
+    def clone(self):
+        cloned_block = Block((self.begin[0], self.begin[1], self.begin[2]), (self.end[0], self.end[1], self.end[2]))
+        return cloned_block
+
+
+def count_supporting(blocks, cb):
+    count = 0
+    for ob in blocks:
+        if ob == cb:
+            continue
+
+        if cb.is_supporting(ob):
+            count += 1
+
+    return count
+
+def count_neighbours(blocks, cb):
+    count = 0
+    for ob in blocks:
+        if ob == cb:
+            continue
+
+        if cb.is_neighbour(ob):
+            count += 1
+
+    return count
+
+
+def can_fall(blocks:[Block], cb:Block):
+    if cb.is_grounded():
+        return False
+    
+    count = count_supporting(blocks, cb)
+    print(f"DEBUG: Supporting count={count} for {cb}")
+    return False if count == 1 else True
+
+def get_blocks_from(filename):
+    lines = fileutils.get_file_lines(filename)
+
+    blocks = []
+    for l in lines:
+        #print(f"DEBUG: l={l}")
+        p1,p2 = l.split('~')
+        begin = strutils.string_to_int_list(p1, ',')
+        end = strutils.string_to_int_list(p2, ',')
+        b = Block(begin, end)
+        #print(f"DEBUG: block={b} ")
+        blocks.append(b) 
+    return blocks
 
 def solve_part1(filename):
-    lines = fileutils.get_file_lines(filename)
-    return 0 # TODO
+    blocks = get_blocks_from(filename)
+    print(f"DEBUG: Initial blocks={blocks} ")
+
+    for b in blocks:
+        if b.is_grounded():
+            continue
+
+        while not (b.is_grounded() or b.is_supported_by_any_of(blocks)):
+            print(f"DEBUG: Moving down: {b}")
+            b.decrement_z()
+
+    print(f"DEBUG: Final blocks={blocks} ")
+
+    count = 0
+    
+    for b in blocks:
+        if b.is_grounded():
+            continue
+
+        supporting_count = count_supporting(blocks, b)
+        neighbour_count = count_neighbours(blocks, b)
+        print(f"DEBUG: {supporting_count} {neighbour_count} {b}")
+
+        if supporting_count == 0 or (supporting_count > 0 and neighbour_count > 0):
+            #print(f"DEBUG: Block can be disintegrated: {b}")
+            count += 1
+    return count
 
 def solve_part2(filename):
     lines = fileutils.get_file_lines(filename)
--- aoc-2023/aoc-2023-day-22/solution-in-python3/test_solution.py
+++ aoc-2023/aoc-2023-day-22/solution-in-python3/test_solution.py
@@ -3,10 +3,48 @@ import pytest
 import solution
 
 @pytest.mark.parametrize(
+    "input,expected",
+    [
+        pytest.param([(1,0,1),(1,2,1)], True),
+        pytest.param([(0,0,2),(2,0,2)], False),
+    ],    
+)
+def test_block_is_grounded(input, expected):
+    b = solution.Block(input[0], input[1])
+    assert b.is_grounded() == expected
+
+BLOCK_A = [(1,0,1),(1,2,1)]
+BLOCK_B = [(0,0,2),(2,0,2)]
+BLOCK_C = [(0,2,3),(2,2,3)]
+BLOCK_D = [(0,0,4),(0,2,4)]
+BLOCK_E = [(2,0,5),(2,2,5)]
+BLOCK_F = [(0,1,6),(2,1,6)]
+BLOCK_G = [(1,1,8),(1,1,9)]
+
+@pytest.mark.parametrize(
+    "input1,input2,expected",
+    [
+        pytest.param(BLOCK_A, BLOCK_B, True), # Block A supports B        
+        pytest.param(BLOCK_E, BLOCK_F, True), 
+        pytest.param(BLOCK_F, BLOCK_G, False),
+        pytest.param(BLOCK_B, BLOCK_A, False), 
+        pytest.param(BLOCK_C, BLOCK_A, False), 
+        pytest.param(BLOCK_C, BLOCK_B, False), 
+        pytest.param(BLOCK_D, BLOCK_E, False), 
+    ],    
+)
+def test_block_is_supporting(input1, input2, expected):
+    b1 = solution.Block(input1[0], input1[1])
+    b2 = solution.Block(input2[0], input2[1])
+
+    assert b1.is_supporting(b2) == expected
+
+
+@pytest.mark.parametrize(
     "filename,expected",
     [
-        pytest.param("puzzle-input-example.txt", -1),
-        #pytest.param("puzzle-input-full.txt", -1),
+        pytest.param("puzzle-input-example.txt", 5),
+        pytest.param("puzzle-input-full.txt", -1), # 1235 too high!
     ],    
 )
 def test_solve_part1(filename, expected):
