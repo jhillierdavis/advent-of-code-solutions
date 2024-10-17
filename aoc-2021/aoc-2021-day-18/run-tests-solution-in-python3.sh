2640292853fca5a113243509c471893b61550ee4
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-10-17 14:03:23 +0100

    Recreate symbolic links to shared helper library files on each execution

--- aoc-2021/aoc-2021-day-18/run-tests-solution-in-python3.sh
+++ aoc-2021/aoc-2021-day-18/run-tests-solution-in-python3.sh
@@ -1,4 +1,7 @@
-ln -s ../../shared/shared_python3/helpers ./solution-in-python3/
+# Remove existing sym. linked shared libraries
+rm solution-in-python3/helpers
 
-# pytest -v # Full output 
+# Sym. link shared libraries (if not already)
+ln -s ../../../shared/shared_python3/helpers ./solution-in-python3/
+
 pytest -v
