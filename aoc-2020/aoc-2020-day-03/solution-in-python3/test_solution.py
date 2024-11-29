f70f5240f099d996be86f9cb02f47045611704be
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-11-29 17:37:28 +0000

    AOC 2020 Day 03: Part 1 solution

--- aoc-2020/aoc-2020-day-03/solution-in-python3/test_solution.py
+++ aoc-2020/aoc-2020-day-03/solution-in-python3/test_solution.py
@@ -9,7 +9,7 @@ input_full = "AOC-2020-Day-03_Puzzle-Input-Full.txt"
     "filename, expected",
     [
         pytest.param(input_example, 7),
-        #pytest.param(input_full, -1),
+        pytest.param(input_full, 228),
     ],    
 )
 def test_find_terms(filename, expected):
