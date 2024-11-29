fc8e55c0291e4038e689509e590f7262d52c944e
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-11-29 13:35:51 +0000

    AOC 2020 Day 00: Template fileset for each day

---
+++ aoc-2020/aoc-2020-day-00/solution-in-python3/test_solution.py
@@ -0,0 +1,20 @@
+import pytest
+
+import solution
+
+input_example = "AOC-2020-Day-XX_Puzzle-Input-Example.txt"
+input_full = "AOC-2020-Day-XX_Puzzle-Input-Full.txt"
+
+# Ignore / skip
+@pytest.mark.skip(reason="TODO: Implement")
+@pytest.mark.parametrize(
+    "filename, expected",
+    [
+        pytest.param(input_example, -1),
+        #pytest.param(input_full, -1),
+    ],    
+)
+def test_find_terms(filename, expected):
+    value = solution.solve(filename)
+    
+    assert expected == value
