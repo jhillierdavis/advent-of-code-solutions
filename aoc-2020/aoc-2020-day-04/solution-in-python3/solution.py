87e7703fb0c28f2fb79aa9624c539fcd5bff7428
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-11-30 00:34:38 +0000

    AOC 2020 Day 04: Solved Part 1 & 2

--- aoc-2020/aoc-2020-day-04/solution-in-python3/solution.py
+++ aoc-2020/aoc-2020-day-04/solution-in-python3/solution.py
@@ -1,5 +1,7 @@
 from helpers import fileutils
 
+import re
+
 """
 Passport fields:
 
@@ -13,19 +15,68 @@ pid (Passport ID)
 cid (Country ID)
 """
 
+def get_combined_lines_from_multi_lines_from(filename):
+    line_array = []
+    multilines = fileutils.get_contiguous_non_empty_lines_from(filename)
+    for ml_array in multilines:
+        line = ' '.join(ml_array)
+        line_array.append(line)
+
+    return line_array
+
 
 def solve_part1(filename):
-    lines = fileutils.get_contiguous_non_empty_lines_from(filename)
+    lines = get_combined_lines_from_multi_lines_from(filename)
     #lines = fileutils.get_file_lines_from(filename)
 
     valid_count = 0
     for l in lines:
-        print(f"DEBUG: {l}")
-        str_passport = ' '.join(l)
-        if all(ele in str_passport for ele in ['byr:', 'iyr:', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']):
+        #print(f"DEBUG: {l}")
+        if all(ele in l for ele in ['byr:', 'iyr:', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']):
             valid_count += 1
 
     return valid_count
+
+
+def is_valid_birthyear(value):
+    if value < 1920 or value > 2002:
+        return False
+    return True
+
+
+def is_valid_height(value):
+
+    #hgt (Height) - a number followed by either cm or in:
+    #If cm, the number must be at least 150 and at most 193.
+    #If in, the number must be at least 59 and at most 76.    
+
+    if value.endswith('cm'):
+        value = int(value.strip('cm'))
+        if value < 150 or value > 193:
+            return False
+        return True
+    elif value.endswith('in'):
+        value = int(value.strip('in'))
+        if value < 59 or value > 76:
+            return False
+        return True
+    else:
+        return False
+
+def is_valid_haircolor(value):
+    pattern = re.compile(r'#[0-9a-f]{6}')
+    if pattern.fullmatch(value):
+        return True
+    return False
+
+def is_valid_eyecolor(value):
+    return value in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+
+def is_valid_passportid(value):
+    pattern = re.compile(r'\d{9}')
+    if pattern.fullmatch(value):
+        return True
+    return False
 
 
 """
@@ -43,5 +94,68 @@ pid (Passport ID) - a nine-digit number, including leading zeroes.
 cid (Country ID) - ignored, missing or not.
 """
 
+def is_valid_passport(passport):
+    fields_array = passport.split(' ')
+    for field in fields_array:
+
+        # byr (Birth Year) - four digits; at least 1920 and at most 2002.
+        if field.startswith('byr:'):
+            value = int(field.split(':')[1])
+            #print(f'DEBUG: field={field} value={value}')
+            if not is_valid_birthyear(value):
+                return False
+
+        # iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+        if field.startswith('iyr:'):
+            value = int(field.split(':')[1])
+            #print(f'DEBUG: field={field} value={value}')
+            if value < 2010 or value > 2020:
+                return False
+
+        # eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+        if field.startswith('eyr:'):
+            value = int(field.split(':')[1])
+            #print(f'DEBUG: field={field} value={value}')
+            if value < 2020 or value > 2030:
+                return False
+
+        #hgt (Height) - a number followed by either cm or in:
+        #If cm, the number must be at least 150 and at most 193.
+        #If in, the number must be at least 59 and at most 76.
+        if field.startswith('hgt:'):
+            value = field.split(':')[1]        
+            if not is_valid_height(value):
+                return False
+                
+        # hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.        
+        if field.startswith('hcl:'):
+            value = field.split(':')[1]               
+            if not is_valid_haircolor(value):
+                return False
+
+        # ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+        if field.startswith('ecl:'):
+            value = field.split(':')[1]   
+            if not is_valid_eyecolor(value):
+                return False
+
+        # pid (Passport ID) - a nine-digit number, including leading zeroes.
+        if field.startswith('pid:'):
+            value = field.split(':')[1]   
+            if not is_valid_passportid(value):
+                return False
+            
+    return True
+
 def solve_part2(filename):
-    return -1
+    lines = get_combined_lines_from_multi_lines_from(filename)
+    #lines = fileutils.get_file_lines_from(filename)
+
+    valid_count = 0
+    for l in lines:
+        #print(f"DEBUG: {l}")
+        if all(ele in l for ele in ['byr:', 'iyr:', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']):
+            if is_valid_passport(l):
+                valid_count += 1
+
+    return valid_count
--- aoc-2020/aoc-2020-day-04/solution-in-python3/test_solution.py
+++ aoc-2020/aoc-2020-day-04/solution-in-python3/test_solution.py
@@ -18,10 +18,83 @@ def test_solve_part1(filename, expected):
     assert expected == value
 
 @pytest.mark.parametrize(
+    "value, expected",
+    [
+        pytest.param(2002, True),
+        pytest.param(2003, False),
+    ]
+)
+def test_is_valid_birthyear(value, expected):
+    assert expected == solution.is_valid_birthyear(value)
+
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        pytest.param('60in', True),
+        pytest.param('190cm', True),
+        pytest.param('190in', False),
+        pytest.param('190', False),
+    ]
+)
+def test_is_valid_height(value, expected):
+    assert expected == solution.is_valid_height(value)
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        pytest.param('#123abc', True),
+        pytest.param('#123abz', False),
+        pytest.param('123abc', False),
+    ]
+)
+def test_is_valid_haircolor(value, expected):
+    assert expected == solution.is_valid_haircolor(value)
+
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        pytest.param('brn', True),
+        pytest.param('wat', False),
+    ]
+)
+def test_is_valid_eyecolor(value, expected):
+    assert expected == solution.is_valid_eyecolor(value)
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        pytest.param('000000001', True),
+        pytest.param('0123456789', False),
+    ]
+)
+def test_is_valid_passportid(value, expected):
+    assert expected == solution.is_valid_passportid(value)
+
+
+@pytest.mark.parametrize(
+    "passport, expected",
+    [
+        pytest.param('eyr:1972 cid:100 hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926', False),
+        pytest.param('iyr:2019 hcl:#602927 eyr:1967 hgt:170cm ecl:grn pid:012533040 byr:1946', False),
+        pytest.param('hcl:dab227 iyr:2012 ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277', False),
+        pytest.param('hgt:59cm ecl:zzz eyr:2038 hcl:74454a iyr:2023 pid:3556412378 byr:2007', False),
+        pytest.param('pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980 hcl:#623a2f', True),
+        pytest.param('eyr:2029 ecl:blu cid:129 byr:1989 iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm', True),
+        pytest.param('hcl:#888785 hgt:164cm byr:2001 iyr:2015 cid:88 pid:545766238 ecl:hzl eyr:2022', True),
+        pytest.param('iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719', True),        
+    ],    
+)
+def test_is_valid_passport(passport, expected):
+    assert expected == solution.is_valid_passport(passport)
+
+#@pytest.mark.skip(reason="TODO")
+@pytest.mark.parametrize(
     "filename, expected",
     [
         #pytest.param(input_example, 2),
-        pytest.param(input_full, "TODO"),
+        pytest.param(input_full, 133),
     ],    
 )
 def test_solve_part2(filename, expected):
