1d6cdf90b69ddee250330e541426653ea2bbaebd
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-11-30 17:49:25 +0000

    Updated solution template

--- aoc-2024/aoc-2024-day-00/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-00/solution-in-python3/solution.py
@@ -1,6 +1,11 @@
 from helpers import fileutils
 
-def solve(filename):
+def solve_part1(filename):
+    lines = fileutils.get_file_lines_from(filename)
+
+    return "TODO"
+
+def solve_part2(filename):
     lines = fileutils.get_file_lines_from(filename)
 
     return "TODO"
--- aoc-2024/aoc-2024-day-01/solution-in-python3/test_solution.py
+++ aoc-2024/aoc-2024-day-01/solution-in-python3/test_solution.py
@@ -5,8 +5,8 @@ import solution
 input_example = "AOC-2024-Day-01_Puzzle-Input-Example.txt"
 input_full = "AOC-2024-Day-01_Puzzle-Input-Full.txt"
 
-# Ignore / skip
-#@pytest.mark.skip(reason="TODO: Implement")
+
+@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
@@ -14,7 +14,21 @@ input_full = "AOC-2024-Day-01_Puzzle-Input-Full.txt"
         #pytest.param(input_full, -1),
     ],    
 )
-def test_find_terms(filename, expected):
-    value = solution.solve(filename)
+def test_solve_part1(filename, expected):
+    value = solution.solve_part1(filename)
+    
+    assert expected == value
+
+
+@pytest.mark.skip(reason="TODO: Ignore until implemented")
+@pytest.mark.parametrize(
+    "filename, expected",
+    [
+        pytest.param(input_example, -1),
+        #pytest.param(input_full, -1),
+    ],    
+)
+def test_solve_part2(filename, expected):
+    value = solution.solve_part2(filename)
     
     assert expected == value
