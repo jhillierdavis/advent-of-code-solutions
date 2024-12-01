15186872364788cd89553b18e2a29502f655e7a6
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-01 05:42:04 +0000

    AOC 2024 Day 01: Refactored solutions

--- aoc-2024/aoc-2024-day-01/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-01/solution-in-python3/solution.py
@@ -1,7 +1,7 @@
 from helpers import fileutils
 
 
-def solve_part1(filename):
+def get_sorted_lists_from(filename):
     lines = fileutils.get_file_lines_from(filename)
 
     a1 = []
@@ -16,35 +16,34 @@ def solve_part1(filename):
     a2.sort()
     #print(f'DEBUG: a1={a1}')
     #print(f'DEBUG: a2={a2}')
+    return a1, a2
 
-    count_diff = 0
+
+def get_sum_of_absolute_distance_between_array_lists(a1, a2):
+    count_dist = 0
     for i in range(len(a1)):
-        count_diff += abs(a1[i] - a2[i])
+        count_dist += abs(a1[i] - a2[i])
 
-    return count_diff
+    return count_dist
 
 
-def solve_part2(filename):
-    lines = fileutils.get_file_lines_from(filename)
-    lines = fileutils.get_file_lines_from(filename)
+def solve_part1(filename):
+    a1, a2 = get_sorted_lists_from(filename) 
+    return get_sum_of_absolute_distance_between_array_lists(a1, a2)
 
-    a1 = []
-    a2 = []
-    for l in lines:
-        values = l.split('   ')
-        #print(f'DEBUG: l={l} values={values}')
-        a1.append(int(values[0]))
-        a2.append(int(values[1]))
 
-    a1.sort()
-    a2.sort()
- 
-    count_sim = 0
+def get_sum_of_frequency_of_occurance_of_elements_of_first_list_in_second_list(a1, a2):
+    count_freq = 0
     for i in range(len(a1)):
         occurs = 0
         for j in range(len(a2)):
             if a1[i] == a2[j]:
                 occurs += 1
-        count_sim += a1[i] * occurs 
+        count_freq += a1[i] * occurs 
 
-    return count_sim
+    return count_freq
+
+
+def solve_part2(filename):
+    a1, a2 = get_sorted_lists_from(filename) 
+    return get_sum_of_frequency_of_occurance_of_elements_of_first_list_in_second_list(a1, a2)
