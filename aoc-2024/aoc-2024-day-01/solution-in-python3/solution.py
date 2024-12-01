68c17eb5cc059f70caa1cbc6e84f8cc333a1c35f
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-01 05:32:31 +0000

    AOC 2024 Day 01: Solved Parts 1 & 2

--- aoc-2024/aoc-2024-day-01/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-01/solution-in-python3/solution.py
@@ -1,11 +1,50 @@
 from helpers import fileutils
 
+
 def solve_part1(filename):
     lines = fileutils.get_file_lines_from(filename)
 
-    return "TODO"
+    a1 = []
+    a2 = []
+    for l in lines:
+        values = l.split('   ')
+        #print(f'DEBUG: l={l} values={values}')
+        a1.append(int(values[0]))
+        a2.append(int(values[1]))
+
+    a1.sort()
+    a2.sort()
+    #print(f'DEBUG: a1={a1}')
+    #print(f'DEBUG: a2={a2}')
+
+    count_diff = 0
+    for i in range(len(a1)):
+        count_diff += abs(a1[i] - a2[i])
+
+    return count_diff
+
 
 def solve_part2(filename):
     lines = fileutils.get_file_lines_from(filename)
+    lines = fileutils.get_file_lines_from(filename)
 
-    return "TODO"
+    a1 = []
+    a2 = []
+    for l in lines:
+        values = l.split('   ')
+        #print(f'DEBUG: l={l} values={values}')
+        a1.append(int(values[0]))
+        a2.append(int(values[1]))
+
+    a1.sort()
+    a2.sort()
+ 
+    count_sim = 0
+    for i in range(len(a1)):
+        occurs = 0
+        for j in range(len(a2)):
+            if a1[i] == a2[j]:
+                occurs += 1
+        count_sim += a1[i] * occurs 
+
+    return count_sim
--- aoc-2024/aoc-2024-day-01/solution-in-python3/test_solution.py
+++ aoc-2024/aoc-2024-day-01/solution-in-python3/test_solution.py
@@ -6,12 +6,11 @@ input_example = "AOC-2024-Day-01_Puzzle-Input-Example.txt"
 input_full = "AOC-2024-Day-01_Puzzle-Input-Full.txt"
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 11),
+        pytest.param(input_full, 1882714),
     ],    
 )
 def test_solve_part1(filename, expected):
@@ -20,12 +19,12 @@ def test_solve_part1(filename, expected):
     assert expected == value
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 31),
+        pytest.param(input_full, 19437052),
     ],    
 )
 def test_solve_part2(filename, expected):
