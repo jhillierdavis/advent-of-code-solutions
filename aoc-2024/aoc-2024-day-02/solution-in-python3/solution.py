932fd41a8fdac7dcca8e30c7bec9783c062cf4c9
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-02 06:30:49 +0000

    AOC 2024 Day 02: Refactored solution

--- aoc-2024/aoc-2024-day-02/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-02/solution-in-python3/solution.py
@@ -6,11 +6,29 @@ def is_safe_gradually_increasing(diff):
             return False
     return True
 
+
 def is_safe_gradually_decreasing(diff):
     for d in diff:
         if d > -1 or d < -3:
             return False
     return True
+
+
+def to_int_array_from(array_str):
+    values = [int(numeric_string) for numeric_string in array_str]
+    return values
+
+
+def get_diff_array_from(array_int):
+    
+    array_diffs = []
+    last = -1
+    for e in array_int:        
+        if last != -1:
+            array_diffs.append(e - last)
+        last = e    
+    return array_diffs
+
 
 def solve_part1(filename):
     lines = fileutils.get_file_lines_from(filename)
@@ -18,19 +36,16 @@ def solve_part1(filename):
     count_safe = 0
     for l in lines:
         #print(f'DEBUG: l={l}')
-        a = l.split(' ')
-        last = -1
-        diff = []
-        for v in a:        
-            value = int(v)
-            if last != -1:
-                diff.append(value - last)
-            last = value
+        values = to_int_array_from(l.split(' '))
+
+        diff = get_diff_array_from(values)
         #print(f'DEBUG: diff={diff}')
+
         if is_safe_gradually_decreasing(diff) or is_safe_gradually_increasing(diff):
             count_safe += 1
 
     return count_safe
+
 
 def solve_part2(filename):
     lines = fileutils.get_file_lines_from(filename)
@@ -38,10 +53,7 @@ def solve_part2(filename):
     count_safe = 0
     for l in lines:
         #print(f'DEBUG: l={l}')
-        a = l.split(' ')
-        values = []
-        for v in a:        
-            values.append(int(v))
+        values = to_int_array_from(l.split(' '))
 
         for i in range(len(values)):
             partial_copy = []
@@ -50,13 +62,8 @@ def solve_part2(filename):
                     continue
                 partial_copy.append(values[j])
 
-            last = -1
-            diff = []
-            for e in partial_copy:        
-                if last != -1:
-                    diff.append(e - last)
-                last = e
-            
+            diff = get_diff_array_from(partial_copy)
+
             if is_safe_gradually_increasing(diff) or is_safe_gradually_decreasing(diff):
                 #print(f'DEBUG: Safe: diff={diff}')
                 count_safe += 1
