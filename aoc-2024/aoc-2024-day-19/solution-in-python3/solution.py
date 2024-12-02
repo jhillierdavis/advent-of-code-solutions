4d8bfa2feaacc994dbe4ae819de655616c64ae58
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-02 17:34:07 +0000

    AOC 2024 Day 19: Initial fileset

---
+++ aoc-2024/aoc-2024-day-19/solution-in-python3/solution.py
@@ -0,0 +1,11 @@
+from helpers import fileutils
+
+def solve_part1(filename):
+    lines = fileutils.get_file_lines_from(filename)
+
+    return "TODO"
+
+def solve_part2(filename):
+    lines = fileutils.get_file_lines_from(filename)
+
+    return "TODO"
