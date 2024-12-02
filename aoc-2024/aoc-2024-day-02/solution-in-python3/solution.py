011aec67a2c4e0e9c8db12c279b6459ff28dfc1c
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-02 06:21:03 +0000

    AOC 2024 Day 02: Solved Part 1 & 2

--- aoc-2024/aoc-2024-day-02/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-02/solution-in-python3/solution.py
@@ -1,11 +1,65 @@
 from helpers import fileutils
 
+def is_safe_gradually_increasing(diff):
+    for d in diff:
+        if d < 1 or d > 3:
+            return False
+    return True
+
+def is_safe_gradually_decreasing(diff):
+    for d in diff:
+        if d > -1 or d < -3:
+            return False
+    return True
+
 def solve_part1(filename):
     lines = fileutils.get_file_lines_from(filename)
 
-    return "TODO"
+    count_safe = 0
+    for l in lines:
+        #print(f'DEBUG: l={l}')
+        a = l.split(' ')
+        last = -1
+        diff = []
+        for v in a:        
+            value = int(v)
+            if last != -1:
+                diff.append(value - last)
+            last = value
+        #print(f'DEBUG: diff={diff}')
+        if is_safe_gradually_decreasing(diff) or is_safe_gradually_increasing(diff):
+            count_safe += 1
+
+    return count_safe
 
 def solve_part2(filename):
     lines = fileutils.get_file_lines_from(filename)
 
-    return "TODO"
+    count_safe = 0
+    for l in lines:
+        #print(f'DEBUG: l={l}')
+        a = l.split(' ')
+        values = []
+        for v in a:        
+            values.append(int(v))
+
+        for i in range(len(values)):
+            partial_copy = []
+            for j in range(len(values)):
+                if i == j:
+                    continue
+                partial_copy.append(values[j])
+
+            last = -1
+            diff = []
+            for e in partial_copy:        
+                if last != -1:
+                    diff.append(e - last)
+                last = e
+            
+            if is_safe_gradually_increasing(diff) or is_safe_gradually_decreasing(diff):
+                #print(f'DEBUG: Safe: diff={diff}')
+                count_safe += 1
+                break
+
+    return count_safe
--- aoc-2024/aoc-2024-day-02/solution-in-python3/test_solution.py
+++ aoc-2024/aoc-2024-day-02/solution-in-python3/test_solution.py
@@ -6,12 +6,12 @@ input_example = "AOC-2024-Day-02_Puzzle-Input-Example.txt"
 input_full = "AOC-2024-Day-02_Puzzle-Input-Full.txt"
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 2),
+        pytest.param(input_full, 516),
     ],    
 )
 def test_solve_part1(filename, expected):
@@ -20,12 +20,12 @@ def test_solve_part1(filename, expected):
     assert expected == value
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 4),
+        pytest.param(input_full, 561),
     ],    
 )
 def test_solve_part2(filename, expected):
