0893ca30d0a5b878dfbcc4c893440248e960f459
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-03 09:30:03 +0000

    AOC 2024 Day 03: Solved Part 1 & 2

--- aoc-2024/aoc-2024-day-03/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-03/solution-in-python3/solution.py
@@ -1,11 +1,47 @@
 from helpers import fileutils
 
-def solve_part1(filename):
-    lines = fileutils.get_file_lines_from(filename)
+import re
 
-    return "TODO"
+def find_repeating_multiplication(text):
+    pattern = re.compile(r'(mul\(\d+,\d+\))+?')
+    matches = pattern.findall(text)
+    return matches
+
+def get_multiplication(snippet):
+    x,y = snippet[4:-1].split(',')
+    return int(x) * int(y)
+
+def solve_part1(filename):
+    text = open(filename).read().strip()
+
+    count = 0
+    matches = find_repeating_multiplication(text)
+    for m in matches:
+        #print(f"DEBUG: m={m}")
+        count += get_multiplication(m)
+
+    return count
+
 
 def solve_part2(filename):
-    lines = fileutils.get_file_lines_from(filename)
+    text = open(filename).read().strip()
 
-    return "TODO"
+    pattern = re.compile(r'mul\(\d+,\d+\)')
+
+    count = 0    
+    enabled = True
+    for i in range(len(text)):
+        if text[i:].startswith('do()'):
+            enabled = True
+            i += 3
+            continue
+        elif text[i:].startswith("don't()"):
+            enabled = False
+            i += 6
+            continue
+
+        match = pattern.match(text[i:])
+        if match is not None and enabled:
+            count += get_multiplication(match.group(0))
+
+    return count
--- aoc-2024/aoc-2024-day-03/solution-in-python3/test_solution.py
+++ aoc-2024/aoc-2024-day-03/solution-in-python3/test_solution.py
@@ -2,16 +2,16 @@ import pytest
 
 import solution
 
-input_example = "AOC-2024-Day-03_Puzzle-Input-Example.txt"
+input_example_part1 = "AOC-2024-Day-03_Puzzle-Input-Example-Part1.txt"
 input_full = "AOC-2024-Day-03_Puzzle-Input-Full.txt"
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example_part1, 161),
+        pytest.param(input_full, 173419328), # not 31748124!
     ],    
 )
 def test_solve_part1(filename, expected):
@@ -20,12 +20,14 @@ def test_solve_part1(filename, expected):
     assert expected == value
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+input_example_part2 = "AOC-2024-Day-03_Puzzle-Input-Example-Part2.txt"
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example_part2, 48),
+        pytest.param(input_full, 90669332),
     ],    
 )
 def test_solve_part2(filename, expected):
