3668acfe61c41a13234fa018e0a83b17479a66af
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-03 10:06:48 +0000

    AOC 2024 Day 03: Refactord

--- aoc-2024/aoc-2024-day-03/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-03/solution-in-python3/solution.py
@@ -2,47 +2,55 @@ from helpers import fileutils
 
 import re
 
-def find_repeating_multiplication(text):
-    pattern = re.compile(r'(mul\(\d+,\d+\))+?')
-    matches = pattern.findall(text)
-    return matches
 
-def get_multiplication(snippet):
-    x,y = snippet[4:-1].split(',')
-    return int(x) * int(y)
+# def find_repeating_multiplication(text):
+#     pattern = re.compile(r'(mul\(\d+,\d+\))+?')
+#     matches = pattern.findall(text)
+#     return matches
 
-def solve_part1(filename):
-    text = open(filename).read().strip()
+# def get_multiplication(snippet):
+#     x,y = snippet[4:-1].split(',')
+#     return int(x) * int(y)
 
-    count = 0
-    matches = find_repeating_multiplication(text)
-    for m in matches:
-        #print(f"DEBUG: m={m}")
-        count += get_multiplication(m)
+# def solve_part1(filename):
+#     text = open(filename).read().strip()
 
-    return count
+#     count = 0
+#     matches = find_repeating_multiplication(text)
+#     for m in matches:
+#         #print(f"DEBUG: m={m}")
+#         count += get_multiplication(m)
+
+#     return count
 
 
-def solve_part2(filename):
-    text = open(filename).read().strip()
-
+def count_multiplications_in(text, is_enabler_active=False):
     # Match via groups (with sub-groups for integer values)
     pattern = re.compile(r'mul\((\d+),(\d+)\)')
-
+   
     count = 0    
     enabled = True
     for i in range(len(text)):
-        if text[i:].startswith('do()'):
-            enabled = True
-            i += 3
-            continue
-        elif text[i:].startswith("don't()"):
-            enabled = False
-            i += 6
-            continue
+        if is_enabler_active:
+            if text[i:].startswith('do()'):
+                enabled = True
+                i += 3
+                continue
+            elif text[i:].startswith("don't()"):
+                enabled = False
+                i += 6
+                continue
 
         match = pattern.match(text[i:])
         if match is not None and enabled:
             count += int(match.group(1)) * int(match.group(2)) 
 
-    return count
+    return count    
+
+def solve_part1(filename):
+    text = open(filename).read().strip()
+    return count_multiplications_in(text)
+
+def solve_part2(filename):
+    text = open(filename).read().strip()
+    return count_multiplications_in(text, True)
