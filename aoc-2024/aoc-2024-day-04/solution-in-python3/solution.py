45a8a34836cdd1f00fe367c7a2eed2854157617d
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-04 06:29:40 +0000

    AOC 2024 Day 04: Solved Parts 1 & 2

--- aoc-2024/aoc-2024-day-04/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-04/solution-in-python3/solution.py
@@ -1,11 +1,116 @@
-from helpers import fileutils
+from helpers import fileutils, grid, point
+
 
 def solve_part1(filename):
     lines = fileutils.get_file_lines_from(filename)
+    g = grid.lines_to_grid(lines)
 
-    return "TODO"
+    #grid.display_grid(g)
+
+    count = 0
+    height = g.get_height()
+    width = g.get_width()
+    for h in range(height):
+        for w in range(width):
+            p  = point.Point2D(w,h)
+            if g.get_symbol(p) == 'X':
+
+                # Forward
+                next = point.Point2D(p.get_x() + 1, p.get_y())
+                if g.contains(next) and g.get_symbol(next) == 'M':
+                    next = point.Point2D(p.get_x() + 2, p.get_y())
+                    if g.contains(next) and g.get_symbol(next) == 'A':
+                        next = point.Point2D(p.get_x() + 3, p.get_y())
+                        if g.contains(next) and g.get_symbol(next) == 'S':
+                             count += 1
+
+                next = point.Point2D(p.get_x() -1, p.get_y())
+                if g.contains(next) and g.get_symbol(next) == 'M':
+                    next = point.Point2D(p.get_x() -2, p.get_y())
+                    if g.contains(next) and g.get_symbol(next) == 'A':
+                        next = point.Point2D(p.get_x() -3, p.get_y())
+                        if g.contains(next) and g.get_symbol(next) == 'S':
+                             count += 1
+
+                next = point.Point2D(p.get_x(), p.get_y() + 1)
+                if g.contains(next) and g.get_symbol(next) == 'M':
+                    next = point.Point2D(p.get_x(), p.get_y() + 2)
+                    if g.contains(next) and g.get_symbol(next) == 'A':
+                        next = point.Point2D(p.get_x(), p.get_y() + 3)
+                        if g.contains(next) and g.get_symbol(next) == 'S':
+                             count += 1
+
+                next = point.Point2D(p.get_x(), p.get_y() - 1)
+                if g.contains(next) and g.get_symbol(next) == 'M':
+                    next = point.Point2D(p.get_x(), p.get_y() - 2)
+                    if g.contains(next) and g.get_symbol(next) == 'A':
+                        next = point.Point2D(p.get_x(), p.get_y() - 3)
+                        if g.contains(next) and g.get_symbol(next) == 'S':
+                             count += 1
+
+                next = point.Point2D(p.get_x() +1, p.get_y() + 1)
+                if g.contains(next) and g.get_symbol(next) == 'M':
+                    next = point.Point2D(p.get_x() +2, p.get_y() + 2)
+                    if g.contains(next) and g.get_symbol(next) == 'A':
+                        next = point.Point2D(p.get_x() +3, p.get_y() + 3)
+                        if g.contains(next) and g.get_symbol(next) == 'S':
+                             count += 1
+
+                next = point.Point2D(p.get_x() - 1, p.get_y() - 1)
+                if g.contains(next) and g.get_symbol(next) == 'M':
+                    next = point.Point2D(p.get_x() - 2, p.get_y() - 2)
+                    if g.contains(next) and g.get_symbol(next) == 'A':
+                        next = point.Point2D(p.get_x() - 3, p.get_y() - 3)
+                        if g.contains(next) and g.get_symbol(next) == 'S':
+                             count += 1
+
+                next = point.Point2D(p.get_x() + 1, p.get_y() - 1)
+                if g.contains(next) and g.get_symbol(next) == 'M':
+                    next = point.Point2D(p.get_x() + 2, p.get_y() - 2)
+                    if g.contains(next) and g.get_symbol(next) == 'A':
+                        next = point.Point2D(p.get_x() + 3, p.get_y() - 3)
+                        if g.contains(next) and g.get_symbol(next) == 'S':
+                             count += 1
+
+                next = point.Point2D(p.get_x() - 1, p.get_y() + 1)
+                if g.contains(next) and g.get_symbol(next) == 'M':
+                    next = point.Point2D(p.get_x() - 2, p.get_y() + 2)
+                    if g.contains(next) and g.get_symbol(next) == 'A':
+                        next = point.Point2D(p.get_x() - 3, p.get_y() + 3)
+                        if g.contains(next) and g.get_symbol(next) == 'S':
+                             count += 1
+
+    return count
+
 
 def solve_part2(filename):
     lines = fileutils.get_file_lines_from(filename)
+    g = grid.lines_to_grid(lines)
 
-    return "TODO"
+    count = 0
+    height = g.get_height()
+    width = g.get_width()
+    for h in range(height):
+        for w in range(width):
+            p  = point.Point2D(w,h)
+            if g.get_symbol(p) == 'A':
+                ne = point.Point2D(p.get_x() - 1, p.get_y() - 1)
+                nw = point.Point2D(p.get_x() + 1, p.get_y() - 1)
+                se = point.Point2D(p.get_x() - 1, p.get_y() + 1)
+                sw = point.Point2D(p.get_x() + 1, p.get_y() + 1)
+                if g.contains(ne) and g.contains(nw) and g.contains(se) and g.contains(sw):
+                    nes = g.get_symbol(ne)
+                    nws = g.get_symbol(nw)
+                    ses = g.get_symbol(se)
+                    sws = g.get_symbol(sw)
+
+                    if nes == 'M' and sws == 'S' and nws == 'M' and ses == 'S':
+                        count += 1              
+                    elif nes == 'S' and sws == 'M' and nws == 'M' and ses == 'S':
+                        count += 1              
+                    elif nes == 'S' and sws == 'M' and nws == 'S' and ses == 'M':
+                        count += 1              
+                    elif nes == 'M' and sws == 'S' and nws == 'S' and ses == 'M':
+                        count += 1              
+
+    return count
--- aoc-2024/aoc-2024-day-04/solution-in-python3/test_solution.py
+++ aoc-2024/aoc-2024-day-04/solution-in-python3/test_solution.py
@@ -6,12 +6,12 @@ input_example = "AOC-2024-Day-04_Puzzle-Input-Example.txt"
 input_full = "AOC-2024-Day-04_Puzzle-Input-Full.txt"
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 18),
+        pytest.param(input_full, 2662),
     ],    
 )
 def test_solve_part1(filename, expected):
@@ -20,12 +20,12 @@ def test_solve_part1(filename, expected):
     assert expected == value
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 9),
+        pytest.param(input_full, 2034),
     ],    
 )
 def test_solve_part2(filename, expected):
