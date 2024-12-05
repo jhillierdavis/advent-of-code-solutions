3734e756ddab0e1354b911cf0ef6a5ee66094e23
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-05 06:49:52 +0000

    AOC 2024 Day 05: Solved Part 1 (not yet Part 2)

--- aoc-2024/aoc-2024-day-05/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-05/solution-in-python3/solution.py
@@ -1,11 +1,131 @@
 from helpers import fileutils
 
-def solve_part1(filename):
-    lines = fileutils.get_file_lines_from(filename)
 
-    return "TODO"
+def get_middle_value(int_array):
+    middle = int(len(int_array)/2)
+    return int_array[middle]
+
+def is_valid_ordering(list_int, orderings):
+    length = len(list_int)
+    for i in range(length):
+        current = list_int[i]
+        for j in range((i+1),length):
+            next = list_int[j]
+            items = orderings.get(next)
+            #print(f"DEBUG: current={current} next={next} items={items}")
+            if items == None:
+                continue
+            if current in items:
+                return False
+    return True
+
+def get_page_orderings_before_from(lines):
+    orderings = dict()
+
+    for l in lines:
+        values = l.split('|')
+        key = int(values[0])
+        item = int(values[1])
+        items = orderings.get(key)
+        if None == items:
+            items = []
+        items.append(item)
+        orderings.update({key: items})  
+
+    return orderings
+
+def get_page_orderings_after_from(lines):
+    page_orderings_afer = dict()
+
+    for l in lines:
+        values = l.split('|')
+        item = int(values[0])
+        key = int(values[1])
+        items = page_orderings_afer.get(key)
+        if None == items:
+            items = []
+        items.append(item)
+        page_orderings_afer.update({key: items})  
+
+    return page_orderings_afer
+
+
+def solve_part1(filename):
+    lines = fileutils.get_lines_before_empty_from_file(filename)
+    #print(f"DEBUG: Ordering lines={lines}")
+
+    orderings = get_page_orderings_before_from(lines)
+    #print(f"DEBUG: Orderings={orderings}")
+
+    lines = fileutils.get_lines_after_empty_from_file(filename)
+    #print(f"DEBUG: Updates={lines}")
+
+    count = 0
+    for l in lines:
+        values = l.split(',')
+        list_int = []
+        for v in values:
+            entry = int(v)
+            list_int.append(entry)
+        #print(f"DEBUG: list_int={list_int}")   
+        if is_valid_ordering(list_int, orderings):
+             print(f"DEBUG: Valid list_int={list_int}")   
+             count += get_middle_value(list_int)
+
+    return count
+
+def reorder_invalid_list(list_int, orderings):
+
+    new_list = []
+    length = len(list_int)
+    for i in range(length):
+        current = list_int[i]
+        for j in range((i+1),length):
+            next = list_int[j]
+            items = orderings.get(next)
+            #print(f"DEBUG: current={current} next={next} items={items}")
+            if items == None:
+                if current not in new_list:
+                    new_list.append(current)
+            elif current in items:
+                #print(f"DEBUG: Higher priority: current = {current} , next = {next}")
+                if next not in new_list:
+                    new_list.append(next)
+                if current not in new_list:    
+                    new_list.append(current)
+            else:
+                if current not in new_list:
+                    new_list.append(current)
+    return new_list
+        
 
 def solve_part2(filename):
-    lines = fileutils.get_file_lines_from(filename)
+    lines = fileutils.get_lines_before_empty_from_file(filename)
+    #print(f"DEBUG: Ordering lines={lines}")
 
-    return "TODO"
+    orderings = get_page_orderings_before_from(lines)
+    #print(f"DEBUG: Orderings={orderings}")
+
+    lines = fileutils.get_lines_after_empty_from_file(filename)
+    #print(f"DEBUG: Updates={lines}")
+
+    count = 0
+    for l in lines:
+        values = l.split(',')
+        list_int = []
+        for v in values:
+            entry = int(v)
+            list_int.append(entry)
+        #print(f"DEBUG: list_int={list_int}")   
+        if not is_valid_ordering(list_int, orderings):
+            print(f"DEBUG: Invalid list_int={list_int}")   
+
+            #TODO: Reoder list
+            new_list = list_int
+            while not is_valid_ordering(new_list, orderings):
+                new_list = reorder_invalid_list(new_list, orderings)
+            #new_list = reorder_invalid_list(new_list, orderings)    
+            print(f"DEBUG: Reordered new_list={new_list}")   
+            count += get_middle_value(new_list)
+
+    return count
--- aoc-2024/aoc-2024-day-05/solution-in-python3/test_solution.py
+++ aoc-2024/aoc-2024-day-05/solution-in-python3/test_solution.py
@@ -6,12 +6,24 @@ input_example = "AOC-2024-Day-05_Puzzle-Input-Example.txt"
 input_full = "AOC-2024-Day-05_Puzzle-Input-Full.txt"
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+@pytest.mark.parametrize(
+    "int_array, expected",
+    [
+        pytest.param([75,47,61,53,29], 61),
+        pytest.param([97,61,53,29,13], 53),
+        pytest.param([75,29,13], 29),
+    ],    
+)
+def test_get_middle_value(int_array, expected):
+    value = solution.get_middle_value(int_array)   
+    assert expected == value
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 143),
+        pytest.param(input_full, 7307),
     ],    
 )
 def test_solve_part1(filename, expected):
@@ -20,12 +32,12 @@ def test_solve_part1(filename, expected):
     assert expected == value
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 123),
+        pytest.param(input_full, -1), # 4713 # 4454 too low!, 4738 too high!
     ],    
 )
 def test_solve_part2(filename, expected):
