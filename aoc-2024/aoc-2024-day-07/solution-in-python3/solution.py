8789206ab74f0224da88fddf6e6177253e8268f0
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-07 07:41:03 +0000

    AOC 2024 Day 07: Solved Parts 1 & 2

--- aoc-2024/aoc-2024-day-07/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-07/solution-in-python3/solution.py
@@ -1,11 +1,99 @@
 from helpers import fileutils
 
+def to_int_array_from(array_str):
+    #print(f"DEBUG: {array_str}")
+    values = [int(numeric_string) for numeric_string in array_str]
+    return values
+
+def is_possible_equation(values, result, subtotal=0):    
+    if subtotal > result:
+        return False
+    
+    addition = values[0] + subtotal
+    multiplication = values[0] if subtotal == 0 else values[0] * subtotal # Fix nasty bug (originally forgot to guard against multiplication by zero)!
+    
+    size = len(values)
+    if size == 1:
+        if result == addition or result == multiplication:
+            return True
+        return False
+
+    if is_possible_equation(values[1:], result, addition) or is_possible_equation(values[1:], result, multiplication):
+        return True  
+
+
+def concatonate(alpha:int, beta:int) -> int:
+    if alpha == 0:
+        return beta
+    return int(str(alpha) + str(beta))
+
+
+def is_possible_equation_with_concatonation(values, result, subtotal=0):
+    #print(f"DEBUG: subtotal={subtotal} values={values}")
+    
+    if subtotal > result:
+        return False
+    
+    addition = values[0] + subtotal
+    multiplication = values[0] if subtotal == 0 else values[0] * subtotal # Fix nasty bug (originally forgot to guard against multiplication by zero)!
+    concatination = concatonate(subtotal, values[0])
+    
+    
+    size = len(values)
+    if size == 1: # Last value
+        if result == addition or result == multiplication or result == concatination:
+            #print(f"DEBUG: Valid subtotal={subtotal} values={values}")
+            return True
+        return False
+
+    if is_possible_equation_with_concatonation(values[1:], result, addition) \
+        or is_possible_equation_with_concatonation(values[1:], result, multiplication) \
+        or is_possible_equation_with_concatonation(values[1:], result, concatination):
+            return True
+    return False
+
+
+"""
+def is_possible_equation_with_concatonation(values, result, subtotal=0):
+    print(f"DEBUG: subtotal={subtotal} values={values} result={result}")
+    
+    if subtotal > result:
+        return False
+    
+    size = len(values)
+    if size == 1:
+        if result == concatonate(subtotal, values[0]):
+            return True
+        return False
+
+    return is_possible_equation(values[1:], result, concatonate(subtotal, values[0]))
+"""
+
 def solve_part1(filename):
     lines = fileutils.get_file_lines_from(filename)
+    ans = 0
+    for l in lines:
+        parts = l.split(': ')
+        result = int(parts[0])
+        values = to_int_array_from(parts[1].split(' '))
 
-    return "TODO"
+        if is_possible_equation(values, result):
+            #print(f"DEBUG: Valid equation: {result} {values}")
+            ans += result
+    return ans
+
 
 def solve_part2(filename):
     lines = fileutils.get_file_lines_from(filename)
+    ans = 0
+    for l in lines:
+        parts = l.split(': ')
+        result = int(parts[0])
+        values = to_int_array_from(parts[1].split(' '))
 
-    return "TODO"
+        if is_possible_equation_with_concatonation(values, result):
+            print(f"DEBUG: Valid equation: {result} {values}")
+            ans += result
+        #else:
+        #    print(f"DEBUG: Invalid equation: {result} {values}")
+    return ans
--- aoc-2024/aoc-2024-day-07/solution-in-python3/test_solution.py
+++ aoc-2024/aoc-2024-day-07/solution-in-python3/test_solution.py
@@ -6,12 +6,41 @@ input_example = "AOC-2024-Day-07_Puzzle-Input-Example.txt"
 input_full = "AOC-2024-Day-07_Puzzle-Input-Full.txt"
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+@pytest.mark.parametrize(
+    "alpha, beta, expected",
+    [
+        pytest.param(1,2,12),
+        pytest.param(12,345,12345),
+    ],    
+)
+def test_concatonate(alpha, beta, expected):
+    assert expected == solution.concatonate(alpha, beta)
+
+
+@pytest.mark.parametrize(
+    "values, result, expected",
+    [
+        pytest.param([0,1],1,True),
+        pytest.param([15,6],156,True),        
+        pytest.param([6,8,6,15],7290,True),
+        pytest.param([17,8,14],192,True),
+        pytest.param([34, 79, 78, 217, 6],2987,True),
+        pytest.param([633, 5, 5],3170,True),
+        pytest.param([7, 1, 9], 19, False),
+        pytest.param([7, 1, 9, 701], 720, False),
+        pytest.param([7, 1, 9, 701, 797],1517,False)
+    ],    
+)
+def test_is_possible_equation_with_concatonation(values, result, expected):
+    assert expected == solution.is_possible_equation_with_concatonation(values, result)    
+
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 3749),
+        pytest.param(input_full, 7885693428401),
     ],    
 )
 def test_solve_part1(filename, expected):
@@ -20,12 +49,12 @@ def test_solve_part1(filename, expected):
     assert expected == value
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 11387),
+        pytest.param(input_full, 348360680516005), # 7887657898042 too low, 348360680517522 too high!
     ],    
 )
 def test_solve_part2(filename, expected):
