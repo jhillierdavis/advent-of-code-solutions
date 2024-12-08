f28a0809d261d48a1f02dc6e082833dcc297ebea
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-08 11:49:50 +0000

    AOC 2024 Day 08: Solved Part 1 & 2

--- aoc-2024/aoc-2024-day-08/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-08/solution-in-python3/solution.py
@@ -1,11 +1,144 @@
-from helpers import fileutils
+from helpers import fileutils, grid, point
+
+from collections import defaultdict
+
+import string
+
+def is_valid_antinode_position(g, p, k):
+    return g.contains(p) and g.get_symbol(p) != k
+        
 
 def solve_part1(filename):
     lines = fileutils.get_file_lines_from(filename)
+    g = grid.lines_to_grid(lines)
 
-    return "TODO"
+    #grid.display_grid(g)
+
+    # Generate array of characters
+    character_array = list(string.ascii_uppercase + string.ascii_lowercase + string.digits)
+
+    # Find antenane points for each antenane types
+    antennae_map = defaultdict(set)
+    for i in range(len(character_array)):
+        ch = character_array[i]
+        #print(f'DEBUG: i={i} ch={ch}')
+
+        smp = g.get_points_matching(ch)
+        antennae_map[ch] = smp
+
+
+    # For each antenane type point, find distance to next and mark antinodes
+    #print(f'DEBUG: antennae_map={antennae_map}')
+    count = 0
+    g_ans = g.clone()
+    for k in antennae_map.keys():
+        v = antennae_map[k]
+        size = len(v)
+        if size <= 1:
+            continue
+        
+        #print(f'DEBUG: k={k} v={v} size={size}')
+        
+        for i in range(size):
+            current = list(v)[i]
+            for j in range(i+1,):
+                next = list(v)[j]
+                if current == next:
+                    continue
+                
+                x_offset = current.get_x() - next.get_x()
+                y_offset = current.get_y() - next.get_y()
+
+                #print(f"DEBUG: compare {current} with {next} :  {x_offset} {y_offset}")
+                next_ap = point.Point2D(current.get_x() + x_offset, current.get_y() + y_offset)
+                if is_valid_antinode_position(g, next_ap, k):
+                    g_ans.set_symbol(next_ap, '#')
+                    count += 1
+
+                next_ap = point.Point2D(current.get_x() - x_offset, current.get_y() - y_offset)
+                if is_valid_antinode_position(g, next_ap, k):
+                    g_ans.set_symbol(next_ap, '#')
+                    count += 1
+
+                next_ap = point.Point2D(next.get_x() + x_offset, next.get_y() + y_offset)
+                if is_valid_antinode_position(g, next_ap, k):
+                    g_ans.set_symbol(next_ap, '#')
+                    count += 1
+
+                next_ap = point.Point2D(next.get_x() - x_offset, next.get_y() - y_offset)
+                if is_valid_antinode_position(g, next_ap, k):
+                    g_ans.set_symbol(next_ap, '#')
+                    count += 1
+
+    #grid.display_grid(g_ans)
+    return g_ans.count_symbol('#')
+    #return count
 
 def solve_part2(filename):
     lines = fileutils.get_file_lines_from(filename)
+    g = grid.lines_to_grid(lines)
 
-    return "TODO"
+    #grid.display_grid(g)
+
+    # Generate array of characters
+    character_array = list(string.ascii_uppercase + string.ascii_lowercase + string.digits)
+
+    # Find antenane points for each antenane types
+    antennae_map = defaultdict(set)
+    for i in range(len(character_array)):
+        ch = character_array[i]
+        #print(f'DEBUG: i={i} ch={ch}')
+
+        smp = g.get_points_matching(ch)
+        antennae_map[ch] = smp
+
+
+    # For each antenane type point, find distance to next and mark antinodes
+    #print(f'DEBUG: antennae_map={antennae_map}')
+    count = 0
+    g_ans = g.clone()
+    for k in antennae_map.keys():
+        v = antennae_map[k]
+        size = len(v)
+        if size <= 1:
+            continue
+        
+        print(f'DEBUG: k={k} v={v} size={size}')
+        
+        for i in range(size):
+            current = list(v)[i]
+            for j in range(i+1,):
+                next = list(v)[j]
+                if current == next:
+                    continue
+                
+                x_initial_offset = current.get_x() - next.get_x()
+                y_intial_offset = current.get_y() - next.get_y()
+
+                #print(f"DEBUG: compare {current} with {next} :  {x_offset} {y_offset}")
+                for z in range(1,g.get_height()):
+                    x_offset = z * x_initial_offset
+                    y_offset = z * y_intial_offset
+
+                    next_ap = point.Point2D(current.get_x() + x_offset, current.get_y() + y_offset)
+                    if g.contains(next_ap):
+                        g_ans.set_symbol(next_ap, '#')
+                        count += 1
+
+                    next_ap = point.Point2D(current.get_x() - x_offset, current.get_y() - y_offset)
+                    if g.contains(next_ap):
+                        g_ans.set_symbol(next_ap, '#')
+                        count += 1
+
+                    next_ap = point.Point2D(next.get_x() + x_offset, next.get_y() + y_offset)
+                    if g.contains(next_ap):
+                        g_ans.set_symbol(next_ap, '#')
+                        count += 1
+
+                    next_ap = point.Point2D(next.get_x() - x_offset, next.get_y() - y_offset)
+                    if g.contains(next_ap):
+                        g_ans.set_symbol(next_ap, '#')
+                        count += 1
+
+    #grid.display_grid(g_ans)
+    return g_ans.count_symbol('#')
--- aoc-2024/aoc-2024-day-08/solution-in-python3/test_solution.py
+++ aoc-2024/aoc-2024-day-08/solution-in-python3/test_solution.py
@@ -6,12 +6,12 @@ input_example = "AOC-2024-Day-08_Puzzle-Input-Example.txt"
 input_full = "AOC-2024-Day-08_Puzzle-Input-Full.txt"
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 14),
+        pytest.param(input_full, 252),  # not 238
     ],    
 )
 def test_solve_part1(filename, expected):
@@ -20,12 +20,12 @@ def test_solve_part1(filename, expected):
     assert expected == value
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 34),
+        pytest.param(input_full, 839),
     ],    
 )
 def test_solve_part2(filename, expected):
