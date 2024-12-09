0e422214ea123039583c4c85ff4af7d5b6314023
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-09 11:04:01 +0000

    AOC 2024 Day 09: Part 1: Refactored

--- aoc-2024/aoc-2024-day-09/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-09/solution-in-python3/solution.py
@@ -38,39 +38,14 @@ def to_block_representation_from(diskmap) -> deque:
     return q
 
 
-def to_block_representation_from_old(diskmap):
-    #print(f"DEBUG: diskmap={diskmap}")
-    rep = ''
-
-    index = 0
-    for i in range(len(diskmap)):        
-        ch_value = diskmap[i]
-        i_value = int(ch_value)
-
-        #print(f"DEBUG: i={i} i_value={i_value} ch_value={ch_value}")
-        if i % 2 == 0: # length of file
-            ch_index = str(index)
-            if i_value == 0:
-                rep += ch_index
-            for j in range(i_value):
-                rep += ch_index
-            index += 1
-        else: # length of free space
-            for j in range(i_value):
-                rep += '.'    
-    return rep
-
 
 def to_compacted_from(q_original:deque):
-    q = deque(q_original)
-    
+    q = deque(q_original)    
     q_compacted = deque()
-
     size = len(q)
-    j = len(q) -1
 
-    print(f"DEBUG: q={q} size={size}")
-    for i in range(size):
+    #print(f"DEBUG: q={q} size={size}")
+    for _ in range(size):
         if len(q) <= 0:
             break
 
@@ -89,24 +64,8 @@ def to_compacted_from(q_original:deque):
     return q_compacted
 
 
-def to_compacted_from_old(block_representation):
-    rep_queue = deque([*block_representation])
-    result =""
-    count_space = 0
-    while len(rep_queue) > 0:
-        ch = rep_queue.popleft()
-        if ch == '.':
-            count_space += 1
-            while len(rep_queue) > 0 and ch == '.':
-                ch = rep_queue.pop()
-            if '.' != ch:
-                result += ch
-        else:
-            result += ch
 
-    return result
-
-
+"""
 def to_checksum_from(compacted):
     total = 0
     for i in range(len(compacted)):        
@@ -118,7 +77,7 @@ def to_checksum_from(compacted):
         total += i_value * i
         
     return total
-
+"""
 
 def solve_part1(filename):
     input = fileutils.get_text_from(filename)
@@ -141,7 +100,7 @@ def solve_part1(filename):
 def solve_part2(filename):
     input = fileutils.get_text_from(filename)
     qbr = to_block_representation_from(input)
-    qc = to_compacted_from(qbr)
+    qc = to_compacted_from(qbr) # TODO: Change compaction approach
 
     #print(f"DEBUG: qc={qc}")
 
