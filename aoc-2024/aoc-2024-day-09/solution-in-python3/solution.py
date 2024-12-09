994806c8521a5dec547968acbc705f4c233c5845
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-09 11:00:05 +0000

    AOC 2024 Day 09: Solved Part 1

--- aoc-2024/aoc-2024-day-09/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-09/solution-in-python3/solution.py
@@ -1,8 +1,44 @@
 from helpers import fileutils
 from collections import deque
 
+def queue_to_string(q_original):
+    q = deque(q_original)
+    s = ""
+    if None == q:
+        return s
+    
+    while len(q) > 0:
+        v = q.popleft()
+        if None == v:
+            s += '.'
+        else:
+            s += str(v)
+    #print(f"DEBUG: s={s}")
+    return s
 
-def to_block_representation_from(diskmap):
+
+def to_block_representation_from(diskmap) -> deque:
+    if None == diskmap:
+        return None
+    
+    #print(f"DEBUG: diskmap={diskmap}")
+    q = deque()
+
+    file_id = 0
+    for i, c in enumerate(diskmap):        
+        v = int(c)
+        if i % 2 == 0: # length of file
+            for _ in range(v):
+                q.append(file_id)
+            file_id += 1
+        else: # length of free space
+            for _ in range(v):
+                q.append(None)
+    #print(f"DEBUG: q={q}")
+    return q
+
+
+def to_block_representation_from_old(diskmap):
     #print(f"DEBUG: diskmap={diskmap}")
     rep = ''
 
@@ -25,7 +61,35 @@ def to_block_representation_from(diskmap):
     return rep
 
 
-def to_compacted_from(block_representation):
+def to_compacted_from(q_original:deque):
+    q = deque(q_original)
+    
+    q_compacted = deque()
+
+    size = len(q)
+    j = len(q) -1
+
+    print(f"DEBUG: q={q} size={size}")
+    for i in range(size):
+        if len(q) <= 0:
+            break
+
+        v_i = q.popleft()
+        #print(f"DEBUG: {v_i}")
+        if v_i == None:
+            v_j = None
+            while len(q) > 0 and v_j == None:
+                v_j = q.pop()
+            if v_j is not None:
+                q_compacted.append(v_j)
+        else:
+            q_compacted.append(v_i)
+
+    #print(f"DEBUG: {q_compacted}")
+    return q_compacted
+
+
+def to_compacted_from_old(block_representation):
     rep_queue = deque([*block_representation])
     result =""
     count_space = 0
@@ -58,12 +122,35 @@ def to_checksum_from(compacted):
 
 def solve_part1(filename):
     input = fileutils.get_text_from(filename)
-    block_representation = to_block_representation_from(input)
-    compacted = to_compacted_from(block_representation)
-    #print(f"DEBUG: compacted={compacted}")
-    return to_checksum_from(compacted)
+    qbr = to_block_representation_from(input)
+    qc = to_compacted_from(qbr)
+
+    #print(f"DEBUG: qc={qc}")
+
+    ans = 0
+    i = 0
+    while len(qc) >= 1:
+        file_id = qc.popleft()
+        multiple = file_id * i
+        #print(f"DEBUG: file_id={file_id} i={i} mulitple = {multiple}")
+        i += 1
+        ans += multiple
+    return ans
 
 
 def solve_part2(filename):
     input = fileutils.get_text_from(filename)
-    return "TODO"
+    qbr = to_block_representation_from(input)
+    qc = to_compacted_from(qbr)
+
+    #print(f"DEBUG: qc={qc}")
+
+    ans = 0
+    i = 0
+    while len(qc) >= 1:
+        file_id = qc.popleft()
+        multiple = file_id * i
+        #print(f"DEBUG: file_id={file_id} i={i} mulitple = {multiple}")
+        i += 1
+        ans += multiple
+    return ans
--- aoc-2024/aoc-2024-day-09/solution-in-python3/test_solution.py
+++ aoc-2024/aoc-2024-day-09/solution-in-python3/test_solution.py
@@ -13,25 +13,30 @@ input_full = "AOC-2024-Day-09_Puzzle-Input-Full.txt"
     ],    
 )
 def test_to_block_representation_from(diskmap, expected):
-    block_representation = solution.to_block_representation_from(diskmap)
-    assert expected == block_representation
-
-
-@pytest.mark.parametrize(
-    "block_representation, expected",
-    [
-        #pytest.param('0..111....22222', '022111222......'),
-        #pytest.param('00...111...2...333.44.5555.6666.777.888899', '0099811188827773336446555566..............'),
-        pytest.param('0..111....22222', '022111222'),
-        pytest.param('00...111...2...333.44.5555.6666.777.888899', '0099811188827773336446555566'),
-    ],    
-)
-def test_to_compacted_from(block_representation, expected):
-    compacted = solution.to_compacted_from(block_representation)
-    assert expected == compacted
-
+    q = solution.to_block_representation_from(diskmap)
+    assert expected ==  solution.queue_to_string(q)
 
 #@pytest.mark.skip(reason="TODO: Ignore until implemented")
+@pytest.mark.parametrize(
+    "diskmap, block_representation, expected",
+    [
+        #pytest.param('12345', '0..111....22222', '022111222......'),
+        #pytest.param('00...111...2...333.44.5555.6666.777.888899', '00...111...2...333.44.5555.6666.777.888899', '0099811188827773336446555566..............'),
+        pytest.param('12345', '0..111....22222', '022111222'),
+        pytest.param('2333133121414131402', '00...111...2...333.44.5555.6666.777.888899', '0099811188827773336446555566'),
+    ],    
+)
+def test_to_compacted_from(diskmap, block_representation, expected):
+    q = solution.to_block_representation_from(diskmap)
+    #print(f"DEBUG: q={q}")
+    assert block_representation == solution.queue_to_string(q)
+    #print(f"DEBUG: q={q}")
+
+    q_compacted = solution.to_compacted_from(q)
+    assert expected == solution.queue_to_string(q_compacted)
+
+
+@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "compacted, expected",
     [
@@ -48,7 +53,7 @@ def test_to_checksum_from(compacted, expected):
     "filename, expected",
     [
         pytest.param(input_example, 1928), 
-        pytest.param(input_full, "TODO"), # 89514618001 too low!
+        pytest.param(input_full, 6288599492129), # 89514618001 too low!
     ],    
 )
 def test_solve_part1(filename, expected):
@@ -56,11 +61,11 @@ def test_solve_part1(filename, expected):
     assert expected == value
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, "TODO"),
+        pytest.param(input_example, 2858),
         #pytest.param(input_full, "TODO"),
     ],    
 )
