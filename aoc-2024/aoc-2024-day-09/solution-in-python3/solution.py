a6b0e0157f0396db313d589edd4e439d776c5871
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-09 07:02:52 +0000

    AOC 2024 Day 09: WIP: Matching example results, failing Part 1 :-(

--- aoc-2024/aoc-2024-day-09/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-09/solution-in-python3/solution.py
@@ -1,11 +1,69 @@
 from helpers import fileutils
+from collections import deque
+
+
+def to_block_representation_from(diskmap):
+    #print(f"DEBUG: diskmap={diskmap}")
+    rep = ''
+
+    index = 0
+    for i in range(len(diskmap)):        
+        ch_value = diskmap[i]
+        i_value = int(ch_value)
+
+        #print(f"DEBUG: i={i} i_value={i_value} ch_value={ch_value}")
+        if i % 2 == 0: # length of file
+            ch_index = str(index)
+            if i_value == 0:
+                rep += ch_index
+            for j in range(i_value):
+                rep += ch_index
+            index += 1
+        else: # length of free space
+            for j in range(i_value):
+                rep += '.'    
+    return rep
+
+
+def to_compacted_from(block_representation):
+    rep_queue = deque([*block_representation])
+    result =""
+    count_space = 0
+    while len(rep_queue) > 0:
+        ch = rep_queue.popleft()
+        if ch == '.':
+            count_space += 1
+            while len(rep_queue) > 0 and ch == '.':
+                ch = rep_queue.pop()
+            if '.' != ch:
+                result += ch
+        else:
+            result += ch
+
+    return result
+
+
+def to_checksum_from(compacted):
+    total = 0
+    for i in range(len(compacted)):        
+        ch_value = compacted[i]
+        if '.' == ch_value:
+            break 
+        i_value = int(ch_value)
+        #print(f"DEBUG: ch_value={ch_value} i={i}")
+        total += i_value * i
+        
+    return total
+
 
 def solve_part1(filename):
-    lines = fileutils.get_file_lines_from(filename)
+    input = fileutils.get_text_from(filename)
+    block_representation = to_block_representation_from(input)
+    compacted = to_compacted_from(block_representation)
+    #print(f"DEBUG: compacted={compacted}")
+    return to_checksum_from(compacted)
 
-    return "TODO"
 
 def solve_part2(filename):
-    lines = fileutils.get_file_lines_from(filename)
-
+    input = fileutils.get_text_from(filename)
     return "TODO"
--- aoc-2024/aoc-2024-day-09/solution-in-python3/test_solution.py
+++ aoc-2024/aoc-2024-day-09/solution-in-python3/test_solution.py
@@ -5,18 +5,54 @@ import solution
 input_example = "AOC-2024-Day-09_Puzzle-Input-Example.txt"
 input_full = "AOC-2024-Day-09_Puzzle-Input-Full.txt"
 
+@pytest.mark.parametrize(
+    "diskmap, expected",
+    [
+        pytest.param('12345', '0..111....22222'),
+        pytest.param('2333133121414131402', '00...111...2...333.44.5555.6666.777.888899'),
+    ],    
+)
+def test_to_block_representation_from(diskmap, expected):
+    block_representation = solution.to_block_representation_from(diskmap)
+    assert expected == block_representation
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+
+@pytest.mark.parametrize(
+    "block_representation, expected",
+    [
+        #pytest.param('0..111....22222', '022111222......'),
+        #pytest.param('00...111...2...333.44.5555.6666.777.888899', '0099811188827773336446555566..............'),
+        pytest.param('0..111....22222', '022111222'),
+        pytest.param('00...111...2...333.44.5555.6666.777.888899', '0099811188827773336446555566'),
+    ],    
+)
+def test_to_compacted_from(block_representation, expected):
+    compacted = solution.to_compacted_from(block_representation)
+    assert expected == compacted
+
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
+@pytest.mark.parametrize(
+    "compacted, expected",
+    [
+        pytest.param('0099811188827773336446555566..............', 1928),
+    ],    
+)
+def test_to_checksum_from(compacted, expected):
+    checksum = solution.to_checksum_from(compacted)
+    assert expected == checksum
+
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 1928), 
+        pytest.param(input_full, "TODO"), # 89514618001 too low!
     ],    
 )
 def test_solve_part1(filename, expected):
-    value = solution.solve_part1(filename)
-    
+    value = solution.solve_part1(filename)    
     assert expected == value
 
 
@@ -24,11 +60,10 @@ def test_solve_part1(filename, expected):
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, "TODO"),
+        #pytest.param(input_full, "TODO"),
     ],    
 )
 def test_solve_part2(filename, expected):
-    value = solution.solve_part2(filename)
-    
+    value = solution.solve_part2(filename)    
     assert expected == value
