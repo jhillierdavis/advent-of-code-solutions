79280e57d0ade30e15febf7e3e5c912d4b168929
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-10 09:00:11 +0000

    AOC 2024 Day 10: Solved Parts 1 and 2

--- aoc-2024/aoc-2024-day-10/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-10/solution-in-python3/solution.py
@@ -1,11 +1,159 @@
-from helpers import fileutils
+import string
+from helpers import fileutils, grid
+
+"""
+def get_trailhead_paths(g, cp, value)
+    cp = int(g.get_symbol(cp))
+    neighbours = g.get_cardinal_point_neighbours(cp)
+    for np in neighbours: 
+        nv = int(g.get_symbol(cp))        
+        if nv == cp + 1:            
+"""
+
+ans = 0
+def count_trailhead_paths(g, sp):
+    global ans
+    sv = int(g.get_symbol(sp))
+
+    if sv == 9:
+        ans += 1
+        return
+
+    neighbours = g.get_cardinal_point_neighbours(sp)
+    for np in neighbours:
+        
+        nv = int(g.get_symbol(np))        
+
+        if nv == sv + 1:     
+            count_trailhead_paths(g, np)
+    return
+
+def get_grid_from(filename):
+    lines = fileutils.get_file_lines_from(filename)
+
+    g = grid.lines_to_grid(lines)
+    #grid.display_grid(g)
+    return g
+
+
+def count_starting_points_from(g):
+    starting_points = g.get_matching_symbol_coords('0')
+    return len(starting_points)
+
+def get_value(g, p):
+    s = g.get_symbol(p)
+    if s in string.digits:   
+        return int(s)
+    return -1
+
+
+def is_trailhead_step(g, cp, np):
+    cv = get_value(g, cp)
+    nv = get_value(g, np)
+    return nv - cv == 1
+        
+def is_trailhead_end(g, cp):
+    cv = get_value(g, cp)
+    return cv == 9
+
+def count_trailhead_paths(g, cp):
+    
+
+    count = 0
+    neighbours = g.get_cardinal_point_neighbours(cp)
+    for np in neighbours:
+        if is_trailhead_step(g, cp, np):
+            #print(f"DBEUG: Step: cv={cv} nv={nv} cp={cp} np={np}")
+            if is_trailhead_end(g, np):        
+                #print(f"DBEUG: End: np={np}")
+                g.set_symbol(np, 'X')            
+            else:
+                count += count_trailhead_paths(g, np)
+    return count            
+    
+        
+def count_trailheads_from(g):
+    starting_points = g.get_matching_symbol_coords('0')
+
+    count = 0
+    for sp in starting_points:   
+        print(f"DEBUG: Start sp={sp}")     
+        count += count_trailhead_paths(g, sp)
+    
+
+    #grid.display_grid(g)
+    return g.count_symbol('X')
+
+
+def count_trailheads_startpoints(g):
+    end_points = g.get_matching_symbol_coords('0')
+
+    count = 0
+    for sp in end_points:   
+        count += count_trailhead_paths(g, sp)
+    return g.count_symbol('X')
+
+def is_stepdown(g, cp, np):
+    cv = get_value(g, cp)
+    nv = get_value(g, np)
+    return cv - nv == 1
+        
+def is_trailhead_start(g, cp):
+    cv = get_value(g, cp)
+    return cv == 0
+
+def find_trailheads(g, cp):    
+    neighbours = g.get_cardinal_point_neighbours(cp)
+    for np in neighbours:
+        if is_stepdown(g, cp, np):
+            #print(f"DBEUG: Step: cv={cv} nv={nv} cp={cp} np={np}")
+            if is_trailhead_start(g, np):        
+                #print(f"DBEUG: End: np={np}")
+                g.set_symbol(np, 'X')            
+            else:
+                find_trailheads(g, np)
 
 def solve_part1(filename):
-    lines = fileutils.get_file_lines_from(filename)
+    g = get_grid_from(filename)
+    
+    end_points = g.get_matching_symbol_coords('9')
 
-    return "TODO"
+    count = 0
+    for ep in end_points:
+        gc = g.clone()
+        find_trailheads(gc, ep)
+        count += gc.count_symbol('X')
+    return count
+
+def is_stepup(g, cp, np):
+    cv = get_value(g, cp)
+    nv = get_value(g, np)
+    return nv - cv == 1
+        
+def is_trailend(g, cp):
+    cv = get_value(g, cp)
+    return cv == 9
+
+
+def find_trailend(g, cp):    
+    count = 0
+    neighbours = g.get_cardinal_point_neighbours(cp)    
+    for np in neighbours:
+        if is_stepup(g, cp, np):
+            #print(f"DBEUG: Step: cv={cv} nv={nv} cp={cp} np={np}")
+            if is_trailend(g, np):        
+                count += 1
+            else:
+                count += find_trailend(g, np)
+    return count
+
 
 def solve_part2(filename):
-    lines = fileutils.get_file_lines_from(filename)
+    g = get_grid_from(filename)
+    
+    start_points = g.get_matching_symbol_coords('0')
 
-    return "TODO"
+    count = 0
+    for sp in start_points:
+        count += find_trailend(g, sp)
+    return count
--- aoc-2024/aoc-2024-day-10/solution-in-python3/test_solution.py
+++ aoc-2024/aoc-2024-day-10/solution-in-python3/test_solution.py
@@ -6,29 +6,27 @@ input_example = "AOC-2024-Day-10_Puzzle-Input-Example.txt"
 input_full = "AOC-2024-Day-10_Puzzle-Input-Full.txt"
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 36),
+        pytest.param(input_full, 535),
     ],    
 )
 def test_solve_part1(filename, expected):
-    value = solution.solve_part1(filename)
-    
+    value = solution.solve_part1(filename)    
     assert expected == value
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 81),
+        pytest.param(input_full, 1186),
     ],    
 )
 def test_solve_part2(filename, expected):
-    value = solution.solve_part2(filename)
-    
+    value = solution.solve_part2(filename)    
     assert expected == value
