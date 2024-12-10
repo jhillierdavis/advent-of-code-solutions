6cfd3d9610b16548084c425f8a35f8232a37ac58
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-10 09:51:32 +0000

    AOC 2024 Day 10: Refactored: added hints & made more DRY

--- aoc-2024/aoc-2024-day-10/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-10/solution-in-python3/solution.py
@@ -1,11 +1,12 @@
 import string
-from helpers import fileutils, grid
+from helpers import fileutils, grid, point
 
 symbol_trailhead = '0'
 symbol_trailend = '9'
 symbol_marker = 'X'
 
-def get_grid_from(filename):
+
+def get_grid_from(filename:str) -> grid.Grid2D:
     lines = fileutils.get_file_lines_from(filename)
 
     g = grid.lines_to_grid(lines)
@@ -13,57 +14,52 @@ def get_grid_from(filename):
     return g
 
 
-def get_value(g, p):
+def get_value(g:grid.Grid2D, p:point.Point2D):
     s = g.get_symbol(p)
     if s in string.digits:   
         return int(s)
     return -1
 
 
-def is_trailhead(g, cp):
+def is_trailhead(g:grid.Grid2D, cp:point.Point2D):
     cv = get_value(g, cp)
     return cv == 0
 
 
-def is_trailend(g, cp):
+def is_trailend(g:grid.Grid2D, cp:point.Point2D):
     cv = get_value(g, cp)
     return cv == 9
 
 
-def get_step_size(g, cp, np):
+def get_step_size(g:grid.Grid2D, cp:point.Point2D, np:point.Point2D):
     cv = get_value(g, cp)
     nv = get_value(g, np)
     return cv - nv
 
 
-def is_stepdown(g, cp, np):
+def is_stepdown(g:grid.Grid2D, cp:point.Point2D, np:point.Point2D) -> bool:
     return get_step_size(g, cp, np) == 1
 
 
-def is_stepup(g, cp, np):
+def is_stepup(g:grid.Grid2D, cp:point.Point2D, np:point.Point2D) -> bool:
     return get_step_size(g, cp, np) == -1
 
 
-def is_trailhead_start(g, cp):
-    cv = get_value(g, cp)
-    return cv == 0
-
-
-def mark_trailheads(g, cp):    
+def mark_trailheads(g:grid.Grid2D, cp:point.Point2D) -> None:
     neighbours = g.get_cardinal_point_neighbours(cp)
     for np in neighbours:
         if is_stepdown(g, cp, np):
             #print(f"DBEUG: Step: cv={cv} nv={nv} cp={cp} np={np}")
-            if is_trailhead_start(g, np):        
+            if is_trailhead(g, np):        
                 #print(f"DBEUG: End: np={np}")
                 g.set_symbol(np, symbol_marker)            
             else:
                 mark_trailheads(g, np)
 
 
-def solve_part1(filename):
+def solve_part1(filename:str) -> int:
     g = get_grid_from(filename)
-    
+
     end_points = g.get_matching_symbol_coords(symbol_trailend)
 
     count = 0    
@@ -75,7 +71,7 @@ def solve_part1(filename):
     return count
 
 
-def count_trailends(g, cp):    
+def count_trailends(g:grid.Grid2D, cp:point.Point2D) -> int:    
     count = 0
     neighbours = g.get_cardinal_point_neighbours(cp)    
     for np in neighbours:
@@ -88,7 +84,7 @@ def count_trailends(g, cp):
     return count
 
 
-def solve_part2(filename):
+def solve_part2(filename:str) -> int:
     g = get_grid_from(filename)
     
     start_points = g.get_matching_symbol_coords(symbol_trailhead)
