fa94a83e90bfaf00a941cb70c3b47f0d80bd9c8c
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-11 06:19:21 +0000

    AOC 2024 Day 11: Solved Part 1

--- aoc-2024/aoc-2024-day-11/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-11/solution-in-python3/solution.py
@@ -1,11 +1,50 @@
 from helpers import fileutils
 
-def solve_part1(filename):
-    lines = fileutils.get_file_lines_from(filename)
+def count(values):
+    stones = values.split(' ')
+    return len(stones)
 
-    return "TODO"
+def split_value(value):
+    size = len(value)
+    midpoint = (size // 2)
+    left = value[0:midpoint]
+    right = value[midpoint:]
+    #while right.startswith('0') and len(right) > 1:
+    #    right = right[1:]
+    return left + " " + str(int(right))
+
+def transform(value):
+    if value == '0':
+        return '1'
+    
+    size = len(value)
+    if size % 2  == 0: # Even
+        return split_value(value)
+    
+    num = int(value)
+    return str(2024 * num)
+
+
+def transform_stones(values):
+    stones = values.split(' ')
+    evolved = ''
+    for s in stones:
+        evolved += transform(s) + ' '
+    return evolved.strip()
+
+def evolve_stones(values, blinks):
+    evolved = values
+    for _ in range(blinks):
+        evolved = transform_stones(evolved)
+    return evolved
+
+def solve_part1(filename):
+    line = fileutils.get_text_from(filename)
+    evolved = evolve_stones(line, 25)
+    return count(evolved)
+
 
 def solve_part2(filename):
-    lines = fileutils.get_file_lines_from(filename)
-
-    return "TODO"
+    line = fileutils.get_text_from(filename)
+    evolved = evolve_stones(line, 75)
+    return count(evolved)
--- aoc-2024/aoc-2024-day-11/solution-in-python3/test_solution.py
+++ aoc-2024/aoc-2024-day-11/solution-in-python3/test_solution.py
@@ -6,12 +6,55 @@ input_example = "AOC-2024-Day-11_Puzzle-Input-Example.txt"
 input_full = "AOC-2024-Day-11_Puzzle-Input-Full.txt"
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+
+@pytest.mark.parametrize(
+    "input, blinks, expected",
+    [    
+        pytest.param('0', 1, '1'),
+        pytest.param('1', 1, '2024'),
+        pytest.param('10', 1, '1 0'),
+        pytest.param('99', 1, '9 9'),
+        pytest.param('999', 1, '2021976'),
+        pytest.param('0 1 10 99 999', 1, '1 2024 1 0 9 9 2021976'),     
+        pytest.param('125 17', 1, '253000 1 7'),
+        pytest.param('253000 1 7', 1, '253 0 2024 14168'),
+        pytest.param('125 17', 2, '253 0 2024 14168'),
+        pytest.param('125 17', 3, '512072 1 20 24 28676032'),
+        pytest.param('125 17', 4, '512 72 2024 2 0 2 4 2867 6032'),
+        pytest.param('125 17', 5, '1036288 7 2 20 24 4048 1 4048 8096 28 67 60 32'),
+        pytest.param('125 17', 6, '2097446912 14168 4048 2 0 2 4 40 48 2024 40 48 80 96 2 8 6 7 6 0 3 2'),
+    ],    
+)
+def test_evolve_stones(input, blinks, expected):
+    stones = solution.evolve_stones(input, blinks)
+    assert expected == stones
+
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
+@pytest.mark.parametrize(
+    "input, blinks, expected",
+    [
+        pytest.param('125 17', 1, 3),
+        pytest.param('125 17', 2, 4),
+        pytest.param('125 17', 3, 5),
+        pytest.param('125 17', 4, 9),
+        pytest.param('125 17', 5, 13),
+        pytest.param('125 17', 6, 22),
+        pytest.param('125 17', 25, 55312),
+    ],    
+)
+def test_count_stones_after_blinks(input, blinks, expected):
+    stones = solution.evolve_stones(input, blinks)
+    assert expected == solution.count(stones)
+
+
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 55312),
+        pytest.param(input_full, 197157),
     ],    
 )
 def test_solve_part1(filename, expected):
