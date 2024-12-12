2a1f487d4e38b08bca93491c112e19fc93788591
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-12 08:22:42 +0000

    AOC 2024 Day 12: Solved Part 1

--- aoc-2024/aoc-2024-day-12/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-12/solution-in-python3/solution.py
@@ -1,11 +1,121 @@
-from helpers import fileutils
+from collections import defaultdict, deque
+
+from helpers import fileutils, grid, point
+
+
+def get_symbol_to_position_map(g:grid.Grid2D) -> defaultdict:
+    map = defaultdict(list)
+
+    for h in range(g.get_height()):
+        for w in range(g.get_width()):
+            p = point.Point2D(w,h)
+            s = g.get_symbol(p)
+            if s in map:
+                continue
+
+            sps = g.get_points_matching(s)
+            map[s] = sps
+
+    #print(f"DEBUG: map.keys={map.keys()}")
+    #print(f"DEBUG: map={map}")
+    return map    
+
+def populate_continguous_region(g:grid.Grid2D, s, sp, region):
+    
+    nps = g.get_cardinal_point_neighbours(sp)
+    for n in nps:
+        if n in region:
+            continue
+
+        if g.get_symbol(n) == s:
+            region.add(n)
+            populate_continguous_region(g, s, n, region)
+        
+
+
+
+def get_contiguous_region_containing(g:grid.Grid2D, s, sp):
+    region = set()
+    region.add(sp)
+    populate_continguous_region(g, s, sp, region)
+    return region
+
+
+def get_contiguous_regions(g:grid.Grid2D, s, sps):
+    regions = []
+
+    for p in sps:
+        region = get_contiguous_region_containing(g, s, p)
+        if region not in regions:
+            regions.append(region)
+    return regions
+
+
+
+def count_regions(g:grid.Grid2D) -> int:
+    map = get_symbol_to_position_map(g)
+
+    count = 0
+    for s, sps in map.items():
+        #print(f"DEBUG; sps={sps}")
+        # For each symbol count distinct regions
+        regions = get_contiguous_regions(g, s, sps)
+        region_count = len(regions)
+        #print(f"DEBUG: s={s} region_count={region_count} regions={regions}")
+        count += region_count
+
+    return count
+
+def calculate_area(region):
+    return len(region)
+
+def calculate_perimeter(g, s, region):
+    size =  len(region)
+
+    if size == 0:
+        return 0
+    
+    if size == 1:
+        return 4
+    
+    count = 0
+    for p in region:
+        nps = g.get_cardinal_point_neighbours(p)
+        n_count = 0
+        for n in nps:
+            ns = g.get_symbol(n)
+            if ns == s:
+                n_count += 1
+        count += 4 - n_count
+
+    return count
 
 def solve_part1(filename):
     lines = fileutils.get_file_lines_from(filename)
+    g = grid.lines_to_grid(lines)
+    grid.display_grid(g)
 
-    return "TODO"
+    map = get_symbol_to_position_map(g)
+
+    ans = 0
+    for s, sps in map.items():
+        #print(f"DEBUG; sps={sps}")
+        # For each symbol count distinct regions
+        regions = get_contiguous_regions(g, s, sps)        
+
+        for r in regions:
+            area = calculate_area(r)
+            perimeter = calculate_perimeter(g, s, r)
+            price = area * perimeter
+            #print(f"DEBUG: s={s} area={area} r={r}")
+            print(f"DEBUG: s={s} area={area} perimeter={perimeter} price={price}")
+            ans += price
+            
+    return ans
+
 
 def solve_part2(filename):
     lines = fileutils.get_file_lines_from(filename)
-
+    g = grid.lines_to_grid(lines)
+    grid.display_grid(g)
     return "TODO"
--- aoc-2024/aoc-2024-day-12/solution-in-python3/test_solution.py
+++ aoc-2024/aoc-2024-day-12/solution-in-python3/test_solution.py
@@ -1,23 +1,46 @@
 import pytest
 
+from helpers import fileutils, grid
 import solution
 
+example_1 = "example-1.txt"
+example_2 = "example-2.txt"
 input_example = "AOC-2024-Day-12_Puzzle-Input-Example.txt"
 input_full = "AOC-2024-Day-12_Puzzle-Input-Full.txt"
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(example_1, 5),
+        pytest.param(example_2, 5),
+        pytest.param(input_example, 11),
+    ],    
+)
+def test_count_regions(filename, expected):
+    lines = fileutils.get_file_lines_from(filename)
+    g = grid.lines_to_grid(lines)
+
+    assert expected == solution.count_regions(g)
+
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
+@pytest.mark.parametrize(
+    "filename, expected",
+    [
+        pytest.param(example_1, 140),
+        pytest.param(example_2, 772),
+        pytest.param(input_example, 1930),
+        pytest.param(input_full, 1396562),
     ],    
 )
 def test_solve_part1(filename, expected):
-    value = solution.solve_part1(filename)
-    
+    value = solution.solve_part1(filename)    
     assert expected == value
+
+
+
 
 
 @pytest.mark.skip(reason="TODO: Ignore until implemented")
@@ -29,6 +52,5 @@ def test_solve_part1(filename, expected):
     ],    
 )
 def test_solve_part2(filename, expected):
-    value = solution.solve_part2(filename)
-    
+    value = solution.solve_part2(filename)    
     assert expected == value
