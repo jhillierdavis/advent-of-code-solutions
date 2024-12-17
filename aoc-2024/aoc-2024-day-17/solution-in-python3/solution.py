f9f36b6e36abaffe6ab44e9851085a5149ad0b48
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-17 07:08:13 +0000

    AOC 2024 Day 17: Solved Part 1

--- aoc-2024/aoc-2024-day-17/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-17/solution-in-python3/solution.py
@@ -1,11 +1,149 @@
-from helpers import fileutils
+import math
+
+from helpers import fileutils, numutils
+
+def operand_combo(value, reg_map):
+    if value >=0 and value <= 3:
+        return value 
+    if value == 4:
+        return reg_map['A']
+    if value == 5:
+        return reg_map['B']
+    if value == 6:
+        return reg_map['C']
+    raise Exception(f'Invalid value: {value}')
+
+# Opcode 0
+def operand_adv(value, reg_map):
+    numerator = reg_map['A']
+    denominator = 2 ** operand_combo(value, reg_map)
+    result = int ( numerator / denominator )
+    reg_map['A'] = result
+    print(f"DEBUG: [operand_adv] result={result} value={value} numerator={numerator} demoinator={denominator} reg_map={reg_map}")
+    return result
+
+
+# Opcode 1
+def operand_bxl(value, reg_map):
+    result = reg_map['B'] ^ value
+    reg_map['B'] = result
+    print(f"DEBUG: [operand_bxl] result={result} value={value} reg_map={reg_map}")
+    return result
+
+
+# Opcode 2
+def operand_bst(value, reg_map):
+    result = operand_combo(value, reg_map) % 8
+    reg_map['B'] = result
+    print(f"DEBUG: [operand_bst] result={result} value={value} reg_map={reg_map}")
+    return result
+
+
+# Opcode 3
+def operand_jnz(value, reg_map):
+    if 0 == reg_map['A']:
+        return None 
+    result = value
+    print(f"DEBUG: [operand_jnz] result={result} value={value} reg_map={reg_map}")
+    return result
+
+# Opcode 4
+def operand_bxc(value, reg_map):
+    result = reg_map['B'] ^ reg_map['C']
+    reg_map['B'] = result
+    print(f"DEBUG: [operand_bxc] result={result} value={value} reg_map={reg_map}")
+    return result
+
+
+# Opcode 5
+def operand_out(value, reg_map):
+    result = operand_combo(value, reg_map) % 8
+    print(f"DEBUG: [operand_out] result={result} value={value} reg_map={reg_map}")
+    return result
+
+# Opcode 6
+def operand_bdv(value, reg_map):
+    numerator = reg_map['A']
+    denominator = 2 ** operand_combo(value, reg_map)
+    result = int ( numerator / denominator )
+    reg_map['B'] = result
+    print(f"DEBUG: [operand_bdv] result={result} value={value} reg_map={reg_map}")
+    return result
+
+# Opcode 7
+def operand_cdv(value, reg_map):
+    numerator = reg_map['A']
+    denominator = 2 ** operand_combo(value, reg_map)
+    result = int ( numerator / denominator )
+    reg_map['C'] = result
+    print(f"DEBUG: [operand_cdv] result={result} value={value} reg_map={reg_map}")
+    return result
+
+
+def process(instructions, reg_map, offset=0):
+    
+
+    result = ""
+    ops = instructions.split(',')
+    size = len(ops)
+    assert offset < size
+
+    for i in range(offset, len(ops)):
+        value = int(ops[i])
+        if numutils.is_even(i):
+            opcode = value            
+        else:
+            operand = value
+            print(f"DEBUG: opcode={opcode} operand={operand} reg_map={reg_map}")
+            output = None
+            if opcode == 0:
+                output = operand_adv(value, reg_map)
+            elif opcode == 1:
+                output = operand_bxl(value, reg_map)
+            elif opcode == 2:
+                output = operand_bst(value, reg_map)
+            elif opcode == 3:
+                output = operand_jnz(value, reg_map)
+                if output is not None:
+                    result += process(instructions, reg_map, offset=output)
+            elif opcode == 4:
+                output = operand_bxc(value, reg_map)
+            elif opcode == 5:
+                output = operand_out(value, reg_map)
+                result += str(output) + ','
+            elif opcode == 6:
+                output = operand_bdv(value, reg_map)
+            elif opcode == 7:
+                output = operand_cdv(value, reg_map)
+
+            print(f"\n")
+            
+
+
+    return result.strip(',') # Remove any last comma
+
 
 def solve_part1(filename):
-    lines = fileutils.get_file_lines_from(filename)
+    #lines = fileutils.get_file_lines_from(filename)
+    reg_lines = fileutils.get_lines_before_empty_from_file(filename)
+    data_line = fileutils.get_lines_after_empty_from_file(filename)[0]
+    #print(f"DEBUG: {reg_lines}")
+    #print(f"DEBUG: {data_line}")
 
-    return "TODO"
+    
+    reg_map = {}
+    for l in reg_lines:
+        suffix = l[len('Register '):]
+        values = suffix.split(': ')
+        reg_map[values[0]] = int(values[1])
+    print(f"DEBUG: reg_map={reg_map}")
+
+    #print(f"DEBUG: {reg_lines}")
+
+    instr = data_line[len('Program: '):]    
+    return process(instr, reg_map)
+
 
 def solve_part2(filename):
     lines = fileutils.get_file_lines_from(filename)
-
     return "TODO"
--- aoc-2024/aoc-2024-day-17/solution-in-python3/test_solution.py
+++ aoc-2024/aoc-2024-day-17/solution-in-python3/test_solution.py
@@ -6,29 +6,54 @@ input_example = "AOC-2024-Day-17_Puzzle-Input-Example.txt"
 input_full = "AOC-2024-Day-17_Puzzle-Input-Full.txt"
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+@pytest.mark.parametrize(
+    "reg_a, reg_b, reg_c, input, expected_reg_a, expected_reg_b, expected_reg_c, expected_output",
+    [
+        pytest.param(0,0,9,"2,6",0,1,9,None),
+        pytest.param(10,0,0,"5,0,5,1,5,4",10,0,0,"0,1,2"),
+        pytest.param(2024,0,0,"0,1,5,4,3,0",0,0,0,"4,2,5,6,7,7,7,7,3,1,0"),
+        pytest.param(0,29,0,"1,7",0,26,0,""),
+        pytest.param(0,2024,43690,"4,0",0,44354,43690,""),
+    ],    
+)
+def test_process(reg_a, reg_b, reg_c, input, expected_reg_a, expected_reg_b, expected_reg_c, expected_output):
+    reg_map = {}
+    reg_map['A'] = reg_a
+    reg_map['B'] = reg_b
+    reg_map['C'] = reg_c
+
+    output = solution.process(input, reg_map)
+    
+    assert reg_map['A'] == expected_reg_a
+    assert reg_map['B'] == expected_reg_b
+    assert reg_map['C'] == expected_reg_c
+    if None != expected_output:
+        assert output == expected_output
+
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, "4,6,3,5,6,3,5,2,1,0"),
+        pytest.param(input_full, "3,1,4,3,1,7,1,6,3"),
     ],    
 )
 def test_solve_part1(filename, expected):
-    value = solution.solve_part1(filename)
-    
+    value = solution.solve_part1(filename)    
     assert expected == value
 
+
+input_example_part2 = "AOC-2024-Day-17_Puzzle-Input-Example-Part2.txt"
 
 @pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
+        pytest.param(input_example, 117440),
         #pytest.param(input_full, -1),
     ],    
 )
 def test_solve_part2(filename, expected):
-    value = solution.solve_part2(filename)
-    
+    value = solution.solve_part2(filename)    
     assert expected == value
