3987e05c1f3a4c43c6c39ddc20401c21412170d2
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-17 23:42:07 +0000

    AOC 2024 Day 17: Solved Part 2

--- aoc-2024/aoc-2024-day-17/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-17/solution-in-python3/solution.py
@@ -1,6 +1,8 @@
 import math
+from functools import cache
 
 from helpers import fileutils, numutils
+
 
 def operand_combo(value, reg_map):
     if value >=0 and value <= 3:
@@ -13,30 +15,31 @@ def operand_combo(value, reg_map):
         return reg_map['C']
     raise Exception(f'Invalid value: {value}')
 
+
 # Opcode 0
 def operand_adv(value, reg_map):
     numerator = reg_map['A']
     denominator = 2 ** operand_combo(value, reg_map)
     result = int ( numerator / denominator )
     reg_map['A'] = result
-    print(f"DEBUG: [operand_adv] result={result} value={value} numerator={numerator} demoinator={denominator} reg_map={reg_map}")
-    return result
+    #print(f"DEBUG: [operand_adv] result={result} value={value} numerator={numerator} demoinator={denominator} reg_map={reg_map}")
+    #return result
 
 
 # Opcode 1
 def operand_bxl(value, reg_map):
     result = reg_map['B'] ^ value
     reg_map['B'] = result
-    print(f"DEBUG: [operand_bxl] result={result} value={value} reg_map={reg_map}")
-    return result
+    #print(f"DEBUG: [operand_bxl] result={result} value={value} reg_map={reg_map}")
+    #return result
 
 
 # Opcode 2
 def operand_bst(value, reg_map):
     result = operand_combo(value, reg_map) % 8
     reg_map['B'] = result
-    print(f"DEBUG: [operand_bst] result={result} value={value} reg_map={reg_map}")
-    return result
+    #print(f"DEBUG: [operand_bst] result={result} value={value} reg_map={reg_map}")
+    #return result
 
 
 # Opcode 3
@@ -44,22 +47,24 @@ def operand_jnz(value, reg_map):
     if 0 == reg_map['A']:
         return None 
     result = value
-    print(f"DEBUG: [operand_jnz] result={result} value={value} reg_map={reg_map}")
+    #print(f"DEBUG: [operand_jnz] result={result} value={value} reg_map={reg_map}")
     return result
+
 
 # Opcode 4
 def operand_bxc(value, reg_map):
     result = reg_map['B'] ^ reg_map['C']
     reg_map['B'] = result
-    print(f"DEBUG: [operand_bxc] result={result} value={value} reg_map={reg_map}")
-    return result
+    ##print(f"DEBUG: [operand_bxc] result={result} value={value} reg_map={reg_map}")
+    #return result
 
 
 # Opcode 5
 def operand_out(value, reg_map):
     result = operand_combo(value, reg_map) % 8
-    print(f"DEBUG: [operand_out] result={result} value={value} reg_map={reg_map}")
+    #print(f"DEBUG: [operand_out] result={result} value={value} reg_map={reg_map}")
     return result
+
 
 # Opcode 6
 def operand_bdv(value, reg_map):
@@ -67,8 +72,9 @@ def operand_bdv(value, reg_map):
     denominator = 2 ** operand_combo(value, reg_map)
     result = int ( numerator / denominator )
     reg_map['B'] = result
-    print(f"DEBUG: [operand_bdv] result={result} value={value} reg_map={reg_map}")
-    return result
+    #print(f"DEBUG: [operand_bdv] result={result} value={value} reg_map={reg_map}")
+    #return result
+
 
 # Opcode 7
 def operand_cdv(value, reg_map):
@@ -76,25 +82,40 @@ def operand_cdv(value, reg_map):
     denominator = 2 ** operand_combo(value, reg_map)
     result = int ( numerator / denominator )
     reg_map['C'] = result
-    print(f"DEBUG: [operand_cdv] result={result} value={value} reg_map={reg_map}")
-    return result
+    #print(f"DEBUG: [operand_cdv] result={result} value={value} reg_map={reg_map}")
+    #return result
 
 
-def process(instructions, reg_map, offset=0):
-    
+#cache = {}
 
-    result = ""
-    ops = instructions.split(',')
-    size = len(ops)
+def process(instr_list, reg_map, offset=0):
+    #print(f"DEBUG: instr_list={instr_list} {type(instr_list)}")
+    result = []
+    #ops = instructions.split(',')
+    #size = len(ops)
+    #assert offset < size
+
+    """
+    key = (instructions, reg_map['A'], reg_map['B'], reg_map['C'], offset)  
+    if key in cache:
+        value = cache[key]        
+        #print(f"Cache key={key} value={value}")
+        reg_map['A'] = value[1]
+        reg_map['B'] = value[2]
+        reg_map['C'] = value[3]
+        return value[0]
+    """
+
+    size = len(instr_list)
     assert offset < size
 
-    for i in range(offset, len(ops)):
-        value = int(ops[i])
+    for i in range(offset, size):
+        value = instr_list[i]
         if numutils.is_even(i):
             opcode = value            
         else:
             operand = value
-            print(f"DEBUG: opcode={opcode} operand={operand} reg_map={reg_map}")
+            #print(f"DEBUG: opcode={opcode} operand={operand} reg_map={reg_map}")
             output = None
             if opcode == 0:
                 output = operand_adv(value, reg_map)
@@ -105,45 +126,112 @@ def process(instructions, reg_map, offset=0):
             elif opcode == 3:
                 output = operand_jnz(value, reg_map)
                 if output is not None:
-                    result += process(instructions, reg_map, offset=output)
+                    sub_results_list = process(instr_list, reg_map, offset=output)
+                    for item in sub_results_list:
+                        result.append(item)
             elif opcode == 4:
                 output = operand_bxc(value, reg_map)
             elif opcode == 5:
                 output = operand_out(value, reg_map)
-                result += str(output) + ','
+                result.append(output)
             elif opcode == 6:
                 output = operand_bdv(value, reg_map)
             elif opcode == 7:
                 output = operand_cdv(value, reg_map)
 
-            print(f"\n")
-            
+            #print(f"\n")        
+
+    #result = result.strip(',') # Remove any last comma
+    #cache[key] = (result, reg_map['A'], reg_map['B'], reg_map['C'])
+    #print(f"DEBUG: result={result}")
+    return result
 
 
-    return result.strip(',') # Remove any last comma
+def process_string_of_instructions(instructions:str,  reg_map, offset=0):
+    instr_list = [int(item) for item in instructions.split(',')]
+    #print(f"DEBUG: int_list={instr_list} type(instr_list")
+    result_list = process(instr_list, reg_map, offset)
+    #print(f"DEBUG: result_list={result_list}")
+    return ",".join(map(str, result_list))
+
+
+def get_reg_map(reg_lines):
+    reg_map = {}
+    for l in reg_lines:
+        suffix = l[len('Register '):]
+        values = suffix.split(': ')
+        reg_map[values[0]] = int(values[1])    
+    return reg_map
 
 
 def solve_part1(filename):
+    #global cache 
+    #cache = {}
+
     #lines = fileutils.get_file_lines_from(filename)
     reg_lines = fileutils.get_lines_before_empty_from_file(filename)
     data_line = fileutils.get_lines_after_empty_from_file(filename)[0]
     #print(f"DEBUG: {reg_lines}")
     #print(f"DEBUG: {data_line}")
-
     
-    reg_map = {}
-    for l in reg_lines:
-        suffix = l[len('Register '):]
-        values = suffix.split(': ')
-        reg_map[values[0]] = int(values[1])
-    print(f"DEBUG: reg_map={reg_map}")
+    reg_map = get_reg_map(reg_lines)
+    #print(f"DEBUG: reg_map={reg_map}")
 
-    #print(f"DEBUG: {reg_lines}")
+    instructions = data_line[len('Program: '):]    
+    return process_string_of_instructions(instructions, reg_map)
 
-    instr = data_line[len('Program: '):]    
-    return process(instr, reg_map)
+"""
+def explore_solve_part2(instructions):
+    instructions = [2,4,1,2,7,5,4,5,1,3,5,5,0,3,3,0]
 
+    last = 0
+    for i in range(100000000):
+        reg_map = {}
+        reg_map['A'] = i
+        reg_map['B'] = 0
+        reg_map['C'] = 0
 
-def solve_part2(filename):
-    lines = fileutils.get_file_lines_from(filename)
-    return "TODO"
+        #output = process(instr, new_reg_map)
+        output = process(instructions, reg_map)
+        a = reg_map['A']
+        b = reg_map['B']
+        c = reg_map['C']
+        if output == [3,0] or output == [3,3,0] or output == [0,3,3,0] or output == [5,0,3,3,0] \
+            or output == [5,5,0,3,3,0] or output == [3,5,5,0,3,3,0] or output == [1,3,5,5,0,3,3,0]:
+            print(f"DEBUG: i={i} diff={last - i} a={a} b={b} c={c} output={output}")
+            last = i
+            if output == [1,3,5,5,0,3,3,0]:
+                break
+    return i
+"""
+
+def solve_part2(instructions):
+    # Work backwards to see what taget value of Register A produced instructions values (reading from right to left)
+
+    # NB: Both example & full instuction set includes 0 (opcode), then 3 (operand)
+    #
+    # Opcode: 0 -> ADV -> A = A / 2 ** 3 (operand) =  A / 8
+    #
+    # So, working backward do the opposite i.e. A * 8
+
+    candidate_values = [0] # List of values to use for Register A (starting at zero)   
+    for i in range(len(instructions)):
+        next_candidate_values = []
+
+        for value in candidate_values:
+            # Try out addition of 0 to 7 (as 8 possibilies) to each candidate value
+            for j in range(8):
+                reg_map = {}                
+                reg_map['B'] = 0
+                reg_map['C'] = 0                
+                target = (value * 8) + j
+                reg_map['A'] = target
+
+                # Check for matches (if so add to next candidate set for instruction value to left)
+                if process(instructions, reg_map) == instructions[-i -1:]:
+                    next_candidate_values.append(target)
+
+        candidate_values = next_candidate_values
+        #print(f'DEBUG: candidate_values={candidate_values}')
+
+    return min(candidate_values)
--- aoc-2024/aoc-2024-day-17/solution-in-python3/test_solution.py
+++ aoc-2024/aoc-2024-day-17/solution-in-python3/test_solution.py
@@ -6,23 +6,27 @@ input_example = "AOC-2024-Day-17_Puzzle-Input-Example.txt"
 input_full = "AOC-2024-Day-17_Puzzle-Input-Full.txt"
 
 
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "reg_a, reg_b, reg_c, input, expected_reg_a, expected_reg_b, expected_reg_c, expected_output",
     [
         pytest.param(0,0,9,"2,6",0,1,9,None),
         pytest.param(10,0,0,"5,0,5,1,5,4",10,0,0,"0,1,2"),
         pytest.param(2024,0,0,"0,1,5,4,3,0",0,0,0,"4,2,5,6,7,7,7,7,3,1,0"),
-        pytest.param(0,29,0,"1,7",0,26,0,""),
         pytest.param(0,2024,43690,"4,0",0,44354,43690,""),
+        pytest.param(0,29,0,"1,7",0,26,0,""),
+        pytest.param(2024,0,0,"0,3,5,4,3,0",0,0,0,"5,7,3,0"),
+        pytest.param(2024,0,0,"0,3,5,4,3,0",0,0,0,"5,7,3,0"),
+        pytest.param(117440,0,0,"0,3,5,4,3,0",0,0,0,"0,3,5,4,3,0"),
     ],    
 )
-def test_process(reg_a, reg_b, reg_c, input, expected_reg_a, expected_reg_b, expected_reg_c, expected_output):
+def test_process_string_of_instructions(reg_a, reg_b, reg_c, input, expected_reg_a, expected_reg_b, expected_reg_c, expected_output):
     reg_map = {}
     reg_map['A'] = reg_a
     reg_map['B'] = reg_b
     reg_map['C'] = reg_c
 
-    output = solution.process(input, reg_map)
+    output = solution.process_string_of_instructions(input, reg_map)
     
     assert reg_map['A'] == expected_reg_a
     assert reg_map['B'] == expected_reg_b
@@ -44,14 +48,12 @@ def test_solve_part1(filename, expected):
     assert expected == value
 
 
-input_example_part2 = "AOC-2024-Day-17_Puzzle-Input-Example-Part2.txt"
-
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, 117440),
-        #pytest.param(input_full, -1),
+        pytest.param([0,3,5,4,3,0], 117440),
+        pytest.param([2,4,1,2,7,5,4,5,1,3,5,5,0,3,3,0], 37221270076916),
     ],    
 )
 def test_solve_part2(filename, expected):
