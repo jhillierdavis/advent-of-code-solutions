65ab2e3bdf437ccf99d4db3c0357389e9fea49d0
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-18 09:53:17 +0000

    Added Dijkstra's algorithm utility functions using Grid2D

--- aoc-2024/aoc-2024-day-18/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-18/solution-in-python3/solution.py
@@ -4,47 +4,10 @@
 
 import heapq
 
-from helpers import fileutils, grid, point
+from helpers import fileutils, grid, point, dijkstra
+
 
 CORRUPTION_BLOCK_CHAR = '#'
-
-
-def get_least_steps(g:grid.Grid2D, block_char:str="#", start_point:point.Point2D = point.Point2D(0, 0), stop_point:point.Point2D = None):
-    if stop_point == None:
-        stop_point = point.Point2D(g.get_width() -1, g.get_height() -1)
-
-    pq = [(0, start_point)] # Priority queue list, holding entries with total risk cost per point (x,y)
-    #heap = heapq.heapify(pq) # Transform a populated list into a heap 
-    visited = set()
-
-    low_risk_path_grid = grid.Grid2D(g.get_width(), g.get_height())
-    low_risk_path_grid.set_symbol(start_point, 0)
-
-    while len(pq) > 0:
-        # Get next lowest total risk cost point (x,y)
-        c, p = heapq.heappop(pq)        
-
-        # Avoid revisits
-        if p in visited:
-            continue # Ignore already visitied points (x,y) on grid
-        visited.add(p)
-
-        # Track total risk score on (another same size) grid
-        low_risk_path_grid.set_symbol(p, c)
-
-        # Stop when reach target destination point
-        if p == stop_point:
-            return c
-            #break
-
-        # Process cardial point (north, south, east, west) immediate neighbours, adding combined risk
-        neighbouring_points = g.get_cardinal_point_neighbours(p)
-        for np in neighbouring_points:
-            ns = g.get_symbol(np)
-            if ns == '.':
-                heapq.heappush(pq, (c + 1, np))
-
-    return -1
 
 
 def get_corruption_blocks(lines):
@@ -74,7 +37,7 @@ def solve_part1(filename:str, size:int, fallen:int) -> int:
     
     #grid.display_grid(g)
 
-    return get_least_steps(g)
+    return  dijkstra.get_least_steps(g)
 
 
 def get_block_point(g:grid.Grid2D, cbps:[point.Point2D], reachable:int=0) -> (int,int):
@@ -84,7 +47,7 @@ def get_block_point(g:grid.Grid2D, cbps:[point.Point2D], reachable:int=0) -> (in
 
         g.set_symbol(p, CORRUPTION_BLOCK_CHAR) # Blocked
             
-        if get_least_steps(g) < 0:
+        if  dijkstra.get_least_steps(g) < 0:
             return (p.get_x(), p.get_y())
 
     return (-1,-1) # Failed
@@ -108,7 +71,7 @@ def get_block_point_optimised(g, cps):
         g.set_symbol(p, '#') # Blocked
             
         # Try to use next point, as stop point, from furthest curruption point to optimise
-        if get_least_steps(g, CORRUPTION_BLOCK_CHAR, point.Point2D(0,0), point.Point2D(max_x, max_y)) < 0:
+        if  dijkstra.get_least_steps(g, CORRUPTION_BLOCK_CHAR, point.Point2D(0,0), point.Point2D(max_x, max_y)) < 0:
             return (p.get_x(), p.get_y())
 
     return (-1,-1)
