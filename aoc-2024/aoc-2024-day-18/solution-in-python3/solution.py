451d6432f093264db5854ef159ec50fc4027d661
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-18 05:38:02 +0000

    AOC 2024 Day 18: Solved Part 1

--- aoc-2024/aoc-2024-day-18/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-18/solution-in-python3/solution.py
@@ -1,11 +1,71 @@
-from helpers import fileutils
+# Solution implementation using Dijkstra's_algorithm (see https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm )
+#
+# NB: Use Heap queue algorithm in Python3: https://docs.python.org/3/library/heapq.html
 
-def solve_part1(filename):
+import heapq
+
+from helpers import fileutils, grid, point
+
+
+def create_low_risk_path_grid(g):
+    start_point = point.Point2D(0, 0)
+    stop_point = point.Point2D(g.get_width() -1, g.get_height() -1)
+
+    pq = [(0, start_point)] # Priority queue list, holding entries with total risk cost per point (x,y)
+    #heap = heapq.heapify(pq) # Transform a populated list into a heap 
+    visited = set()
+
+    low_risk_path_grid = grid.Grid2D(g.get_width(), g.get_height())
+    low_risk_path_grid.set_symbol(start_point, 0)
+
+    while len(pq) > 0:
+        # Get next lowest total risk cost point (x,y)
+        c, p = heapq.heappop(pq)        
+
+        # Avoid revisits
+        if p in visited:
+            continue # Ignore already visitied points (x,y) on grid
+        visited.add(p)
+
+        # Track total risk score on (another same size) grid
+        low_risk_path_grid.set_symbol(p, c)
+
+        # Stop when reach target destination point
+        if p == stop_point:
+            return c
+            #break
+
+        # Process cardial point (north, south, east, west) immediate neighbours, adding combined risk
+        neighbouring_points = g.get_cardinal_point_neighbours(p)
+        for np in neighbouring_points:
+            ns = g.get_symbol(np)
+            if ns == '.':
+                heapq.heappush(pq, (c + 1, np))
+
+    return -1
+
+
+def solve_part1(filename, fallen, size):
     lines = fileutils.get_file_lines_from(filename)
+    g = grid.Grid2D(size, size)
 
-    return "TODO"
+    cps = [] # Corruption points
+    for l in lines:
+        values = l.split(",")
+        x = int(values[0])
+        y = int(values[1])
+        p = point.Point2D(x,y)
+        cps.append(p)
+    
+    for i in range(fallen):
+        p = cps[i]
+        g.set_symbol(p, '#')
+    
+    grid.display_grid(g)
 
-def solve_part2(filename):
+    return create_low_risk_path_grid(g)
+
+
+def solve_part2(filename, size):
     lines = fileutils.get_file_lines_from(filename)
-
     return "TODO"
--- aoc-2024/aoc-2024-day-18/solution-in-python3/test_solution.py
+++ aoc-2024/aoc-2024-day-18/solution-in-python3/test_solution.py
@@ -6,29 +6,27 @@ input_example = "AOC-2024-Day-18_Puzzle-Input-Example.txt"
 input_full = "AOC-2024-Day-18_Puzzle-Input-Full.txt"
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
-    "filename, expected",
+    "filename, size, fallen, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 7, 12, 22),
+        pytest.param(input_full, 71, 1024, 252),
     ],    
 )
-def test_solve_part1(filename, expected):
-    value = solution.solve_part1(filename)
-    
+def test_solve_part1(filename, size, fallen, expected):
+    value = solution.solve_part1(filename, fallen, size)    
     assert expected == value
 
 
 @pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
-    "filename, expected",
+    "filename, size, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 7, (6,1)),
+        #pytest.param(input_full, 71, (-1,-1)),
     ],    
 )
-def test_solve_part2(filename, expected):
-    value = solution.solve_part2(filename)
-    
+def test_solve_part2(filename, size, expected):
+    value = solution.solve_part2(filename, size)    
     assert expected == value
