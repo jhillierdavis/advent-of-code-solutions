a9dd223901f98598aafb8a7300412fce4ba015a7
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-19 12:10:53 +0000

    Refactored: to make more DRY & added comments for clarity

--- aoc-2024/aoc-2024-day-19/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-19/solution-in-python3/solution.py
@@ -1,78 +1,73 @@
-#from functools import cache
-
 from helpers import fileutils, csvutils
 
 
-#@cache # Will not work with list!
 def has_patterns(patterns, design, cache=dict()):
-    #print(f"DEBUG: has_patterns {patterns} design={design} cache={cache}")
+    return count_patterns(patterns, design, cache) > 0
 
-    if design in cache: 
-        print(f"DEBUG: Cache: {design} {cache[design]}")
-        return cache[design]
+# Check each initial part of the design provided against available patterns, 
+# if matched then check the remainder (repeating for all pattern combos)
+def count_patterns(patterns, design, cache=dict()):
 
-    if not design: # Nothing left to match i.e. len(design) <= 0
-        #print(f"DEBUG: Cache: {design}")
-        return True
-
-    #print(f"DEBUG: Patterns {patterns}")
-    found = False
-    for p in patterns:
-        #print(f"DEBUG: p={p}")
-        if design.startswith(p):
-            remainder = design[len(p):]
-            # Check remainder
-            #print(f"DEBUG: Matched {p} remainder={remainder}")
-            if has_patterns(patterns, remainder, cache):
-                found = True
-                break
-    cache[design] = found
-    return found
-
-
-def count_patterns(patterns, design, cache):
+    # Check if can used cached result (for performance)
     if design in cache:
         return cache[design]
 
-    if not design: # Nothing left to match
-        return 1
+    if not design: # Nothing left to match i.e. len(design) <= 0
+        return 1 # At a match
 
-    #print(f"DEBUG: Patterns {patterns}")
-    count = 0
+    #print(f"DEBUG: Patterns {patterns} to match")
+    count = 0 # No matches
     for p in patterns:
-        #print(f"DEBUG: p={p}")
+        #print(f"DEBUG: Pattern to match: p={p}")
         if design.startswith(p):
-            remainder = design[len(p):]
             # Check remainder
+            remainder = design[len(p):] # Remaining design without matched pattern
             #print(f"DEBUG: Matched {p} remainder={remainder}")
             count += count_patterns(patterns, remainder, cache)
+    
+    # Cache the result (the number of times the design matches different combos of patterns)
     cache[design] = count
+
     return count
 
-    
 
-def solve_part1(filename):
+def get_input_patterns(filename):   
     lines = fileutils.get_file_lines(filename)
     patterns = csvutils.comma_separated_values_to_list(lines[0])
+    return patterns
+
+
+def get_input_designs(filename):
     designs = fileutils.get_lines_after_empty_from_file(filename)
+    return designs
+
+
+def solve_part1(filename):
+    patterns = get_input_patterns(filename)
+    designs = get_input_designs(filename)
     #print(f"DEBUG: patterns={patterns}")
 
-    ans = 0
+    # Cache whether known designs can be constructed from patterns provided
+    # NB: Does not need to be reset between designs (as based on patterns which remain the same)
+    cache = dict() 
+
+    total_count = 0
     for d in designs:
-        cache = dict() # Cache whether known designs can be constructed from patterns provided
         if has_patterns(patterns, d, cache):
-            ans += 1
-    return ans
+            total_count += 1
+    return total_count
 
 
 def solve_part2(filename):
-    lines = fileutils.get_file_lines(filename)
-    patterns = csvutils.comma_separated_values_to_list(lines[0])
-    designs = fileutils.get_lines_after_empty_from_file(filename)
+    patterns = get_input_patterns(filename)
+    designs = get_input_designs(filename)
     #print(f"DEBUG: patterns={patterns}")
 
-    ans = 0
-    for d in designs:
-        cache = dict() # Cache the number of ways each design can be constructed from patterns provided (0 is impossible)
-        ans += count_patterns(patterns, d, cache)
-    return ans
+    # Cache the number of ways each design can be constructed from patterns provided (0 indicates impossible)
+    # NB: Does not need to be reset between designs (as based on patterns which remain the same througout)
+    cache = dict() 
+
+    total_count = 0
+    for d in designs:        
+        total_count += count_patterns(patterns, d, cache)
+    return total_count
