18ba3cf8f69f05ce556dccc4d745ae8c1bb6044f
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-19 09:05:37 +0000

    AOC 2024 Day 19: Solved Part 1 & 2

--- aoc-2024/aoc-2024-day-19/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-19/solution-in-python3/solution.py
@@ -1,11 +1,102 @@
+#from functools import cache
+
 from helpers import fileutils
 
-def solve_part1(filename):
-    lines = fileutils.get_file_lines_from(filename)
 
-    return "TODO"
+def is_possible_design(towel_pattern, design) -> bool:
+    patterns = towel_pattern.split(", ")
+    patterns = sorted(patterns, key=len, reverse=True)
+    #print(f"DEBUG: {patterns}")
+
+    has_patterns = False
+    
+    s = design
+    size = len(design)
+    for i in range(size):
+        for p in patterns:
+            if s.startswith(p):                
+                s = s[len(p):]
+                #print(f"DEBUG: {s}")
+                continue
+
+    has_patterns = len(s) <= 0
+    return has_patterns
+
+
+
+#@cache # Will not work with list!
+def has_patterns(patterns, design, cache=dict()):
+    if design in cache:
+        return cache[design]
+
+    """
+    if len(design) <= 0:
+        print(f"Matched design={design}")
+        return True    
+    """
+    if not design: # Nothing left to match
+        return True
+
+    #print(f"DEBUG: Patterns {patterns}")
+    found = False
+    for p in patterns:
+        #print(f"DEBUG: p={p}")
+        if design.startswith(p):
+            remainder = design[len(p):]
+            # Check remainder
+            #print(f"DEBUG: Matched {p} remainder={remainder}")
+            if has_patterns(patterns, remainder, cache):
+                found = True
+                break
+    cache[design] = found
+    return found
+
+
+def count_patterns(patterns, design, cache):
+    if design in cache:
+        return cache[design]
+
+    if not design: # Nothing left to match
+        return 1
+
+    #print(f"DEBUG: Patterns {patterns}")
+    count = 0
+    for p in patterns:
+        #print(f"DEBUG: p={p}")
+        if design.startswith(p):
+            remainder = design[len(p):]
+            # Check remainder
+            #print(f"DEBUG: Matched {p} remainder={remainder}")
+            count += count_patterns(patterns, remainder, cache)
+    cache[design] = count
+    return count
+
+    
+
+def solve_part1(filename):
+    lines = fileutils.get_file_lines(filename)
+
+    patterns = lines[0].split(', ')
+    designs = fileutils.get_lines_after_empty_from_file(filename)
+    print(patterns)
+
+    ans = 0
+    for d in designs:
+        cache = dict() # Cache whether known designs can be constructed from patterns provided
+        if has_patterns(patterns, d, cache):
+            ans += 1
+    return ans
+
 
 def solve_part2(filename):
-    lines = fileutils.get_file_lines_from(filename)
+    lines = fileutils.get_file_lines(filename)
 
-    return "TODO"
+    patterns = lines[0].split(', ')
+    designs = fileutils.get_lines_after_empty_from_file(filename)
+    #print(f"DEBUG: patterns={patterns}")
+
+    ans = 0
+    for d in designs:
+        cache = dict() # Cache the number of ways each design can be constructed from patterns provided (0 is impossible)
+        ans += count_patterns(patterns, d, cache)
+    return ans
--- aoc-2024/aoc-2024-day-19/solution-in-python3/test_solution.py
+++ aoc-2024/aoc-2024-day-19/solution-in-python3/test_solution.py
@@ -6,29 +6,68 @@ input_example = "AOC-2024-Day-19_Puzzle-Input-Example.txt"
 input_full = "AOC-2024-Day-19_Puzzle-Input-Full.txt"
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
-    "filename, expected",
+    "towel_pattern, design, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param('r, wr, b, g, bwu, rb, gb, br', 'brwrr', True), # br + wr + r
+        pytest.param('r, wr, b, g, bwu, rb, gb, br', 'bggr', True), # b + g + g + r
+        pytest.param('r, wr, b, g, bwu, rb, gb, br', 'gbbr', True), # g + b + b + r
+        pytest.param('r, wr, b, g, bwu, rb, gb, br', 'rrbgbr', True), # r + rb + g + br
+        pytest.param('r, wr, b, g, bwu, rb, gb, br', 'ubwu', False), # no u
+        pytest.param('r, wr, b, g, bwu, rb, gb, br', 'bwurrg', True),
+        pytest.param('r, wr, b, g, bwu, rb, gb, br', 'brgr', True), 
+        pytest.param('r, wr, b, g, bwu, rb, gb, br', 'bbrgwb', False), 
+        pytest.param('r, wr, b, g, bwu, rb, gb, br', 'w', False), 
     ],    
 )
-def test_solve_part1(filename, expected):
-    value = solution.solve_part1(filename)
-    
+def test_is_possible_design(towel_pattern, design, expected):
+    value = solution.is_possible_design(towel_pattern, design)   
+    assert expected == value
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
+@pytest.mark.parametrize(
+    "towel_pattern, design, expected",
+    [
+        pytest.param('r, wr, b, g, bwu, rb, gb, br', 'brwrr', True), # br + wr + r
+        pytest.param('r, wr, b, g, bwu, rb, gb, br', 'bggr', True), # b + g + g + r
+        pytest.param('r, wr, b, g, bwu, rb, gb, br', 'gbbr', True), # g + b + b + r
+        pytest.param('r, wr, b, g, bwu, rb, gb, br', 'rrbgbr', True), # r + rb + g + br
+        pytest.param('r, wr, b, g, bwu, rb, gb, br', 'ubwu', False), # no u
+        pytest.param('r, wr, b, g, bwu, rb, gb, br', 'bwurrg', True),
+        pytest.param('r, wr, b, g, bwu, rb, gb, br', 'brgr', True), 
+        pytest.param('r, wr, b, g, bwu, rb, gb, br', 'bbrgwb', False), 
+        pytest.param('r, wr, b, g, bwu, rb, gb, br', 'w', False), 
+    ],    
+)
+def test_has_patterns(towel_pattern, design, expected):
+    patterns = towel_pattern.split(', ')
+    value = solution.has_patterns(patterns, design)   
     assert expected == value
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 6),
+        pytest.param(input_full, 293), # 318 too high! , # 277 too low!
+    ],    
+)
+def test_solve_part1(filename, expected):
+    value = solution.solve_part1(filename)   
+    assert expected == value
+
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
+@pytest.mark.parametrize(
+    "filename, expected",
+    [
+        pytest.param(input_example, 16),
+        pytest.param(input_full, 623924810770264),
     ],    
 )
 def test_solve_part2(filename, expected):
-    value = solution.solve_part2(filename)
-    
+    value = solution.solve_part2(filename)    
     assert expected == value
