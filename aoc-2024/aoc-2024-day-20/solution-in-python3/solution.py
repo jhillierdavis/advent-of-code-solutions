403fdf904fa71a4d81b728253ca226e62cbd3de0
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-20 08:01:40 +0000

    AOC 2024 Day 20: Solved Part 1 (albeit very slowly)

--- aoc-2024/aoc-2024-day-20/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-20/solution-in-python3/solution.py
@@ -1,11 +1,141 @@
-from helpers import fileutils
+from helpers import fileutils, grid, point, dijkstra
 
-def solve_part1(filename):
+
+def get_shortest_path(filename) -> int:
     lines = fileutils.get_file_lines_from(filename)
+    g = grid.lines_to_grid(lines)
+    #grid.display_grid(g)
 
-    return "TODO"
+    sp = grid.get_single_symbol_point(g,'S')
+    g.set_symbol(sp, '.')
+
+    ep = grid.get_single_symbol_point(g,'E')
+    g.set_symbol(ep, '.')
+    
+    #grid.display_grid(g)
+    #print(f"DEBUG: sp={sp} ep={ep}")
+    
+    count = dijkstra.get_least_steps(g, '#', sp, ep)
+    return count
+
+
+def get_shortest_path_with_cheat(filename) -> int:
+    lines = fileutils.get_file_lines_from(filename)
+    g = grid.lines_to_grid(lines)
+
+    cp = grid.get_single_symbol_point(g,'1')
+    g.set_symbol(cp, '.')
+
+    cp = grid.get_single_symbol_point(g,'2')
+    g.set_symbol(cp, '.')
+    ep = cp
+
+    sp = grid.get_single_symbol_point(g,'S')
+    g.set_symbol(sp, '.')
+
+    points = g.get_points_matching('E')
+    #print(f"DEBUG: points={points}")
+    if len(points) > 0:
+        ep = grid.get_single_symbol_point(g,'E')
+        g.set_symbol(ep, '.')    
+
+    #grid.display_grid(g)
+    #print(f"DEBUG: sp={sp} ep={ep}")
+
+    count = dijkstra.get_least_steps(g, '#', sp, ep)
+    return count
+
+
+def count_number_of_cheats_for_saving(filename, saving):
+    lines = fileutils.get_file_lines_from(filename)
+    g = grid.lines_to_grid(lines)
+    #grid.display_grid(g)
+
+    sp = grid.get_single_symbol_point(g,'S')
+    g.set_symbol(sp, '.')
+
+    ep = grid.get_single_symbol_point(g,'E')
+    g.set_symbol(ep, '.')
+    
+    #grid.display_grid(g)
+    #print(f"DEBUG: sp={sp} ep={ep}")
+
+    count = dijkstra.get_least_steps(g, '#', sp, ep)
+    target = count - saving
+    print(f"DEBUG: target={target} count={count} saving={saving}")
+
+    spaces = g.get_points_matching('.') 
+    total_count = 0
+    cheat_point_set = set()
+    for pp in spaces:
+        nps = g.get_cardinal_point_neighbours(pp)
+        for np in nps:
+            if np in cheat_point_set:
+                continue
+
+            ns = g.get_symbol(np)
+            if ns == '#':
+                gc = g.clone()
+                gc.set_symbol(np, '.') # Cheat
+                count = dijkstra.get_least_steps(gc, '#', sp, ep)
+                if count > 0 and count == target :
+                    cheat_point_set.add(np)
+                    total_count += 1
+    
+
+    return total_count
+
+
+def solve_part1(filename, saving):
+    lines = fileutils.get_file_lines_from(filename)
+    g = grid.lines_to_grid(lines)
+    #grid.display_grid(g)
+
+    sp = grid.get_single_symbol_point(g,'S')
+    g.set_symbol(sp, '.')
+
+    ep = grid.get_single_symbol_point(g,'E')
+    g.set_symbol(ep, '.')
+    
+    #grid.display_grid(g)
+    #print(f"DEBUG: sp={sp} ep={ep}")
+
+    count = dijkstra.get_least_steps(g, '#', sp, ep)
+    target = count - saving
+    print(f"DEBUG: target={target} count={count} saving={saving}")
+
+    spaces = g.get_points_matching('.') 
+    total_count = 0
+    cheat_point_set = set()
+    for pp in spaces:
+        nps = g.get_cardinal_point_neighbours(pp)
+        for np in nps:
+            if np in cheat_point_set:
+                continue
+
+            # Check that provides a usable cheat path
+            nnps = g.get_cardinal_point_neighbours(pp)
+            num = 0
+            for nnp in nnps:
+                nns = g.get_symbol(nnp)
+                if nns == '.':
+                    num += 1
+            if num <= 1:
+                continue
+
+            ns = g.get_symbol(np)
+            if ns == '#':
+                gc = g.clone()
+                gc.set_symbol(np, '.') # Cheat
+                count = dijkstra.get_least_steps(gc, '#', sp, ep)
+                if count > 0 and count <= target:
+                    cheat_point_set.add(np)
+                    total_count += 1
+    
+
+    return total_count
+
 
 def solve_part2(filename):
     lines = fileutils.get_file_lines_from(filename)
-
     return "TODO"
--- aoc-2024/aoc-2024-day-20/solution-in-python3/test_solution.py
+++ aoc-2024/aoc-2024-day-20/solution-in-python3/test_solution.py
@@ -5,20 +5,74 @@ import solution
 input_example = "AOC-2024-Day-20_Puzzle-Input-Example.txt"
 input_full = "AOC-2024-Day-20_Puzzle-Input-Full.txt"
 
+input_example_cheat_1 = "example_1_cheat.txt"
+input_example_cheat_2 = "example_2_cheat.txt"
+input_example_cheat_3 = "example_3_cheat.txt"
+input_example_cheat_4 = "example_4_cheat.txt"
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
+        pytest.param(input_example, 84),
+        pytest.param(input_full, 9420),
+    ],    
+)
+def test_get_shortest_path(filename, expected):
+    value = solution.get_shortest_path(filename)    
+    assert expected == value
+
+
+@pytest.mark.parametrize(
+    "filename, expected",
+    [
+        pytest.param(input_example_cheat_1, 72),
+        pytest.param(input_example_cheat_2, 64),
+        pytest.param(input_example_cheat_3, 84 - 38),
+        pytest.param(input_example_cheat_4, 84 - 64),
+    ],    
+)
+def test_get_shortest_path_with_cheat(filename, expected):
+    value = solution.get_shortest_path_with_cheat(filename)    
+    assert expected == value
+
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
+@pytest.mark.parametrize(
+    "filename, saving, expected",
+    [
+        pytest.param(input_example, 2, 14),
+        pytest.param(input_example, 4, 14),
+        pytest.param(input_example, 6, 2),
+        pytest.param(input_example, 8, 4),
+        pytest.param(input_example, 10, 2),
+        pytest.param(input_example, 12, 3),
+        pytest.param(input_example, 20, 1),
+        pytest.param(input_example, 36, 1),
+        pytest.param(input_example, 38, 1),
+        pytest.param(input_example, 40, 1),
+        pytest.param(input_example, 64, 1),
+        #pytest.param(input_example, 11),
         #pytest.param(input_full, -1),
     ],    
 )
-def test_solve_part1(filename, expected):
-    value = solution.solve_part1(filename)
-    
+def test_count_number_of_cheats_for_saving(filename, saving, expected):
+    value = solution.count_number_of_cheats_for_saving(filename, saving)    
     assert expected == value
 
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
+@pytest.mark.parametrize(
+    "filename, saving, expected",
+    [
+        pytest.param(input_example, 64, 1),
+        pytest.param(input_full, 100, 1454), # But, very slow (~ 20 mins!)
+    ],    
+)
+def test_solve_part1(filename, saving, expected):
+    value = solution.solve_part1(filename, saving)
+    
+    assert expected == value
 
 @pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
