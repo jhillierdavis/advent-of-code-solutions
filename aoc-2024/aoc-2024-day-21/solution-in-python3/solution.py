c465e9ad538a3f29a8e0e2d9f713168efc0ac3b5
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-21 11:20:32 +0000

    AOC 2024 Day 21: WIP Part 1

--- aoc-2024/aoc-2024-day-21/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-21/solution-in-python3/solution.py
@@ -1,11 +1,122 @@
-from helpers import fileutils
+import math
+from collections import deque
+
+from helpers import fileutils, grid, point
+
+# TODO: Handle '•' sysmbol (non-navigatable)
+
+def find_paths_with_directions(g:grid.Grid2D, start_symbol, end_symbol):
+    start = grid.get_single_symbol_point(g, start_symbol)
+    end = grid.get_single_symbol_point(g, end_symbol)
+
+    def dfs(p, path, directions_path, visited):
+        if p == end:
+            paths.append((path, directions_path))
+            return
+        
+        visited.add(p)
+
+        np = g.get_neighbour_north(p)        
+        if None != np and np not in visited:        
+            new_path = path + [(np.get_x(), np.get_y())]
+            dfs(np, new_path, directions_path + ['^'], set(visited))
+
+        np = g.get_neighbour_east(p)
+        if None != np and np not in visited:
+            new_path = path + [(np.get_x(), np.get_y())]
+            dfs(np, new_path, directions_path + ['>'], set(visited))
+
+        np = g.get_neighbour_south(p)
+        if None != np and np not in visited:
+            new_path = path + [(np.get_x(), np.get_y())]
+            dfs(np, new_path, directions_path + ['v'], set(visited))
+
+        np = g.get_neighbour_west(p)
+        if None != np and np not in visited:
+            new_path = path + [(np.get_x(), np.get_y())]
+            dfs(np, new_path, directions_path + ['<'], set(visited))
+
+    paths = []
+    dfs(start, [(start.get_x(), start.get_y())], [], set())
+    return paths
+
+
+cache = {}
+def find_min_directions(g:grid.Grid2D, start_symbol, end_symbol):
+    if (start_symbol, end_symbol) in cache:
+        return cache[(start_symbol, end_symbol)]
+
+    paths = find_paths_with_directions(g, start_symbol, end_symbol)
+    #print(f"DEBUG: paths={paths}")
+    min_size = math.inf
+    min_directions = None
+    for p, d in paths:
+        size = len(d)
+        if size < min_size:
+            min_directions = d
+            min_size = size
+
+    cache[(start_symbol, end_symbol)] = min_directions
+
+    return min_directions
+
+
+def calculate_complexity(code, sequence):
+    return int(code[:-1]) * len(sequence)
+
+
+def create_directional_keypad_grid():
+    g = grid.Grid2D(3,2)
+    g.set_symbol(point.Point2D(0,0), '*')
+    g.set_symbol(point.Point2D(1,0), '^')
+    g.set_symbol(point.Point2D(2,0), 'A')
+    g.set_symbol(point.Point2D(0,1), '<')
+    g.set_symbol(point.Point2D(1,1), 'v')
+    g.set_symbol(point.Point2D(2,1), '>')
+    grid.display_grid(g)
+    print()
+    return g
+
+
+def create_numerical_keypad_grid():
+    g = grid.Grid2D(3,4)
+    g.set_symbol(point.Point2D(0,0), '7')
+    g.set_symbol(point.Point2D(1,0), '8')
+    g.set_symbol(point.Point2D(2,0), '9')
+    g.set_symbol(point.Point2D(0,1), '4')
+    g.set_symbol(point.Point2D(1,1), '5')
+    g.set_symbol(point.Point2D(2,1), '6')
+    g.set_symbol(point.Point2D(0,2), '1')
+    g.set_symbol(point.Point2D(1,2), '2')
+    g.set_symbol(point.Point2D(2,2), '3')
+    g.set_symbol(point.Point2D(0,3), '*')
+    g.set_symbol(point.Point2D(1,3), '0')
+    g.set_symbol(point.Point2D(2,3), 'A')
+
+    grid.display_grid(g)
+    print()
+    return g
+
+
+def get_shortest_directional_sequence_for_code(code):
+    g = create_numerical_keypad_grid()
+
+    start = 'A'
+    sequence = ''
+    for c in code:
+        min_directions = find_min_directions(g, start, c)
+#       print(f"DEBUG: c={c} min_directions={min_directions}")        
+        for md in min_directions:
+            sequence += md
+        sequence += 'A'
+        start = c
+
+    return sequence
+
 
 def solve_part1(filename):
     lines = fileutils.get_file_lines_from(filename)
 
-    return "TODO"
-
-def solve_part2(filename):
-    lines = fileutils.get_file_lines_from(filename)
-
+    nkg = create_numerical_keypad_grid()
+    dkg = create_directional_keypad_grid()
     return "TODO"
--- aoc-2024/aoc-2024-day-21/solution-in-python3/test_solution.py
+++ aoc-2024/aoc-2024-day-21/solution-in-python3/test_solution.py
@@ -5,18 +5,42 @@ import solution
 input_example = "AOC-2024-Day-21_Puzzle-Input-Example.txt"
 input_full = "AOC-2024-Day-21_Puzzle-Input-Full.txt"
 
+@pytest.mark.parametrize(
+    "code, sequence, expected",
+    [
+        pytest.param('029A', '<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A', 68 * 29),
+        pytest.param('980A', '<v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A', 60 * 980),
+        pytest.param('179A', '<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A', 68 * 179), 
+        pytest.param('456A', '<v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A', 64 * 456),
+        pytest.param('379A', '<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A', 64 * 379),
+    ],    
+)
+def test_calculate_complexity(code, sequence, expected):
+    value = solution.calculate_complexity(code, sequence)
+    assert expected == value
+
+
+@pytest.mark.parametrize(
+    "code, expected",
+    [
+        pytest.param('029A', ['<A^A>^^AvvvA', '<A^A^>^AvvvA', '<A^A^^>AvvvA']),
+    ],    
+)
+def test_get_shortest_directional_sequence_for_code(code, expected):
+    value = solution.get_shortest_directional_sequence_for_code(code)
+    assert value in expected
+
 
 @pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
+        pytest.param(input_example, 126384),
         #pytest.param(input_full, -1),
     ],    
 )
 def test_solve_part1(filename, expected):
-    value = solution.solve_part1(filename)
-    
+    value = solution.solve_part1(filename)    
     assert expected == value
 
 
@@ -30,5 +54,4 @@ def test_solve_part1(filename, expected):
 )
 def test_solve_part2(filename, expected):
     value = solution.solve_part2(filename)
-    
     assert expected == value
