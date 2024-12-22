dd3ecc551d30a77633dbfc5078245d26733f8dde
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-22 06:34:27 +0000

    AOC 2024 Day 22: Solved Part 1

--- aoc-2024/aoc-2024-day-22/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-22/solution-in-python3/solution.py
@@ -1,11 +1,55 @@
 from helpers import fileutils
 
+def mix(num, mixin):
+    return mixin ^ num 
+
+def prune(num):
+    return num % 16777216
+
+def generate_next_secret_number(initial):
+    next = initial
+
+    mixin = 64 * initial
+    next = mix(next, mixin)
+    next = prune(next)
+
+    mixin = next // 32
+    next = mix(next, mixin)
+    next = prune(next)
+
+    mixin = next * 2048
+    next = mix(next, mixin)
+    next = prune(next)
+
+    return next
+
+
+def generate_secret_number_sequence(initial, max):
+    sequence = []
+    next = initial
+    for _ in range(max):
+        next = generate_next_secret_number(next)
+        sequence.append(next)
+    return sequence
+
+def generate_nth_secret_number(initial, n):
+    next = initial
+    for _ in range(n):
+        next = generate_next_secret_number(next)
+    return next
+
+
 def solve_part1(filename):
     lines = fileutils.get_file_lines_from(filename)
 
-    return "TODO"
+    count = 0
+    for l in lines:
+        num = int(l)
+        count += generate_nth_secret_number(num, 2000)
+
+    return count
+
 
 def solve_part2(filename):
     lines = fileutils.get_file_lines_from(filename)
-
-    return "TODO"
+    return -1
--- aoc-2024/aoc-2024-day-22/solution-in-python3/test_solution.py
+++ aoc-2024/aoc-2024-day-22/solution-in-python3/test_solution.py
@@ -6,12 +6,63 @@ input_example = "AOC-2024-Day-22_Puzzle-Input-Example.txt"
 input_full = "AOC-2024-Day-22_Puzzle-Input-Full.txt"
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+@pytest.mark.parametrize(
+    "initial, mixin, expected",
+    [
+        pytest.param(42, 15, 37),
+    ],    
+)
+def test_mix(initial, mixin, expected):
+    value = solution.mix(initial, mixin)    
+    assert expected == value
+
+@pytest.mark.parametrize(
+    "initial, expected",
+    [
+        pytest.param(100000000, 16113920),
+    ],    
+)
+def test_prune(initial, expected):
+    value = solution.prune(initial)    
+    assert expected == value
+    
+
+
+@pytest.mark.parametrize(
+    "initial, max, expected",
+    [
+        pytest.param(123, 1, [15887950]),
+        pytest.param(123, 10, [15887950, 16495136, 527345, 704524, 1553684, 12683156, 11100544, 12249484, 7753432, 5908254]),
+    ],    
+)
+def test_generate_secret_number_sequence(initial, max, expected):
+    value = solution.generate_secret_number_sequence(initial, max)
+    
+    assert expected == value
+
+@pytest.mark.parametrize(
+    "initial, n, expected",
+    [
+        pytest.param(123, 1, 15887950),
+        pytest.param(123, 10, 5908254),
+        pytest.param(1, 2000, 8685429),
+        pytest.param(10, 2000, 4700978),
+        pytest.param(100, 2000, 15273692),
+        pytest.param(2024, 2000, 8667524),
+    ],    
+)
+def test_generate_nth_secret_number(initial, n, expected):
+    value = solution.generate_nth_secret_number(initial, n)    
+    assert expected == value
+
+
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 37327623),
+        pytest.param(input_full, 'TODO'),
     ],    
 )
 def test_solve_part1(filename, expected):
@@ -24,8 +75,8 @@ def test_solve_part1(filename, expected):
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 'TODO'),
+        #pytest.param(input_full, 'TODO'),
     ],    
 )
 def test_solve_part2(filename, expected):
