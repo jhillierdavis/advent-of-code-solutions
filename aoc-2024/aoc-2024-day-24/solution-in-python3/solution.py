2f967a341a29375e0fa4351ac4497912e5223a38
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-12-24 12:12:19 +0000

    AOC 2024 Day 24: WIP Part 2

--- aoc-2024/aoc-2024-day-24/solution-in-python3/solution.py
+++ aoc-2024/aoc-2024-day-24/solution-in-python3/solution.py
@@ -11,14 +11,24 @@ def get_wire_value_map(filename):
     return wire_value_map
 
 
-def get_zwire_result(wire_value_map):
+def get_decimal_value_of_zwire(wire_value_map):
+    return get_specific_wire_decimal_value(wire_value_map, 'z')
+
+def get_decimal_value_of_ywire(wire_value_map):
+    return get_specific_wire_decimal_value(wire_value_map, 'y')
+
+def get_decimal_value_of_xwire(wire_value_map):
+    return get_specific_wire_decimal_value(wire_value_map, 'x')
+
+
+def get_specific_wire_decimal_value(wire_value_map, ch):
     binary_ans = ''
     for i in range(1000):
-        zwire = 'z' + str(i).zfill(2)
-        if zwire not in wire_value_map:
+        wire = ch + str(i).zfill(2)
+        if wire not in wire_value_map:
             break 
-        binary_ans = str(wire_value_map[zwire]) + binary_ans
-
+        binary_ans = str(wire_value_map[wire]) + binary_ans
+    print(f"DEBUG: binary_ans={binary_ans} ch={ch}")
     return int(binary_ans, 2)
 
 
@@ -33,12 +43,7 @@ def process_wire_signals(wire_value_map, wire_left, operation, wire_right, wire_
         raise Exception(f'Unknown operation: {operation}')
 
 
-def solve_part1(filename):
-    #lines = fileutils.get_file_lines_from(filename)
-
-    wire_value_map = get_wire_value_map(filename)
-    #print(f"DEBUG: wire_value_map={wire_value_map}")
-
+def process_gate_operations(filename, wire_value_map):
     queue = deque()
     lines = fileutils.get_lines_after_empty_from_file(filename)
     for l in lines:
@@ -61,11 +66,30 @@ def solve_part1(filename):
             queue.appendleft(entry)
         else:
             process_wire_signals(wire_value_map, wire_left, operation, wire_right, wire_result)
-    
+
+
+def solve_part1(filename):
+    wire_value_map = get_wire_value_map(filename)
     #print(f"DEBUG: wire_value_map={wire_value_map}")
 
-    return get_zwire_result(wire_value_map)
+    process_gate_operations(filename, wire_value_map)    
+    #print(f"DEBUG: wire_value_map={wire_value_map}")
+
+    return get_decimal_value_of_xwire(wire_value_map)
+
 
 def solve_part2(filename):
-    lines = fileutils.get_file_lines_from(filename)
-    return -1
+    wire_value_map = get_wire_value_map(filename)
+    #print(f"DEBUG: wire_value_map={wire_value_map}")
+    
+    process_gate_operations(filename, wire_value_map)
+    #print(f"DEBUG: wire_value_map={wire_value_map}")
+
+    x = get_decimal_value_of_xwire(wire_value_map)
+    y = get_decimal_value_of_ywire(wire_value_map)
+    z = get_decimal_value_of_zwire(wire_value_map)
+    target = x & y # Bitwise AND
+
+    print(f"DEBUG: target={target} z={z} x={x} y={y}")
+
+    return get_decimal_value_of_zwire(wire_value_map)
--- aoc-2024/aoc-2024-day-24/solution-in-python3/test_solution.py
+++ aoc-2024/aoc-2024-day-24/solution-in-python3/test_solution.py
@@ -5,9 +5,11 @@ import solution
 input_example = "AOC-2024-Day-24_Puzzle-Input-Example.txt"
 input_example_larger = "AOC-2024-Day-24_Puzzle-Input-Example-Larger.txt"
 input_full = "AOC-2024-Day-24_Puzzle-Input-Full.txt"
+input_example_part2 = "AOC-2024-Day-24_Puzzle-Input-Example-Part2.txt"
+input_example_part2_corrected = "AOC-2024-Day-24_Puzzle-Input-Example-Part2-Corrected.txt"
 
 
-#@pytest.mark.skip(reason="TODO: Ignore until implemented")
+@pytest.mark.skip
 @pytest.mark.parametrize(
     "filename, expected",
     [
@@ -22,12 +24,11 @@ def test_solve_part1(filename, expected):
     assert expected == value
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, 'TODO'),
-        #pytest.param(input_example_larger, 'TODO'),
+        pytest.param(input_example_part2, 'z00,z01,z02,z05'),
         #pytest.param(input_full, 'TODO'),
     ],    
 )
