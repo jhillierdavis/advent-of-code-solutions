1b09536405cec2396aaa1ae15f62ee5608d30447
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-01-03 12:18:33 +0000

    AOC 2024 Day 20: Refactored

--- aoc-2024/aoc-2024-day-20/solution-in-python3/solution_part2.py
+++ aoc-2024/aoc-2024-day-20/solution-in-python3/solution_part2.py
@@ -6,7 +6,19 @@ def get_path(filename):
     g = grid.lines_to_grid(lines)
 
     sp = grid.find_single_symbol_point_and_replace(g, 'S', pathutils.DEFAULT_PATH_SYMBOL)
-    ep = grid.find_single_symbol_point_and_replace(g, 'E', pathutils.DEFAULT_PATH_SYMBOL)
+    #ep = grid.find_single_symbol_point_and_replace(g, 'E', pathutils.DEFAULT_PATH_SYMBOL)
+    points = g.get_points_matching('1')
+    if len(points) > 0:
+        grid.find_single_symbol_point_and_replace(g,'1', pathutils.DEFAULT_PATH_SYMBOL)
+
+    points = g.get_points_matching('2')
+    if len(points) > 0:
+        ep = grid.find_single_symbol_point_and_replace(g,'2', pathutils.DEFAULT_PATH_SYMBOL)
+
+    points = g.get_points_matching('E')
+    #print(f"DEBUG: points={points}")
+    if len(points) > 0:
+        ep = grid.find_single_symbol_point_and_replace(g, 'E', pathutils.DEFAULT_PATH_SYMBOL)    
 
     path = pathutils.get_quickest_path(g, sp, ep)
     #print(f"DEBUG: path={path}")    
@@ -24,7 +36,56 @@ def create_path_position_to_distance_map(path):
     return path_dist_map
 
 
-def solve_part2(filename, min_saving, cheat_duration):
+def get_best_chart_path_length(filename, cheat_duration=2):
+    path = get_path(filename)
+    if path == None:
+        return 0
+    
+    path_dist_map = create_path_position_to_distance_map(path)
+
+    size = len(path)
+    min_length = size
+    for i in range(size - 2):
+        p1 = path[i]
+        for j in range(i+2, size):
+            p2 = path[j]
+
+            if p1 == p2:
+                continue
+
+            md =  p1.get_manhatten_distance_to(p2)
+            if md >= 2 and md <= cheat_duration and path_dist_map[p2] > path_dist_map[p1]:
+                diff = path_dist_map[p2] - path_dist_map[p1]
+                if size + md - diff < min_length:
+                    min_length = size + md - diff
+
+    return min_length
+
+
+def count_number_of_cheats_for_exact_saving(filename, min_saving, cheat_duration):
+    path = get_path(filename)
+    path_dist_map = create_path_position_to_distance_map(path)
+
+    count = 0
+    size = len(path)
+    for i in range(size - 2):
+        p1 = path[i]
+        for j in range(i+2, size):
+            p2 = path[j]
+
+            if p1 == p2:
+                continue
+
+            md =  p1.get_manhatten_distance_to(p2)
+            if md >= 2 and md <= cheat_duration and path_dist_map[p2] > path_dist_map[p1]:
+                diff = path_dist_map[p2] - path_dist_map[p1]
+                if diff == min_saving + md:
+                    count += 1
+
+    return count
+
+
+def count_number_of_cheats_for_saving_or_less(filename, min_saving, cheat_duration):
     path = get_path(filename)
     path_dist_map = create_path_position_to_distance_map(path)
 
--- aoc-2024/aoc-2024-day-20/solution-in-python3/test_solution.py
+++ aoc-2024/aoc-2024-day-20/solution-in-python3/test_solution.py
@@ -12,36 +12,31 @@ input_example_cheat_3 = "example_3_cheat.txt"
 input_example_cheat_4 = "example_4_cheat.txt"
 
 
+import solution_part2
+
+
 @pytest.mark.parametrize(
     "filename, expected",
     [
         pytest.param(input_example, 84),
         pytest.param(input_full, 9420),
-    ],    
-)
-def test_get_shortest_path(filename, expected):
-    value = solution.get_shortest_path(filename)    
-    assert expected == value
-
-
-@pytest.mark.parametrize(
-    "filename, expected",
-    [
         pytest.param(input_example_cheat_1, 72),
         pytest.param(input_example_cheat_2, 64),
         pytest.param(input_example_cheat_3, 84 - 38),
         pytest.param(input_example_cheat_4, 84 - 64),
+
     ],    
 )
-def test_get_shortest_path_with_cheat(filename, expected):
-    value = solution.get_shortest_path_with_cheat(filename)    
-    assert expected == value
+def test_get_shortest_path(filename, expected):
+    # When
+    path = solution_part2.get_path(filename)    
+    size = len(path) - 1 # Deduct 1 for starting point
+
+    # Then
+    assert expected == size
 
 
-
-
-
-#@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip
 @pytest.mark.parametrize(
     "filename, saving, expected",
     [
@@ -59,113 +54,30 @@ def test_get_shortest_path_with_cheat(filename, expected):
     ],    
 )
 def test_count_number_of_cheats_for_saving(filename, saving, expected):
-    value = solution.count_number_of_cheats_for_saving(filename, saving)    
+    value = solution_part2.count_number_of_cheats_for_exact_saving(filename, saving, 2)    
     assert expected == value
-
-
-@pytest.mark.parametrize(
-    "filename, size, expected",
-    [
-        pytest.param(input_example, 1, ((7,1),(9,1),1)), # from Example 1
-        pytest.param(input_example, 1, ((9,7),(11,7),1)), # from Example 2
-        pytest.param(input_example, 1, ((8,7),(8,9),1)), # from Example 3
-        pytest.param(input_example, 1, ((7,7),(5,7),1)), # from Example 4
-    ],    
-)
-def test_get_cheat_paths(filename, size, expected):
-    lines = fileutils.get_file_lines_from(filename)
-    g = grid.lines_to_grid(lines)
-    #grid.display_grid(g)
-
-    sp = solution.find_single_symbol_point_and_clear(g,'S')
-    ep = solution.find_single_symbol_point_and_clear(g,'E')
-
-    cheat_paths = solution.get_cheat_paths(g, size)
-    es, ee, el = expected
-    print(f"DEBUG: es={es} ee={ee} el={el}")
-    esx, esy = es
-    eex, eey = ee
-    print(f"DEBUG: esx={esx} esy={esy} eex={eex} eey={eey}")
-    esp = point.Point2D(esx,esy)
-    eep = point.Point2D(eex,eey)
-    print(f"DEBUG: esp={esp} eep={eep}")
-
-    matched = False
-    for cp in cheat_paths:
-        cs, ce, cl = cp
-        if cs == esp and ce == eep and cl == el:
-            matched = True
-            break
-    
-    assert matched
 
 
 #@pytest.mark.skip
 @pytest.mark.parametrize(
-    "filename, saving, expected",
-    [
-        pytest.param(input_example, 64, 1),
-        #pytest.param(input_full, 100, 1454), # But, very slow (~ 20 mins!)
-    ],    
-)
-def test_solve_part1(filename, saving, expected):
-    value = solution.solve_part1(filename, saving)
-    
-    assert expected == value
-
-
-#@pytest.mark.skip(reason="TODO: Ignore until implemented")
-@pytest.mark.parametrize(
-    "filename, saving, expected",
-    [
-        pytest.param(input_example, 2, 14),
-        pytest.param(input_example, 4, 14),
-        pytest.param(input_example, 6, 2),
-        pytest.param(input_example, 8, 4),
-        pytest.param(input_example, 10, 2),
-        pytest.param(input_example, 12, 3),
-        pytest.param(input_example, 20, 1),
-        pytest.param(input_example, 36, 1),
-        pytest.param(input_example, 38, 1),
-        pytest.param(input_example, 40, 1),
-        pytest.param(input_example, 64, 1),
-        #pytest.param(input_full, 100, -1), # Too slow
-    ],    
-)
-def test_count_number_of_cheats_for_saving_alt_approach(filename, saving, expected):
-    value = solution.count_number_of_cheats_for_saving_alt_approach(filename, saving)
-    
-    assert expected == value
-
-import solution_part2
-
-"""
-@pytest.mark.skip
-@pytest.mark.parametrize(
-    "filename, saving, duration, expected",
-    [
-        pytest.param(input_example, 64, 1, 1),
-        #pytest.param(input_example, 50, 19, 32),
-        #pytest.param(input_example, 52, 20, 31),
-        #pytest.param(input_example, 74, 20, 4),
-        #pytest.param(input_example, 76, 20, 3),
-    ],    
-)
-def test_foo(filename, saving, duration, expected):
-    value = solution_part2.foo(filename, saving, duration)
-    
-    assert expected == value
-"""
-
-@pytest.mark.parametrize(
     "filename, saving, duration, expected",
     [
         pytest.param(input_example, 64, 2, 1),
-        pytest.param(input_full, 100, 2, 1454), # Still a bit slow
+        pytest.param(input_full, 100, 2, 1454) # Still a bit slow
+    ],    
+)
+def test_solve_part1(filename, saving, duration, expected):
+    value = solution_part2.count_number_of_cheats_for_saving_or_less(filename, saving, duration)    
+    assert expected == value
+
+
+#@pytest.mark.skip
+@pytest.mark.parametrize(
+    "filename, saving, duration, expected",
+    [
         pytest.param(input_full, 100, 20, 997879), # Still a bit slow
     ],    
 )
 def test_solve_part2(filename, saving, duration, expected):
-    value = solution_part2.solve_part2(filename, saving, duration)
-    
+    value = solution_part2.count_number_of_cheats_for_saving_or_less(filename, saving, duration)    
     assert expected == value
