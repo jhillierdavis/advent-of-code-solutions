1e3b38bb7a23be528e412f2d3b1d53aa2d1c7d40
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-08-12 15:06:52 +0100

    AOC 2023: Setup for unit test execution (e.g. Z3 & NetworkX modules installation)

---
+++ aoc-2023/setup.sh
@@ -0,0 +1,13 @@
+# Prerequiste module installs for some unit tests
+
+# use latest pip(s)
+pip install --upgrade pip
+pip3 install --upgrade pip
+
+# for AOC-2023 Day 24
+pip install z3-solver
+pip3 install z3-solver
+
+# for AOC-2023 Day 25
+pip install networkx
+pip3 install networkx
