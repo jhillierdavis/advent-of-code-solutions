fb5403772d9df504c4bdbe8a23e92bba8455d353
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-08-12 16:48:58 +0100

    AOC 2021 Day 18 Part 1: WIP: Calculate snailfish number magnitude

--- aoc-2021/aoc-2021-day-18/solutions-in-python3/solution.py
+++ aoc-2021/aoc-2021-day-18/solutions-in-python3/solution.py
@@ -1,3 +1,13 @@
+
+def get_magnitude(snailfish_number:list) -> int:    
+    left = snailfish_number[0]
+    right = snailfish_number[1]
+    if type(left) == list:
+        left = get_magnitude(left)
+    if type(right) == list:
+        right = get_magnitude(right)
+    return (3 * left) + (2 * right)
+
 def add(snailfish_number_x, snailfish_number_y):
     # TODO
     return []
--- aoc-2021/aoc-2021-day-18/solutions-in-python3/test_solution.py
+++ aoc-2021/aoc-2021-day-18/solutions-in-python3/test_solution.py
@@ -3,6 +3,25 @@ import pytest
 # Local
 import solution
 
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        pytest.param([9,1], 29),
+        pytest.param([1,9], 21),
+        pytest.param([[9,1],[1,9]], 129),
+        pytest.param([[1,2],[[3,4],5]],143),
+        pytest.param([[[[0,7],4],[[7,8],[6,0]]],[8,1]], 1384),
+        pytest.param([[[[1,1],[2,2]],[3,3]],[4,4]], 445),
+        pytest.param([[[[3,0],[5,3]],[4,4]],[5,5]], 791),
+        pytest.param([[[[5,0],[7,4]],[5,5]],[6,6]], 1137),
+        pytest.param([[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]], 3488),
+        pytest.param([[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]], 4140),
+    ],    
+)
+def test_snailfish_number_get_magnitude(input, expected):
+    assert solution.get_magnitude(input) == expected
+
+
 
 @pytest.mark.parametrize(
     "original, expected",
@@ -15,6 +34,7 @@ import solution
         pytest.param([[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]], [[[[0,7],4],[7,[[8,4],9]]],[1,1]]),
     ],    
 )
+@pytest.mark.skip(reason="TODO")
 def test_snailfish_number_explode(original, expected):
     assert solution.explode(original) == expected
 
@@ -24,7 +44,8 @@ def test_snailfish_number_explode(original, expected):
         pytest.param([[[[4,3],4],4],[7,[[8,4],9]]], [1,1], [[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]])
     ],    
 ) 
-def test_snailfist_number_add(x,y,expected):
+@pytest.mark.skip(reason="TODO")
+def test_snailfish_number_add(x,y,expected):
     assert solution.add(x, y) == expected
 
 @pytest.mark.parametrize(
@@ -33,5 +54,6 @@ def test_snailfist_number_add(x,y,expected):
         pytest.param([[[[4,3],4],4],[7,[[8,4],9]]], [1,1], [[[[0,7],4],[[7,8],[6,0]]],[8,1]])
     ],    
 ) 
-def test_snailfist_number_add_and_reduce(x,y,expected):
+@pytest.mark.skip(reason="TODO")
+def test_snailfish_number_add_and_reduce(x,y,expected):
     assert solution.add_and_reduce(x, y) == expected
