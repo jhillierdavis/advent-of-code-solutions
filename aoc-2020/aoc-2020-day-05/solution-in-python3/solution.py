a6d3de8fa78fcfbcaa0689ea040c0a414102a256
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-08-21 14:04:40 +0100

    AOC 2020 Day 05 Part 1: WIP: Added unit tests

--- aoc-2020/aoc-2020-day-05/solution-in-python3/solution.py
+++ aoc-2020/aoc-2020-day-05/solution-in-python3/solution.py
@@ -10,6 +10,22 @@ logging.config.fileConfig('logging.conf')
 logger = logging.getLogger(__name__)
 logger = logging.getLogger('simpleLogger')
 
+
+def get_row(boarding_pass:str):
+    return 0
+
+def get_column(boarding_pass:str):
+    return 0
+
+def calculate_seat_id(row:int, column:int) -> int:
+    return (row * 8) + column
+
+def get_seat_id(boarding_pass:str):
+    row = get_row(boarding_pass)
+    column = get_column(boarding_pass)
+    return calculate_seat_id(row, column)
+
+
 def solve_part1(filename):
     logger.debug("TODO: Implement Part 1")
     lines = fileutils.get_file_lines_from(filename)
--- aoc-2020/aoc-2020-day-05/solution-in-python3/test_solution.py
+++ aoc-2020/aoc-2020-day-05/solution-in-python3/test_solution.py
@@ -2,6 +2,28 @@ import pytest
 
 import solution
 
+
+@pytest.mark.parametrize(
+    "boarding_pass, expected_column, expected_row, expected_seat_id",
+    [
+        pytest.param('FBFBBFFRLR', 44, 5, 357),
+        #pytest.param('BFFFBBFRRR', 70, 7, 567),
+        #pytest.param('FBFBBFFRLR', 14, 7, 119),
+        #pytest.param('FBFBBFFRLR', 102, 4, 820),
+        
+    ],    
+)
+def test_get_seat_id(boarding_pass, expected_column, expected_row, expected_seat_id):
+    row = solution.get_row(boarding_pass)
+    assert expected_row == row
+
+    column = solution.get_column(boarding_pass)
+    assert expected_column == column
+    
+    seat_id = solution.get_seat_id(boarding_pass)
+    assert expected_seat_id == seat_id
+
+
 input_example = "AOC-2020-Day-05_Puzzle-Input-Example.txt"
 input_full = "AOC-2020-Day-05_Puzzle-Input-Full.txt"
 
@@ -10,7 +32,7 @@ input_full = "AOC-2020-Day-05_Puzzle-Input-Full.txt"
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
+        pytest.param(input_example, 820),
         #pytest.param(input_full, -1),
     ],    
 )
