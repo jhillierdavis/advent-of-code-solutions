e631e7bd5779c425e3688ed5230c89c4aaca316d
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-08-22 14:18:13 +0100

    AOC 2020 Day 6: Refactored solutions (e.g. extract method)

--- aoc-2020/aoc-2020-day-06/solution-in-python3/solution.py
+++ aoc-2020/aoc-2020-day-06/solution-in-python3/solution.py
@@ -10,6 +10,13 @@ logging.config.fileConfig('logging.conf')
 logger = logging.getLogger(__name__)
 logger = logging.getLogger('simpleLogger')
 
+
+def get_entry_count_and_clear(yes_set:set) -> int:
+    count = len(yes_set) # Add number of unique entries
+    yes_set.clear()
+    return count
+
+
 def solve_part1(filename):
     #logger.debug("TODO: Implement Part 1")
     lines = fileutils.get_file_lines_from(filename)
@@ -17,43 +24,54 @@ def solve_part1(filename):
     count = 0
     yes_set = set()
     for l in lines:
-        if len(l) == 0:
-            count += len(yes_set) # Add number of unique entries
-            yes_set = set() # Clear set
-        else:
-            for ch in l:
-                yes_set.add(ch)
+        if len(l) == 0: # Empty line
+            count += get_entry_count_and_clear(yes_set)
+            continue
+
+        for ch in l:
+            yes_set.add(ch)
 
     # Remember to count the last entry (if present)
-    count += len(yes_set)
+    count += get_entry_count_and_clear(yes_set)
 
     return count
+
+
+def get_count_matching_group_size_and_clear(yes_dict:dict, group_size:int) -> int:
+    count = 0
+    for _,v in yes_dict.items():
+        if v == group_size:
+            count += 1     
+    yes_dict.clear()
+    return count
+
+
+def add_to_dict(yes_dict:dict, line:str):
+    for ch in line:                
+        if not ch in yes_dict:
+            yes_dict[ch] = 1
+        else:
+            value = yes_dict[ch]
+            yes_dict[ch] = 1 + value                
+
 
 def solve_part2(filename):
     #logger.debug("TODO: Implement Part 2")
     lines = fileutils.get_file_lines_from(filename)
+    
     count = 0
     yes_dict = dict()
     group_size = 0
     for l in lines:
-        if len(l) == 0:
-            for _,v in yes_dict.items():
-                if v == group_size:
-                    count += 1     
-            yes_dict.clear()
-            group_size = 0       
-        else:
-            group_size += 1
-            for ch in l:                
-                if not ch in yes_dict:
-                    yes_dict[ch] = 1
-                else:
-                    value = yes_dict[ch]
-                    yes_dict[ch] = 1 + value                
+        if len(l) == 0: # Empty
+            count += get_count_matching_group_size_and_clear(yes_dict, group_size)
+            group_size = 0   
+            continue    
+
+        add_to_dict(yes_dict, l)
+        group_size += 1
 
     # Remember to count the last entry (if present)
-    for _,v in yes_dict.items():
-        if v == group_size:
-            count += 1     
+    count += get_count_matching_group_size_and_clear(yes_dict, group_size)   
  
     return count
