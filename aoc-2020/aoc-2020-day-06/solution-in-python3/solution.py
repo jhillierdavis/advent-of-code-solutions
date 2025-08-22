420f34119c0daa21899b0b1ac3a9ef3f0aba2c7e
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-08-22 12:37:53 +0100

    AOC 2020 Day 06: Part 2: Working solution

--- aoc-2020/aoc-2020-day-06/solution-in-python3/solution.py
+++ aoc-2020/aoc-2020-day-06/solution-in-python3/solution.py
@@ -30,7 +30,30 @@ def solve_part1(filename):
     return count
 
 def solve_part2(filename):
-    logger.debug("TODO: Implement Part 2")
+    #logger.debug("TODO: Implement Part 2")
     lines = fileutils.get_file_lines_from(filename)
+    count = 0
+    yes_dict = dict()
+    group_size = 0
+    for l in lines:
+        if len(l) == 0:
+            for _,v in yes_dict.items():
+                if v == group_size:
+                    count += 1     
+            yes_dict.clear()
+            group_size = 0       
+        else:
+            group_size += 1
+            for ch in l:                
+                if not ch in yes_dict:
+                    yes_dict[ch] = 1
+                else:
+                    value = yes_dict[ch]
+                    yes_dict[ch] = 1 + value                
 
-    return "TODO"
+    # Remember to count the last entry (if present)
+    for _,v in yes_dict.items():
+        if v == group_size:
+            count += 1     
+ 
+    return count
