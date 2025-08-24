5c169930d19b4e112a6d5848a6cc09b0b545f6e2
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-08-24 20:59:07 +0100

    AOC 2020 Day 08: Part 1: Working solution

--- aoc-2020/aoc-2020-day-08/solution-in-python3/solution.py
+++ aoc-2020/aoc-2020-day-08/solution-in-python3/solution.py
@@ -11,10 +11,49 @@ logger = logging.getLogger(__name__)
 logger = logging.getLogger('simpleLogger')
 
 def solve_part1(filename):
-    logger.debug("TODO: Implement Part 1")
+    #logger.debug("TODO: Implement Part 1")
     lines = fileutils.get_file_lines_from(filename)
 
-    return "TODO"
+    operation_list = []
+
+    for l in lines:
+        o, v = l.split(' ')
+        logger.debug(f"o={o}, v={v}")
+        operation_list.append((o, int(v)))
+
+    size = len(operation_list)                          
+    logger.debug(f"operation_list={operation_list} size={size}")
+
+    accumulator = 0
+    executed = set()
+
+    index = 0
+    
+    while(True):
+        
+        e = operation_list[index]
+        logger.debug(f"index={index} e={e} accumulator={accumulator}")
+        if index in executed:
+            logger.debug(f"Loop at index={index}")
+            break
+        else:
+            executed.add(index)
+        
+        o, v = e
+        if o == 'nop':
+            index += 1
+        elif o == 'acc':
+            accumulator += v
+            index += 1
+        elif o == 'jmp':
+            if v >= 0:
+                index += (v % size)
+            else:
+                offset = (abs(v) % size)
+                index -= offset
+                logger.debug(f"Jump: index={index} v={v} offset={offset}")
+
+    return accumulator
 
 def solve_part2(filename):
     logger.debug("TODO: Implement Part 2")
--- aoc-2020/aoc-2020-day-08/solution-in-python3/test_solution.py
+++ aoc-2020/aoc-2020-day-08/solution-in-python3/test_solution.py
@@ -6,12 +6,12 @@ input_example = "AOC-2020-Day-08_Puzzle-Input-Example.txt"
 input_full = "AOC-2020-Day-08_Puzzle-Input-Full.txt"
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
         pytest.param(input_example, 5),
-        #pytest.param(input_full, -1),
+        pytest.param(input_full, 1528),
     ],    
 )
 def test_solve_part1(filename, expected):
