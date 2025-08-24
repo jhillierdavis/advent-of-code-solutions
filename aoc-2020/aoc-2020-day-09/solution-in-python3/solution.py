5da9572d4347e641ed871e9dfc670af4d3def6e5
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-08-24 21:59:49 +0100

    AOC 2020 Day 09: Part 1: Working solution

--- aoc-2020/aoc-2020-day-09/solution-in-python3/solution.py
+++ aoc-2020/aoc-2020-day-09/solution-in-python3/solution.py
@@ -10,11 +10,53 @@ logging.config.fileConfig('logging.conf')
 logger = logging.getLogger(__name__)
 logger = logging.getLogger('simpleLogger')
 
-def solve_part1(filename):
-    logger.debug("TODO: Implement Part 1")
+def solve_part1(filename, preamble:int):
+    #logger.debug("TODO: Implement Part 1")
     lines = fileutils.get_file_lines_from(filename)
 
-    return "TODO"
+    input_list = []
+    for l in lines:
+        input_list.append(int(l))
+
+    logger.debug(f"input_list={input_list}")
+
+    """
+    preamble_list = []
+    for i in range(preamble):
+        preamble_list.append(input_list[i])
+
+    logger.debug(f"preamble_list={preamble_list}")
+
+    valid_set = set()   
+    for i, e in enumerate(preamble_list):
+        for j in range(i+1, preamble):
+            valid_set.add(e + preamble_list[j])
+
+    logger.debug(f"valid_set={valid_set}")
+    """
+
+    for i, e in enumerate(input_list):
+        logger.debug(f"i={i} e={e}")
+        if i < preamble:
+            continue
+
+        preamble_list = []
+        for x in range(i-preamble, i):
+            preamble_list.append(input_list[x])
+        logger.debug(f"i={i} preamble_list={preamble_list}")
+
+        valid_set = set()   
+        for j, je in enumerate(preamble_list):
+            for k in range(j+1, preamble):
+                valid_set.add(je + preamble_list[k])
+
+        logger.debug(f"valid_set={valid_set}")
+
+        if not e in valid_set:
+            return e
+
+    return -1
+
 
 def solve_part2(filename):
     logger.debug("TODO: Implement Part 2")
--- aoc-2020/aoc-2020-day-09/solution-in-python3/test_solution.py
+++ aoc-2020/aoc-2020-day-09/solution-in-python3/test_solution.py
@@ -6,12 +6,12 @@ input_example = "AOC-2020-Day-09_Puzzle-Input-Example.txt"
 input_full = "AOC-2020-Day-09_Puzzle-Input-Full.txt"
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, preamble, expected",
     [
         pytest.param(input_example, 5, 127),
-        #pytest.param(input_full, 25, -1),
+        pytest.param(input_full, 25, 144381670),
     ],    
 )
 def test_solve_part1(filename, preamble, expected):
