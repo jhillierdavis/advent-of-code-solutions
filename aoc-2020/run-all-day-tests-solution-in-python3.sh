a14a6cc0ffdce2afd0b72aebc9ad8c0e3aeb0602
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-08-25 13:44:43 +0100

    AOC 2020: Updated all test execution up to current WIP day (10)

--- aoc-2020/run-all-day-tests-solution-in-python3.sh
+++ aoc-2020/run-all-day-tests-solution-in-python3.sh
@@ -6,7 +6,7 @@ dir=$PWD
 #./run-tests-solution-in-python3.sh
 
 # Loop through sub-directories of each puzzle day & execute unit tests for each
-for i in {01..08}; do
+for i in {01..10}; do
     # Pad value (if required) to 2 digits (e.g. 07)
     x=$i
     while [ ${#x} -ne 2 ];
