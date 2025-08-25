906b02914e3d65bb30410c7191f62b7faf50e896
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-08-25 14:29:43 +0100

    AOC 2020 Day 10: Part 1: Working solution

--- aoc-2020/aoc-2020-day-10/solution-in-python3/solution.py
+++ aoc-2020/aoc-2020-day-10/solution-in-python3/solution.py
@@ -10,12 +10,41 @@ logging.config.fileConfig('logging.conf')
 logger = logging.getLogger(__name__)
 logger = logging.getLogger('simpleLogger')
 
-
-def solve_part1(filename):
-    logger.debug("TODO: Implement Part 1")
+def get_sorted_ascending_adaptor_list(filename):
     lines = fileutils.get_file_lines_from(filename)
 
-    return "TODO"
+    adaptor_list = []
+    for l in lines:
+        adaptor_list.append(int(l))
+
+    adaptor_list.sort()
+
+    max_adaptor = max(adaptor_list)
+    adaptor_list.append(max_adaptor + 3)
+
+    logger.debug(f"adaptor_list={adaptor_list}")
+    return adaptor_list
+
+def get_jolt_diffs(adaptor_list:list, increment:int) -> int:
+    count = 0
+    prior_value = 0
+    for a in adaptor_list:
+        diff = a - prior_value
+        if diff == increment:
+            count += 1
+        prior_value = a
+    return count
+
+
+
+def solve_part1(filename):
+    #logger.debug("TODO: Implement Part 1")
+
+    adaptor_list = get_sorted_ascending_adaptor_list(filename)
+    jolt_diffs_of_1 = get_jolt_diffs(adaptor_list, 1)
+    jolt_diffs_of_3 = get_jolt_diffs(adaptor_list, 3)
+
+    return jolt_diffs_of_1 * jolt_diffs_of_3
 
 
 def solve_part2(filename):
--- aoc-2020/aoc-2020-day-10/solution-in-python3/test_solution.py
+++ aoc-2020/aoc-2020-day-10/solution-in-python3/test_solution.py
@@ -6,14 +6,28 @@ input_example = "AOC-2020-Day-10_Puzzle-Input-Example.txt"
 input_example_larger = "AOC-2020-Day-10_Puzzle-Input-Example-Larger.txt"
 input_full = "AOC-2020-Day-10_Puzzle-Input-Full.txt"
 
+@pytest.mark.parametrize(
+    "filename, increment, expected",
+    [
+        pytest.param(input_example, 1, 7),        
+        pytest.param(input_example_larger, 1, 22),
+        pytest.param(input_example, 3, 5),
+        pytest.param(input_example_larger, 3, 10),
+    ],    
+)
+def test_get_1_jolt_diffs(filename, increment, expected):
+    adaptor_list = solution.get_sorted_ascending_adaptor_list(filename)
+    value = solution.get_jolt_diffs(adaptor_list, increment)
+    assert expected == value
+
 
 #@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_example_larger, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 7*5),
+        pytest.param(input_example_larger, 22*10),
+        pytest.param(input_full, 2432),
     ],    
 )
 def test_solve_part1(filename, expected):
@@ -26,7 +40,8 @@ def test_solve_part1(filename, expected):
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
+        pytest.param(input_example, 8),
+        pytest.param(input_example_larger, 19208),
         #pytest.param(input_full, -1),
     ],    
 )
