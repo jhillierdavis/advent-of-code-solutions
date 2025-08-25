eaee6f09874d5c1d63bab9e3038bd3f08954a6c1
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-08-25 13:43:36 +0100

    AOC 2020 Day 10: Initial fileset

---
+++ aoc-2020/aoc-2020-day-10/solution-in-python3/test_solution.py
@@ -0,0 +1,36 @@
+import pytest
+
+import solution
+
+input_example = "AOC-2020-Day-10_Puzzle-Input-Example.txt"
+input_example_larger = "AOC-2020-Day-10_Puzzle-Input-Example-Larger.txt"
+input_full = "AOC-2020-Day-10_Puzzle-Input-Full.txt"
+
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
+@pytest.mark.parametrize(
+    "filename, expected",
+    [
+        pytest.param(input_example, -1),
+        #pytest.param(input_example_larger, -1),
+        #pytest.param(input_full, -1),
+    ],    
+)
+def test_solve_part1(filename, expected):
+    value = solution.solve_part1(filename)
+    
+    assert expected == value
+
+
+@pytest.mark.skip(reason="TODO: Ignore until implemented")
+@pytest.mark.parametrize(
+    "filename, expected",
+    [
+        pytest.param(input_example, -1),
+        #pytest.param(input_full, -1),
+    ],    
+)
+def test_solve_part2(filename, expected):
+    value = solution.solve_part2(filename)
+    
+    assert expected == value
