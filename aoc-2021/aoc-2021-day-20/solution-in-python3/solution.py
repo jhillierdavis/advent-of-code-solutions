f4d5211b694620bc4633c51642638ec5b12c49d4
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-09-15 22:46:05 +0100

    AOC 2021 Day 20: Part 1: WIP: Example working

--- aoc-2021/aoc-2021-day-20/solution-in-python3/solution.py
+++ aoc-2021/aoc-2021-day-20/solution-in-python3/solution.py
@@ -1,5 +1,5 @@
 # Shared helper libraries
-from helpers import fileutils
+from helpers import fileutils, grid, point
 
 # Logging libraries
 import logging
@@ -10,11 +10,89 @@ logging.config.fileConfig('logging.conf')
 logger = logging.getLogger(__name__)
 logger = logging.getLogger('simpleLogger')
 
+
+def  get_image_enhancement_algorithm(filename):
+    lines = fileutils.get_file_lines(filename)
+    image_enhancement_algorithm = lines[0]
+    logger.debug(f"image_enhancement_algorithm={image_enhancement_algorithm}")
+    return image_enhancement_algorithm
+
+
+def pixels_to_binary(pixel_str):
+    return ''.join('1' if char == '#' else '0' for char in pixel_str)
+
+
+def get_expanded_image_grid(image_grid):
+    expansion = 2
+    expanded_grid = grid.Grid2D(image_grid.get_width() + (2 * expansion), image_grid.get_height() + (2*expansion))
+        
+    for h in range(image_grid.get_height()):
+        for w in range(image_grid.get_width()):
+            symbol = image_grid.get_symbol(point.Point2D(w,h))
+            if symbol == '#':
+                expanded_grid.set_symbol(point.Point2D(w+expansion, h+expansion), '#')
+
+    return expanded_grid
+
+
+def get_value(image_grid, p:point.Point2D) -> set:
+    x = p.get_x()
+    y = p.get_y()
+
+    surrounding_points = [(x-1,y-1), (x,y-1), (x+1,y-1), (x-1,y), (x,y), (x+1,y), (x-1,y+1), (x,y+1),(x+1,y+1)]
+
+    value = ''
+    for (a,b) in surrounding_points:
+        np = point.Point2D(a,b)
+        if image_grid.contains(np):
+            s = image_grid.get_symbol(np)
+            value += s
+    return value    
+
+
+def get_enhanced_image_grid(image_grid, image_enhancement_algorithm):
+    width = image_grid.get_width()
+    height = image_grid.get_height()
+
+    enhanced_grid = grid.Grid2D(height, width)
+    
+
+    for h in range(1, height-1):
+        for w in range(1, width-1):        
+            p = point.Point2D(w,h)
+            value = get_value(image_grid, p)
+            binary_str = pixels_to_binary(value)
+            index = int(binary_str, 2)
+            s = image_enhancement_algorithm[index]
+            logger.debug(f"p={p} value={value} binary_str={binary_str} index={index} s={s}")
+            enhanced_grid.set_symbol(p, s)
+    return enhanced_grid
+
+
 def solve_part1(filename):
     logger.debug("TODO: Implement Part 1")
     lines = fileutils.get_file_lines_from(filename)
+    image_enhancement_algorithm = lines[0]    
 
-    return "TODO"
+    grid_lines = []
+    for i in range(2, len(lines)):        
+        grid_lines.append(lines[i])
+    logger.debug(f"grid_lines={grid_lines}")
+
+    image_grid = grid.lines_to_grid(grid_lines)
+    #grid.display_grid(image_grid)
+
+    for i in range(2):
+        logger.debug(f"Enhancement {i+1}")
+        expanded_grid = get_expanded_image_grid(image_grid)
+        grid.display_grid(expanded_grid)
+
+        enhanced_image_grid = get_enhanced_image_grid(expanded_grid, image_enhancement_algorithm)
+        grid.display_grid(enhanced_image_grid)
+
+        image_grid = enhanced_image_grid
+
+    return enhanced_image_grid.count_symbol('#')
 
 def solve_part2(filename):
     logger.debug("TODO: Implement Part 2")
--- aoc-2021/aoc-2021-day-20/solution-in-python3/test_solution.py
+++ aoc-2021/aoc-2021-day-20/solution-in-python3/test_solution.py
@@ -5,13 +5,41 @@ import solution
 input_example = "AOC-2021-Day-20_Puzzle-Input-Example.txt"
 input_full = "AOC-2021-Day-20_Puzzle-Input-Full.txt"
 
+@pytest.mark.parametrize(
+    "filename",
+    [
+        pytest.param(input_example),
+        #pytest.param(input_full),
+    ],    
+)
+def test_image_enhancement_algorithm(filename):
+    image_enhancement_algorithm = solution.get_image_enhancement_algorithm(filename)
+
+    assert len(image_enhancement_algorithm) == 512
+
+    assert image_enhancement_algorithm[0] == '.'
+    assert image_enhancement_algorithm[10] == '#'
+    assert image_enhancement_algorithm[20] == '#'
+    assert image_enhancement_algorithm[30] == '#'
+    assert image_enhancement_algorithm[34] == '#'
+    assert image_enhancement_algorithm[40] == '#'
+    assert image_enhancement_algorithm[50] == '#'
+    assert image_enhancement_algorithm[60] == '.'
+    assert image_enhancement_algorithm[70] == '.'
+
+
+def test_pixels_to_binary():
+    actual = solution.pixels_to_binary("...#...#.")
+    assert actual == "000100010"
+    assert int(actual, 2) == 34
+
 
 #@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
         pytest.param(input_example, 35),
-        #pytest.param(input_full, -1),
+        pytest.param(input_full, -1), #  5708 too high
     ],    
 )
 def test_solve_part1(filename, expected):
