0c88cf523e40eef25ce8698e680c4406c50cee82
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-09-17 14:27:09 +0100

    AOC 2022: Day 22: Part 1: Working solution

--- aoc-2021/aoc-2021-day-22/solution-in-python3/solution.py
+++ aoc-2021/aoc-2021-day-22/solution-in-python3/solution.py
@@ -10,11 +10,41 @@ logging.config.fileConfig('logging.conf')
 logger = logging.getLogger(__name__)
 logger = logging.getLogger('simpleLogger')
 
+def get_coords(desc_str:str):
+    v1,v2 = desc_str.split("..")
+    return max(int(v1), -50), min(int(v2), 50)
+
+def get_cubes(desc_str:str):
+    values = desc_str.split(',')
+    
+    x1,x2 = get_coords(values[0][2:])
+    y1,y2 = get_coords(values[1][2:])
+    z1,z2 = get_coords(values[2][2:])
+
+    cubes = set()
+    for x in range(int(x1), 1+int(x2)):
+        for y in range(int(y1), 1+int(y2)):
+            for z in range(int(z1), 1+int(z2)):
+                cubes.add((x,y,z))
+    return cubes
+    
+
 def solve_part1(filename):
-    logger.debug("TODO: Implement Part 1")
+    #logger.debug("TODO: Implement Part 1")
     lines = fileutils.get_file_lines_from(filename)
 
-    return "TODO"
+    on_cubes = set()
+    for l in lines:
+        values = l.split(' ')
+        is_on = True if values[0] == 'on' else False
+        cubes = get_cubes(values[1])
+        if is_on:
+            on_cubes = on_cubes.union(cubes)
+        else:
+            on_cubes = on_cubes.difference(cubes)
+
+
+    return len(on_cubes)
 
 def solve_part2(filename):
     logger.debug("TODO: Implement Part 2")
--- aoc-2021/aoc-2021-day-22/solution-in-python3/test_solution.py
+++ aoc-2021/aoc-2021-day-22/solution-in-python3/test_solution.py
@@ -2,16 +2,28 @@ import pytest
 
 import solution
 
+input_example_small = "AOC-2021-Day-22_Puzzle-Input-Example-Small.txt"
 input_example = "AOC-2021-Day-22_Puzzle-Input-Example.txt"
 input_full = "AOC-2021-Day-22_Puzzle-Input-Full.txt"
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+def test_get_cubes_list():
+    cubes = solution.get_cubes("x=10..12,y=10..12,z=10..12")
+    assert len(cubes) == 27 # 3x3x3
+    assert (10,10,10) in cubes
+    assert (10,12,10) in cubes
+    assert (11,11,11) in cubes
+    assert (12,10,12) in cubes
+    assert (12,12,12) in cubes
+
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example_small, 39),
+        pytest.param(input_example, 590784),
+        pytest.param(input_full, 590467), 
     ],    
 )
 def test_solve_part1(filename, expected):
