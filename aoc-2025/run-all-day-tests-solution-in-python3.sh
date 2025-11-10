7d5b18a9d9f2511a7ff3ae3086d7d55a556d41f2
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-11-10 13:32:24 +0000

    Only 12 AOC Days in 2025

--- aoc-2025/run-all-day-tests-solution-in-python3.sh
+++ aoc-2025/run-all-day-tests-solution-in-python3.sh
@@ -6,7 +6,7 @@ dir=$PWD
 #./run-tests-solution-in-python3.sh
 
 # Loop through sub-directories of each puzzle day & execute unit tests for each
-for i in {01..25}; do
+for i in {01..12}; do
     # Pad value (if required) to 2 digits (e.g. 07)
     x=$i
     while [ ${#x} -ne 2 ];
@@ -14,6 +14,6 @@ for i in {01..25}; do
         x="0"$x
     done
 
-    cd $dir/aoc-2024-day-$x
+    cd $dir/aoc-2025-day-$x
     ./run-tests-solution-in-python3.sh
 done
