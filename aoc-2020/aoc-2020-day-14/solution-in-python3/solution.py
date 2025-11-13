fdde4d2a31206c2c19d5370b9bb27847d9125bb9
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-11-13 08:42:48 +0000

    AOC 2020 Day 14: Part 2: Working solution

--- aoc-2020/aoc-2020-day-14/solution-in-python3/solution.py
+++ aoc-2020/aoc-2020-day-14/solution-in-python3/solution.py
@@ -25,9 +25,9 @@ def combine_with_mask_value(value, current_mask):
             new_value += value[i]
         else:
             new_value += current_mask[i]
-            logger.debug(f"i={i} new_value={new_value}")                
+            #logger.debug(f"i={i} new_value={new_value}")                
 
-    logger.debug(f"new_value={new_value}")
+    #logger.debug(f"new_value={new_value}")
     return new_value
 
 
@@ -62,8 +62,114 @@ def solve_part1(filename):
     return total
 
 
+def combine_with_mask_value_part2(value, current_mask):
+    new_value = ""
+    for i in range(len(current_mask)):
+        if current_mask[i] == '0':
+            new_value += value[i]
+        elif current_mask[i] == '1':
+            new_value += '1'
+        else:
+            new_value += current_mask[i]
+    return new_value
+
+
+def add_values(result, xmask, offset, value):
+    size = len(xmask) 
+    if offset >= size:
+        result.add(value)
+        return
+    
+    prefix = value
+    for i in range(offset, size):
+        if xmask[i] == 'X':
+            add_values(result, xmask, i+1, prefix + '0')
+            add_values(result, xmask, i+1, prefix + '1')
+            break
+        else:
+            prefix += xmask[i]
+    return result
+
+
+def generate_floating_value_set(xmask):
+    result = set()
+    if 'X' not in xmask:
+        result.add(xmask)
+        return result
+    
+    add_values(result, xmask, 0, "")
+    return result
+
+"""
+def generate_floating_values(s):
+    if 'X' not in s:
+        return s
+
+    addresses = []
+    xi = s.index('X')
+    s0 = s[:xi] + '0' + s[xi+1:]
+    s1 = s[:xi] + '1' + s[xi+1:]
+
+    addresses += generate_floating_values(s0) + ',' + generate_floating_values(s1)
+    return addresses
+"""
+
+def generate_floating_values(s, results:list):
+    if 'X' not in s:
+        results.append(s)
+        return
+    
+    xi = s.index('X')
+    s0 = s[:xi] + '0' + s[xi+1:]
+    s1 = s[:xi] + '1' + s[xi+1:]
+
+    generate_floating_values(s0, results)
+    generate_floating_values(s1, results)
+    return
+
+
+def generate_floating_value_set_as_intergers(xmask):
+    #results = sorted(generate_floating_value_set(xmask))
+    results = []
+    generate_floating_values(xmask, results)
+    converted_results = set()
+    for r in results:
+        converted_results.add(int(r,2))
+    return converted_results
+
 def solve_part2(filename):
-    logger.debug("TODO: Implement Part 2")
+    #logger.debug("TODO: Implement Part 2")
     lines = fileutils.get_file_lines_from(filename)
 
-    return "TODO"
+    memvalues = defaultdict(set[int])
+    
+    current_mask = None
+    mask_length = 36
+    for l in lines:
+        if len(l) == 0:
+            continue
+
+        k,v = l.split(" = ")
+        #logger.debug(f"{l} k={k} v={v}")
+        
+        if k.startswith('mask'):            
+            #logger.debug(f"mask = {v}")
+            current_mask = v
+        elif k.startswith('mem'):
+            nums = extract_numbers(l)
+            index = nums[0]
+            value = format(index, '0' + str(mask_length) + 'b')
+            logger.debug(f"l={l} index={index} value={value} current_mask={current_mask}")
+            
+            xmask = combine_with_mask_value_part2(value, current_mask)
+            values = generate_floating_value_set_as_intergers(xmask)
+            logger.debug(f"current_mask={current_mask} xmask={xmask}, index={index} values={values}")
+            for v in values:                
+                memvalues[v] = nums[1]           
+
+    #logger.debug(memvalues)
+    return sum(memvalues.values())
+
+#xmask = '1X0XX'
+#print(sorted(generate_floating_value_set(xmask)))
+#print(get_addresses(xmask))
--- aoc-2020/aoc-2020-day-14/solution-in-python3/test_solution.py
+++ aoc-2020/aoc-2020-day-14/solution-in-python3/test_solution.py
@@ -20,12 +20,50 @@ def test_solve_part1(filename, expected):
     assert expected == value
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+@pytest.mark.parametrize(
+    "xmask, expected",
+    [
+ #      pytest.param("00000000000000000000000000000001X0XX", {16,17,18,19,24,25,26,27}),
+        pytest.param("00000000000000000000000000000001X0XX", {"000000000000000000000000000000010000","000000000000000000000000000000010001","000000000000000000000000000000010010","000000000000000000000000000000010011","000000000000000000000000000000011000","000000000000000000000000000000011001","000000000000000000000000000000011010","000000000000000000000000000000011011"}),
+    ],    
+)
+def test_generate_floating_value_set(xmask, expected):
+    value = solution.generate_floating_value_set(xmask)    
+    assert expected == value
+
+
+@pytest.mark.parametrize(
+    "xmask, expected",
+    [
+        pytest.param("00000000000000000000000000000001X0XX", {16,17,18,19,24,25,26,27}),
+    ],    
+)
+def test_generate_floating_value_set_as_intergers(xmask, expected):
+    value = solution.generate_floating_value_set_as_intergers(xmask)    
+    assert expected == value
+
+
+input_example_part2 = "AOC-2020-Day-14_Puzzle-Input-Example-Part2.txt"
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
+@pytest.mark.parametrize(
+    "xmask, expected",
+    [
+        pytest.param("X0XX1", ['00001','00011','00101','00111','10001','10011','10101','10111']),        
+    ],    
+)
+def test_generate_floating_values(xmask, expected):
+    values = []
+    solution.generate_floating_values(xmask, values)    
+    assert expected == values
+
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        #pytest.param(input_example_part2, 208),
+        pytest.param(input_full, 4195339838136), #924192532172 too low! 4195339838136
     ],    
 )
 def test_solve_part2(filename, expected):
