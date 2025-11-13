539bef2b03c41955d9e3adad6d1ec0c50be47195
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-11-13 18:36:36 +0000

    AOC 2020 Day 15: Part 1 and 2: Working solution: Refactored

--- aoc-2020/aoc-2020-day-15/solution-in-python3/solution.py
+++ aoc-2020/aoc-2020-day-15/solution-in-python3/solution.py
@@ -13,45 +13,6 @@ logger = logging.getLogger('simpleLogger')
 from collections import defaultdict
 
 
-def last_index_of(lst, value):
-    """
-    Returns the index of the last occurrence of 'value' in the list 'lst'.
-    If the value is not found, returns -1.
-    """
-    for i in range(len(lst) - 1, -1, -1):
-        if lst[i] == value:
-            return i
-    return -1
-
-
-def solve_part1(starting_values, nth=2020):  
-    sequence = []
-    current_value = 0
-    size = len(starting_values)
-
-    for i in range(0, nth):
-        if i < size:
-            current_value = starting_values[i]
-            logger.debug(f"Starting value: i={i} current_value={current_value} sequence={sequence} nth={nth}")
-            if i < size -1:
-                sequence.append(current_value)
-                continue
-            
-        if i == nth -1:
-            break
-
-        last_index = last_index_of(sequence, current_value)
-        sequence.append(current_value)
-        if last_index >= 0:
-            logger.debug(f"i={i} current_value={current_value} last_index={last_index} sequence={sequence}")
-            current_value = i - last_index
-        else:            
-            current_value = 0
-        logger.debug(f"i={i} current_value={current_value}")
-
-    return current_value
-
-
 def solve(starting_values, nth):
     assert nth >= 0
     age = defaultdict(lambda:-1)
