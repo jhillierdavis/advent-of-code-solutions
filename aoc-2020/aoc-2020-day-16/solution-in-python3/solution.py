eef3db0656af34f817df208ab5bbc77e12f2ff40
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-11-14 15:31:33 +0000

    AOC 2020 Day 16: Part 2: WIP

--- aoc-2020/aoc-2020-day-16/solution-in-python3/solution.py
+++ aoc-2020/aoc-2020-day-16/solution-in-python3/solution.py
@@ -35,7 +35,7 @@ def count_invalid_nearby_ticket_entries(lines, ranges):
             continue
 
         if found:            
-            logger.debug(f"l={l}")    
+            #logger.debug(f"l={l}")    
             vals = l.split(',')
             for v in vals:
                 is_valid = False
@@ -58,8 +58,108 @@ def solve_part1(filename):
     return ans
 
 
-def solve_part2(filename):
-    logger.debug("TODO: Implement Part 2")
-    lines = fileutils.get_file_lines_from(filename)
+def get_valid_nearby_ticket_entries(lines, ranges):
+    found = False
+    valid_lines = list()
+    for l in lines:
+        if l == 'nearby tickets:':
+            found = True
+            continue
 
+        if found:            
+            #logger.debug(f"l={l}")    
+            vals = l.split(',')
+
+            
+            for v in vals:
+                is_valid = False                
+                vnum = int(v)
+                for r in ranges:
+                    if numutils.is_within_range(vnum, r[0], r[1]):
+                        is_valid = True                        
+                        continue
+                if not is_valid:
+                    break
+
+            if is_valid:
+                valid_list = list()
+                for v in vals:
+                    vnum = int(v)
+                    valid_list.append(vnum)
+                valid_lines.append(valid_list)
+                    
+    return valid_lines
+
+
+def get_valid_nearby_tickets(filename):
+    lines = fileutils.get_lines_before_empty_from_file(filename)
+    ranges = extract_anonymous_ranges(lines)
+
+    lines = fileutils.get_lines_after_empty_from_file(filename)
+    valid_nearby = get_valid_nearby_ticket_entries(lines, ranges)
+    return valid_nearby
+
+
+def extract_field_ranges(filename):
+    lines = fileutils.get_lines_before_empty_from_file(filename)
+    field_ranges = dict()
+    
+    for l in lines:
+        ranges = list()
+        vals = l.split(' ')
+        field = vals[0][:-1]
+        #logger.debug(f"line={l} vals={vals}")
+        for v in vals:
+            if '-' in v:
+                vmin, vmax = v.split("-")
+                #logger.debug(f"vmin={vmin}, vmax={vmax}")
+                ranges.append((int(vmin), int(vmax)))
+        field_ranges[field] = ranges
+    #logger.debug(f"ranges={ranges}")
+    return field_ranges
+
+
+def get_field_order(filename):    
+    field_ranges = extract_field_ranges(filename)
+
+    logger.debug(f"field_ranges={field_ranges}")
+    num_fields = len(field_ranges.keys())
+
+    d = dict()
+    for x in range(num_fields):
+        d[x] = set(field_ranges.keys())
+
+    logger.debug(f"d={d}")
+
+    valid_nearby = get_valid_nearby_tickets(filename)
+
+    for kfr, vfr in field_ranges.items():
+        for vn in valid_nearby:
+            
+            for i, v in enumerate(vn):
+                is_valid = False    
+                for r in vfr:
+                    if numutils.is_within_range(v, r[0], r[1]):
+                        is_valid = True
+#                if is_invalid:
+#                    logger.debug(f"{kfr} invalid in range at {i}!") 
+                if not is_valid:
+                    s = d[i]
+                    s.remove(kfr)
+                    d[i] = s
+
+    logger.debug(f"d={d}")
+
+    return []
+
+
+def solve_part2(filename):
+    #logger.debug("TODO: Implement Part 2")
+    lines = fileutils.get_lines_before_empty_from_file(filename)
+    ranges = extract_anonymous_ranges(lines)
+
+    lines = fileutils.get_lines_after_empty_from_file(filename)
+    valid_nearby = get_valid_nearby_ticket_entries(lines, ranges)
+
+    logger.debug(f"valid_nearby={valid_nearby}")
     return "TODO"
--- aoc-2020/aoc-2020-day-16/solution-in-python3/test_solution.py
+++ aoc-2020/aoc-2020-day-16/solution-in-python3/test_solution.py
@@ -15,8 +15,45 @@ input_full = "AOC-2020-Day-16_Puzzle-Input-Full.txt"
     ],    
 )
 def test_solve_part1(filename, expected):
-    value = solution.solve_part1(filename)
-    
+    value = solution.solve_part1(filename)    
+    assert expected == value
+
+
+input_example_part2 = "AOC-2020-Day-16_Puzzle-Input-Example-Part2.txt"
+
+@pytest.mark.parametrize(
+    "filename, expected",
+    [
+        pytest.param(input_example, [[7,3,47]]),
+        pytest.param(input_example_part2, [[3, 9, 18], [15, 1, 5], [5, 14, 9]]),
+    ],    
+)
+def test_get_valid_nearby_tickets(filename, expected):
+    value = solution.get_valid_nearby_tickets(filename)    
+    assert expected == value
+
+
+@pytest.mark.parametrize(
+    "filename, expected",
+    [
+        pytest.param(input_example_part2, {'class': [(0, 1), (4, 19)], 'row': [(0, 5), (8, 19)], 'seat': [(0, 13), (16, 19)]}),
+    ],    
+)
+def test_extract_field_ranges(filename, expected):
+    value = solution.extract_field_ranges(filename)
+    assert expected == value
+
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
+@pytest.mark.parametrize(
+    "filename, expected",
+    [
+        pytest.param(input_example_part2, ['row', 'column', 'seat']),
+        pytest.param(input_full, ['row', 'column', 'seat']),
+    ],    
+)
+def test_get_field_order(filename, expected):
+    value = solution.get_field_order(filename)
     assert expected == value
 
 
@@ -29,6 +66,5 @@ def test_solve_part1(filename, expected):
     ],    
 )
 def test_solve_part2(filename, expected):
-    value = solution.solve_part2(filename)
-    
+    value = solution.solve_part2(filename)    
     assert expected == value
