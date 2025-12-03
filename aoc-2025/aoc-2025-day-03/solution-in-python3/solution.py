ce1e4603d2edbcdbb103132fb070f681a101fb5d
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-12-03 05:59:13 +0000

    AOC 2025 Day 03: Solved parts 1 & 2

--- aoc-2025/aoc-2025-day-03/solution-in-python3/solution.py
+++ aoc-2025/aoc-2025-day-03/solution-in-python3/solution.py
@@ -10,14 +10,58 @@ logging.config.fileConfig('logging.conf')
 logger = logging.getLogger(__name__)
 logger = logging.getLogger('simpleLogger')
 
+
+def get_max_digit_and_index(value:str) -> int:
+    size = len(value)
+    max_value = 0
+    max_index = 0
+    for i in range(size):
+        iv = int(value[i])
+        if iv > max_value:
+            max_value = iv
+            max_index = i
+    #logger.debug(f"value={value} max_value={max_value} max_index={max_index}")
+    return max_value, max_index
+
+
+
+def get_max_joltage(value:str) -> int:
+    d1,i1 = get_max_digit_and_index(value[:-1])
+    d2,i2 = get_max_digit_and_index(value[i1+1:])
+    return (d1*10) + d2
+
+
+def get_largest_joltage(value:str) -> int:
+    d1,i1 = get_max_digit_and_index(value[:-11])
+    d2,i2 = get_max_digit_and_index(value[i1+1:-10])
+    d3,i3 = get_max_digit_and_index(value[i1+i2+2:-9])
+    d4,i4 = get_max_digit_and_index(value[i1+i2+i3+3:-8])
+    d5,i5 = get_max_digit_and_index(value[i1+i2+i3+i4+4:-7])
+    d6,i6 = get_max_digit_and_index(value[i1+i2+i3+i4+i5+5:-6])
+    d7,i7 = get_max_digit_and_index(value[i1+i2+i3+i4+i5+i6+6:-5])
+    d8,i8 = get_max_digit_and_index(value[i1+i2+i3+i4+i5+i6+i7+7:-4])
+    d9,i9 = get_max_digit_and_index(value[i1+i2+i3+i4+i5+i6+i7+i8+8:-3])
+    d10,i10 = get_max_digit_and_index(value[i1+i2+i3+i4+i5+i6+i7+i8+i9+9:-2])
+    d11,i11 = get_max_digit_and_index(value[i1+i2+i3+i4+i5+i6+i7+i8+i9+i10+10:-1])
+    d12,i12 = get_max_digit_and_index(value[i1+i2+i3+i4+i5+i6+i7+i8+i9+i10+i11+11:])
+    return (d1*pow(10,11)) + (d2*pow(10,10)) + (d3*pow(10,9)) + (d4*pow(10,8)) + (d5*pow(10,7)) + (d6*pow(10,6)) + (d7*pow(10,5))  + (d8*pow(10,4)) + (d9*pow(10,3)) + (d10*pow(10,2)) + (d11*10) + d12
+
+
 def solve_part1(filename):
-    logger.debug("TODO: Implement Part 1")
+    #logger.debug("TODO: Implement Part 1")
     lines = fileutils.get_file_lines_from(filename)
 
-    return "TODO"
+    ans = 0
+    for l in lines:
+        ans += get_max_joltage(l)
+    return ans
+
 
 def solve_part2(filename):
-    logger.debug("TODO: Implement Part 2")
+    #logger.debug("TODO: Implement Part 2")
     lines = fileutils.get_file_lines_from(filename)
 
-    return "TODO"
+    ans = 0
+    for l in lines:
+        ans += get_largest_joltage(l)
+    return ans
--- aoc-2025/aoc-2025-day-03/solution-in-python3/test_solution.py
+++ aoc-2025/aoc-2025-day-03/solution-in-python3/test_solution.py
@@ -6,29 +6,55 @@ input_example = "AOC-2025-Day-03_Puzzle-Input-Example.txt"
 input_full = "AOC-2025-Day-03_Puzzle-Input-Full.txt"
 
 
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        pytest.param('987654321111111', 98),
+        pytest.param('811111111111119', 89),
+        pytest.param('234234234234278', 78),
+        pytest.param('818181911112111', 92),
+    ],    
+)
+def test_get_max_joltage(value, expected):
+    value = solution.get_max_joltage(value)    
+    assert expected == value
+
+
 #@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 357),
+        pytest.param(input_full, 17445),
     ],    
 )
 def test_solve_part1(filename, expected):
-    value = solution.solve_part1(filename)
-    
+    value = solution.solve_part1(filename)    
     assert expected == value
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        pytest.param('987654321111111', 987654321111),
+        pytest.param('811111111111119', 811111111119),
+        pytest.param('234234234234278', 434234234278),
+        pytest.param('818181911112111', 888911112111),
+    ],    
+)
+def test_get_largest_joltage(value, expected):
+    value = solution.get_largest_joltage(value)    
+    assert expected == value
+
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 3121910778619),
+        pytest.param(input_full, 173229689350551),
     ],    
 )
 def test_solve_part2(filename, expected):
-    value = solution.solve_part2(filename)
-    
+    value = solution.solve_part2(filename)    
     assert expected == value
