55312acda64693f9f677bff6aedb65b11b5416d5
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-12-03 06:36:51 +0000

    AOC 2025 Day 03: Refactored

--- aoc-2025/aoc-2025-day-03/solution-in-python3/solution.py
+++ aoc-2025/aoc-2025-day-03/solution-in-python3/solution.py
@@ -24,13 +24,13 @@ def get_max_digit_and_index(value:str) -> int:
     return max_value, max_index
 
 
-
+"""
 def get_max_joltage(value:str) -> int:
     d1,i1 = get_max_digit_and_index(value[:-1])
     d2,i2 = get_max_digit_and_index(value[i1+1:])
     return (d1*10) + d2
-
-
+"""
+"""
 def get_largest_joltage(value:str) -> int:
     d1,i1 = get_max_digit_and_index(value[:-11])
     d2,i2 = get_max_digit_and_index(value[i1+1:-10])
@@ -45,23 +45,33 @@ def get_largest_joltage(value:str) -> int:
     d11,i11 = get_max_digit_and_index(value[i1+i2+i3+i4+i5+i6+i7+i8+i9+i10+10:-1])
     d12,i12 = get_max_digit_and_index(value[i1+i2+i3+i4+i5+i6+i7+i8+i9+i10+i11+11:])
     return (d1*pow(10,11)) + (d2*pow(10,10)) + (d3*pow(10,9)) + (d4*pow(10,8)) + (d5*pow(10,7)) + (d6*pow(10,6)) + (d7*pow(10,5))  + (d8*pow(10,4)) + (d9*pow(10,3)) + (d10*pow(10,2)) + (d11*10) + d12
+"""
+
+def get_largest_joltage(value:str, digits:int=2) -> int:
+    result = 0
+    offset = 0
+    for d in range(0,digits):
+        x = digits-(d+1)
+        sub_v = value[offset:-x] if x > 0 else value[offset:]
+        v,i = get_max_digit_and_index(sub_v)
+        offset += (i + 1)
+        result += v * pow(10,x)
+        #logger.debug(f"value={value} d={d} x={x} sub_v={sub_v} v={v} i={i} offset={offset} result={result}")
+    return result
 
 
 def solve_part1(filename):
-    #logger.debug("TODO: Implement Part 1")
-    lines = fileutils.get_file_lines_from(filename)
-
-    ans = 0
-    for l in lines:
-        ans += get_max_joltage(l)
-    return ans
+    return process_input_data_lines(filename, 2)
 
 
 def solve_part2(filename):
-    #logger.debug("TODO: Implement Part 2")
+    return process_input_data_lines(filename, 12)
+
+
+def process_input_data_lines(filename:str, digits:int=2) -> int:
     lines = fileutils.get_file_lines_from(filename)
 
     ans = 0
     for l in lines:
-        ans += get_largest_joltage(l)
+        ans += get_largest_joltage(l, digits)
     return ans
--- aoc-2025/aoc-2025-day-03/solution-in-python3/test_solution.py
+++ aoc-2025/aoc-2025-day-03/solution-in-python3/test_solution.py
@@ -5,7 +5,7 @@ import solution
 input_example = "AOC-2025-Day-03_Puzzle-Input-Example.txt"
 input_full = "AOC-2025-Day-03_Puzzle-Input-Full.txt"
 
-
+"""
 @pytest.mark.parametrize(
     "value, expected",
     [
@@ -16,9 +16,9 @@ input_full = "AOC-2025-Day-03_Puzzle-Input-Full.txt"
     ],    
 )
 def test_get_max_joltage(value, expected):
-    value = solution.get_max_joltage(value)    
+    value = solution.get_largest_joltage(value, 2)
     assert expected == value
-
+"""
 
 #@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
@@ -34,16 +34,20 @@ def test_solve_part1(filename, expected):
 
 
 @pytest.mark.parametrize(
-    "value, expected",
+    "value, size, expected",
     [
-        pytest.param('987654321111111', 987654321111),
-        pytest.param('811111111111119', 811111111119),
-        pytest.param('234234234234278', 434234234278),
-        pytest.param('818181911112111', 888911112111),
+        pytest.param('987654321111111', 2, 98),
+        pytest.param('811111111111119', 2, 89),
+        pytest.param('234234234234278', 2, 78),
+        pytest.param('818181911112111', 2, 92),
+        pytest.param('987654321111111', 12, 987654321111),
+        pytest.param('811111111111119', 12, 811111111119),
+        pytest.param('234234234234278', 12, 434234234278),
+        pytest.param('818181911112111', 12, 888911112111),
     ],    
 )
-def test_get_largest_joltage(value, expected):
-    value = solution.get_largest_joltage(value)    
+def test_get_largest_joltage(value, size, expected):
+    value = solution.get_largest_joltage(value, size)    
     assert expected == value
 
 
