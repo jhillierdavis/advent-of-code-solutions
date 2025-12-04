dce65ae338899c8e7876e5f57be9c0ea395f3a9c
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-12-04 06:09:01 +0000

    AOC 2025 Day 04: Solved parts 1 & 2

--- aoc-2025/aoc-2025-day-04/solution-in-python3/solution.py
+++ aoc-2025/aoc-2025-day-04/solution-in-python3/solution.py
@@ -1,5 +1,5 @@
 # Shared helper libraries
-from helpers import fileutils
+from helpers import fileutils, grid, point
 
 # Logging libraries
 import logging
@@ -10,14 +10,71 @@ logging.config.fileConfig('logging.conf')
 logger = logging.getLogger(__name__)
 logger = logging.getLogger('simpleLogger')
 
+
+def get_closest_neighbours(p):
+    neighbour_set = set()
+    for x in range(-1,2):
+        for y in range(-1,2):
+            np = point.Point2D(p.get_x() + x, p.get_y() + y)
+            neighbour_set.add(np)
+
+    neighbour_set.remove(p)
+    #assert len(neighbour_set) == 26 # 9 + 9 + 8
+    #logger.debug(f"p3d={p3d} neighbour_set={neighbour_set}")
+    return neighbour_set  
+
+def is_roll_symbol(s):
+    return not (s == '.' or s == 'x')
+
+def get_matching_rolls(g):
+    match_set = set()
+    for h in range(g.get_height()):
+        for w in range(g.get_width()):
+            p = point.Point2D(w,h)
+            s = g.get_symbol(p)
+            if not is_roll_symbol(s):
+                continue
+
+            count = 0
+            neighbours = get_closest_neighbours(p)
+            for np in neighbours:
+                if g.contains(np):
+                    ns = g.get_symbol(np)
+                    if not is_roll_symbol(ns):
+                        continue
+                    else:
+                        count += 1
+
+            if count < 4:
+                #logger.debug(f"s={s} p={p} count={count}")            
+                match_set.add(p)
+    return match_set
+
+
 def solve_part1(filename):
-    logger.debug("TODO: Implement Part 1")
     lines = fileutils.get_file_lines_from(filename)
 
-    return "TODO"
+    g = grid.lines_to_grid(lines)
+    #grid.display_grid(g)
+
+    match_set = get_matching_rolls(g)
+    return len(match_set)
+
 
 def solve_part2(filename):
-    logger.debug("TODO: Implement Part 2")
+    #logger.debug("TODO: Implement Part 2")
     lines = fileutils.get_file_lines_from(filename)
 
-    return "TODO"
+    ans = 0
+    g = grid.lines_to_grid(lines)
+    for i in range(100):
+        match_set = get_matching_rolls(g)
+        count = len(match_set)
+        if count == 0:
+            break
+
+        ans += count
+        for m in match_set:
+            g.set_symbol(m, 'x')
+
+    return ans
--- aoc-2025/aoc-2025-day-04/solution-in-python3/test_solution.py
+++ aoc-2025/aoc-2025-day-04/solution-in-python3/test_solution.py
@@ -10,25 +10,23 @@ input_full = "AOC-2025-Day-04_Puzzle-Input-Full.txt"
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 13),
+        pytest.param(input_full, 1551),
     ],    
 )
 def test_solve_part1(filename, expected):
-    value = solution.solve_part1(filename)
-    
+    value = solution.solve_part1(filename)    
     assert expected == value
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 43),
+        pytest.param(input_full, 9784),
     ],    
 )
 def test_solve_part2(filename, expected):
-    value = solution.solve_part2(filename)
-    
+    value = solution.solve_part2(filename)    
     assert expected == value
