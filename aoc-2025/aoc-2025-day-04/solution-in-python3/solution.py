7333f63a1552e5d8b15e19343c98e7e5bdafd77c
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-12-04 09:22:22 +0000

    AOC 2025 Day 04: Refactored: Tidied

--- aoc-2025/aoc-2025-day-04/solution-in-python3/solution.py
+++ aoc-2025/aoc-2025-day-04/solution-in-python3/solution.py
@@ -5,32 +5,19 @@ from helpers import fileutils, grid, point
 import logging
 import logging.config
 
-# create logger
+# Create logger
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger(__name__)
 logger = logging.getLogger('simpleLogger')
 
-"""
-def get_closest_neighbours(p):
-    neighbour_set = set()
-    for x in range(-1,2):
-        for y in range(-1,2):
-            np = point.Point2D(p.get_x() + x, p.get_y() + y)
-            neighbour_set.add(np)
-
-    neighbour_set.remove(p)
-    #assert len(neighbour_set) == 26 # 9 + 9 + 8
-    #logger.debug(f"p3d={p3d} neighbour_set={neighbour_set}")
-    return neighbour_set  
-"""
 
 def is_roll_symbol(s):
-    #return not (s == '.' or s == 'x')
     return s == '@'
 
 
 def get_matching_rolls(g):
     match_set = set()
+
     for h in range(g.get_height()):
         for w in range(g.get_width()):
             p = point.Point2D(w,h)
@@ -51,6 +38,7 @@ def get_matching_rolls(g):
             if count < 4:
                 #logger.debug(f"s={s} p={p} count={count}")            
                 match_set.add(p)
+
     return match_set
 
 
