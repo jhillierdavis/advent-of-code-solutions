0fcc26abdf8d4cad056df887d988d9dfc10dfc01
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-12-05 10:28:09 +0000

    AOC 2025 Day 05: Solved parts 1 & 2

--- aoc-2025/aoc-2025-day-05/solution-in-python3/solution.py
+++ aoc-2025/aoc-2025-day-05/solution-in-python3/solution.py
@@ -5,19 +5,88 @@ from helpers import fileutils
 import logging
 import logging.config
 
-# create logger
+# Create logger
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger(__name__)
 logger = logging.getLogger('simpleLogger')
 
-def solve_part1(filename):
-    logger.debug("TODO: Implement Part 1")
-    lines = fileutils.get_file_lines_from(filename)
 
-    return "TODO"
+def is_fresh(i_id, range_list):
+    for r in range_list:
+        r_min = r[0]
+        r_max = r[1]
+
+        if i_id >= r_min and i_id <= r_max:
+            #logger.debug(f"Fresh: i_id={i_id} in range r={r}")
+            return True
+    return False
+
+
+def get_ranges(filename):
+    range_lines = fileutils.get_lines_before_empty_from_file(filename)
+
+    range_list = []
+    for rl in range_lines:
+        r_min, r_max = rl.split('-')
+        r = (int(r_min),int(r_max))
+        assert r[0] <= r[1], f"r_min={r_min} r_max={r_max}"
+        range_list.append(r)
+    return range_list
+
+
+def solve_part1(filename):
+    #logger.debug("TODO: Implement Part 1")
+    #lines = fileutils.get_file_lines_from(filename)
+    
+    range_list = get_ranges(filename)
+
+    #logger.debug(f"range_list={range_list}")
+
+    ingredient_lines = fileutils.get_lines_after_empty_from_file(filename)
+    fresh_count = 0
+    for il in ingredient_lines:
+        i_id = int(il)
+        if is_fresh(i_id, range_list):
+            fresh_count += 1
+
+    return fresh_count
+
+
+def merge_overlapping_ranges(range_list:list[tuple[int,int]]) -> set[tuple[int,int]]:
+    range_list.sort(key=lambda r: r[0]) # Sort by min value in each range(min,max) in list
+    
+    merged_range_set = set()
+
+    size = len(range_list)
+    rpmin = range_list[0][0] # First range min
+    rpmax = range_list[0][1] # First range max
+
+    for i in range(1,size):
+        r = range_list[i]
+        if rpmax < r[0]:
+            # Add range
+            merged_range_set.add((rpmin, rpmax))
+            rpmin = r[0] # Range min
+            rpmax = r[1] # Range max
+        else:
+            rpmax = max(rpmax, r[1]) 
+    
+    # Add last merged range (if not already)
+    merged_range_set.add((rpmin, rpmax))
+
+    return merged_range_set
+
 
 def solve_part2(filename):
-    logger.debug("TODO: Implement Part 2")
-    lines = fileutils.get_file_lines_from(filename)
+    range_list = get_ranges(filename)
+    #logger.debug(f"Original: range_list={range_list}")
+    
+    unique_range_set = merge_overlapping_ranges(range_list)
+    #logger.debug(f"Merged: unique_range_set={unique_range_set}")
 
-    return "TODO"
+    ans = 0
+    for r in unique_range_set:
+        r_size = (r[1] - r[0]) + 1
+        ans += r_size    
+
+    return ans
--- aoc-2025/aoc-2025-day-05/solution-in-python3/test_solution.py
+++ aoc-2025/aoc-2025-day-05/solution-in-python3/test_solution.py
@@ -10,25 +10,39 @@ input_full = "AOC-2025-Day-05_Puzzle-Input-Full.txt"
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 3),
+        pytest.param(input_full, 737),
     ],    
 )
 def test_solve_part1(filename, expected):
-    value = solution.solve_part1(filename)
-    
+    value = solution.solve_part1(filename)    
     assert expected == value
 
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        pytest.param([(1, 1), (3, 5)], {(1, 1), (3, 5)}),
+        pytest.param([(3, 5), (1, 1)], {(1, 1), (3, 5)}),
+        pytest.param([(3, 5), (1, 1), (2,4)], {(1, 1), (2, 5)}),
+        pytest.param([(3, 5), (10, 14), (16, 18), (12, 20)], {(3, 5), (10, 20)}),
+        pytest.param([(3, 5), (10, 14), (16, 20), (12, 18)], {(3, 5), (10, 20)}),
+        pytest.param([(16, 20), (10, 14), (12, 18), (3, 5)], {(3, 5), (10, 20)}), # Unsorted input list
+        pytest.param([(16, 20), (10, 14), (12, 18), (10, 14), (3, 5)], {(3, 5), (10, 20)}), # Unsorted input list with duplicates
+    ],    
+)
+def test_merge_overlapping_ranges(input, expected):
+    actual = solution.merge_overlapping_ranges(input)
+    assert actual == expected
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 14),
+        pytest.param(input_full, 357485433193284),
     ],    
 )
 def test_solve_part2(filename, expected):
-    value = solution.solve_part2(filename)
-    
+    value = solution.solve_part2(filename)    
     assert expected == value
