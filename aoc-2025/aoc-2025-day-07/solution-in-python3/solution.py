d706de741c934bf5af7ff5237236f1d1d27f339e
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-12-07 13:52:34 +0000

    AOC 2025 Day 07: Solved part 2

--- aoc-2025/aoc-2025-day-07/solution-in-python3/solution.py
+++ aoc-2025/aoc-2025-day-07/solution-in-python3/solution.py
@@ -11,24 +11,79 @@ logger = logging.getLogger(__name__)
 logger = logging.getLogger('simpleLogger')
 
 
-def solve_part1(filename):
-    logger.debug("TODO: Implement Part 1")
+def get_all_beam_paths_as_display_lines_from_file(filename):
+    g = create_grid_from_file(filename)
+
+    cp = get_starting_point(g)
+    #logger.debug(f"Current point: cp={cp}")
+    #g.set_symbol(cp, '|')
+
+    splitters = get_splitter_points(g)
+
+    height = g.get_height()    
+    beams = {cp}
+
+    split_count = 0
+    for h in range(height-1):
+        next_beams = set()
+        for bp in beams:
+            x = bp.get_x()
+            y = bp.get_y() 
+
+            np = point.Point2D(x,y+1)
+            if np in splitters:
+                npe = g.get_neighbour_east(np)
+                if npe:
+                    g.set_symbol(npe, '|')
+                    next_beams.add(npe)
+                npw = g.get_neighbour_west(np)
+                if npw:
+                    g.set_symbol(npw, '|')
+                    next_beams.add(npw)
+                split_count += 1
+            else:
+                g.set_symbol(np, '|')
+                next_beams.add(np)
+        
+        beams = next_beams  
+
+    grid.display_grid(g)
+
+    return grid.grid_to_lines(g)
+
+
+def create_grid_from_file(filename):
     lines = fileutils.get_file_lines_from(filename)
 
     g = grid.lines_to_grid(lines)
-
     #grid.display_grid(g)
 
+    return g
+
+
+def get_starting_point(g):
     start_points = g.get_points_matching('S')
     assert len(start_points) == 1
 
-    cp = start_points.pop()
+    return start_points.pop()
+
+
+def get_splitter_points(g):
+    sps = g.get_points_matching('^')
+    assert len(sps) > 0
+    #logger.debug(f"Splitter points: sps={sps}")
+    #logger.debug(f"Splitters size: {len(sps)}")
+    return sps   
+
+
+def solve_part1(filename):
+    g = create_grid_from_file(filename)
+
+    cp = get_starting_point(g)
     #logger.debug(f"Current point: cp={cp}")
     g.set_symbol(cp, '|')
 
-    splitters = g.get_points_matching('^')
-    assert len(splitters) > 0
-    #logger.debug(f"Splitter points: splitters={splitters}")
+    splitters = get_splitter_points(g)
 
     height = g.get_height()    
     beams = {cp}
@@ -65,6 +120,53 @@ def solve_part1(filename):
 
 
 def solve_part2(filename):
-    logger.debug("TODO: Implement Part 2")
-    lines = fileutils.get_file_lines_from(filename)
-    return "TODO"
+    from collections import defaultdict
+
+    g = create_grid_from_file(filename)
+
+    sp = get_starting_point(g)
+    beams = {sp}
+
+    splitters = get_splitter_points(g)
+
+    height = g.get_height()    
+    particle_counter = defaultdict(int)
+    particle_counter[sp.get_x()] = 1 # Single initial particle (& timeline)
+    
+    # Iterate downward through rows, propogating beam tips (particles) & keeping count as go
+    for h in range(1, height-1):   
+        next_beams = set()     
+        beam_tip_counter = defaultdict(int)
+
+        for bp in beams:
+            x = bp.get_x()
+            y = bp.get_y() 
+
+            np = point.Point2D(x,y+1)
+            value = particle_counter[x]
+            if np in splitters:
+                # Split east / right
+                npe = g.get_neighbour_east(np)                
+                if npe:
+                    #g.set_symbol(npe, '|')
+                    next_beams.add(npe)
+                    beam_tip_counter[npe.get_x()] += value
+
+                # Split west / left
+                npw = g.get_neighbour_west(np)
+                if npw:
+                    #g.set_symbol(npw, '|')
+                    next_beams.add(npw)
+                    beam_tip_counter[npw.get_x()] += value
+            else:
+                next_beams.add(np)
+                beam_tip_counter[x] += value
+            
+        beams = next_beams     
+        particle_counter = beam_tip_counter
+            
+        #logger.debug(f"h={h} beams={beams}")       
+
+    #logger.debug(dict(sorted(particle_counter.items())))
+    #logger.debug(sum(particle_counter.values()))
+    return sum(particle_counter.values()) # Number of particles == timelines
--- aoc-2025/aoc-2025-day-07/solution-in-python3/test_solution.py
+++ aoc-2025/aoc-2025-day-07/solution-in-python3/test_solution.py
@@ -6,6 +6,16 @@ input_example = "AOC-2025-Day-07_Puzzle-Input-Example.txt"
 input_full = "AOC-2025-Day-07_Puzzle-Input-Full.txt"
 
 
+def test_get_all_beam_paths_as_display_lines_from_file():
+    from helpers import fileutils
+
+    expected = fileutils.get_file_lines_from("AOC-2025-Day-07_Puzzle-Output-Example.txt")
+
+    actual = solution.get_all_beam_paths_as_display_lines_from_file(input_example)
+
+    assert actual == expected
+
+
 #@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
@@ -19,12 +29,12 @@ def test_solve_part1(filename, expected):
     assert expected == value
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
         pytest.param(input_example, 40),
-        #pytest.param(input_full, -1),
+        pytest.param(input_full, 5921061943075), # 3130 too low
     ],    
 )
 def test_solve_part2(filename, expected):
