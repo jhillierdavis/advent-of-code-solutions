adf52793193e4f648f45880109d49ec03eb159cc
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-12-09 22:33:10 +0000

    AOC 2025: Day 09: Solved part 2: very inefficiently

--- aoc-2025/aoc-2025-day-09/solution-in-python3/solution.py
+++ aoc-2025/aoc-2025-day-09/solution-in-python3/solution.py
@@ -5,7 +5,7 @@ from helpers import fileutils
 import logging
 import logging.config
 
-# create logger
+# Create logger
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger(__name__)
 logger = logging.getLogger('simpleLogger')
@@ -20,36 +20,184 @@ def get_2d_points_from_lines(lines:str) -> list[tuple[int,int]]:
 def get_rectangle_size(p, q) -> int:
     width = 1 + abs(p[0] - q[0])
     height = 1 + abs(p[1] - q[1])
-    rectange = width * height
-    return rectange
+    rectangle_size = width * height
+    return rectangle_size
 
 
-def get_rectangles(points:list[tuple[int,int]]):
-    rectangles = list()
+def get_rectangle_sizes(points:list[tuple[int,int]]):
+    rectangle_sizes = list()
 
     size = len(points)
     for i, p in enumerate(points):
         for j in range(i+1, size):      
             q = points[j]    
 
-            rectangle = get_rectangle_size(p,q)
-            rectangles.append(rectangle)
+            r_size = get_rectangle_size(p,q)
+            rectangle_sizes.append(r_size)
 
-    #logger.debug(f"rectangles={rectangles}")
-    return rectangles
+    #logger.debug(f"rectangle_sizes={rectangle_sizes}")
+    return rectangle_sizes
 
 
-def solve_part1(filename):
+def solve_part1(filename:str) -> int:
     lines = fileutils.get_file_lines_from(filename)
 
     points = get_2d_points_from_lines(lines)
     
-    rectangles = get_rectangles(points)
+    rectangle_sizes = get_rectangle_sizes(points)
 
-    return max(rectangles)
+    return max(rectangle_sizes)
 
 
-def solve_part2(filename):
-    logger.debug("TODO: Implement Part 2")
+from functools import cache
+
+
+@cache
+def get_x_and_y_min_max(p:tuple[int,int], q:tuple[int,int]) -> tuple[int, int, int, int]:
+    x_min = min(p[0], q[0])
+    x_max = max(p[0], q[0])
+    y_min = min(p[1], q[1])
+    y_max = max(p[1], q[1])
+
+    return x_min, x_max, y_min, y_max
+
+
+@cache
+def get_midpoint(p:tuple[int,int], q:tuple[int,int]) -> tuple[int,int]:
+    x_min, x_max, y_min, y_max = get_x_and_y_min_max(p,q)
+    return (x_min + ((x_max - x_min) // 2) , y_min + ((y_max - y_min) // 2) )
+
+
+@cache
+def get_innerpoints(p:tuple[int,int], q:tuple[int,int]) -> list[tuple[int,int]]:
+    inner_points = list()
+
+    x_min, x_max, y_min, y_max = get_x_and_y_min_max(p,q)
+
+    inner_points.append((x_min+1, y_min+1))
+    inner_points.append((x_min+1, y_max-1))
+    inner_points.append((x_max-1, y_max-1))
+    inner_points.append((x_max-1, y_min+1))
+
+    return inner_points
+
+
+@cache
+def get_sample_points(p:tuple[int,int], q:tuple[int,int]) -> list[tuple[int,int]]:
+    sample_points = get_innerpoints(p,q)
+    sample_points.append(get_midpoint(p,q))
+    return sample_points
+
+
+def solve_part2_using_brute_force_approach(filename:str) -> int:
     lines = fileutils.get_file_lines_from(filename)
-    return "TODO"
+
+    points = get_2d_points_from_lines(lines)
+
+    @cache
+    def point_in_rectilinear_polygon(p:tuple[int,int]) -> bool:
+        """
+        Ray casting for rectilinear polygon (inclusive of boundary).
+        
+        see:
+        
+        https://en.wikipedia.org/wiki/Rectilinear_polygon
+        https://en.wikipedia.org/wiki/Point_in_polygon#Ray_casting_algorithm
+        """
+        x, y = p
+        n = len(points)
+        is_point_inside = False
+
+        for i in range(n):
+            x1, y1 = points[i]
+            x2, y2 = points[(i + 1) % n]
+
+            # Boundary check
+            if x1 == x2 and x == x1 and min(y1, y2) <= y <= max(y1, y2):
+                return True
+            
+            if y1 == y2 and y == y1 and min(x1, x2) <= x <= max(x1, x2):
+                return True
+
+            # Ray casting
+            if (y1 > y) != (y2 > y):
+                x_intersect = x1 if x1 == x2 else None
+                if x_intersect is not None:
+                    if x_intersect == x:
+                        return True
+                    if x_intersect > x:
+                        is_point_inside = not is_point_inside
+
+        return is_point_inside
+
+    @cache
+    def is_valid_sub_rectangle(p:tuple[int,int], q:tuple[int,int]) -> bool:
+        x_min, x_max, y_min, y_max = get_x_and_y_min_max(p,q)
+        
+        for x in range(x_min + 1, x_max):
+            if not point_in_rectilinear_polygon((x, y_min)) or not point_in_rectilinear_polygon((x, y_max)):
+                return False
+        
+        for y in range(y_min + 1, y_max):
+            if not point_in_rectilinear_polygon((x_min, y)) or not point_in_rectilinear_polygon((x_max, y)):
+                return False
+                
+        return True
+
+    def has_valid_enclosed_sample_points(p:tuple[int,int], q:tuple[int,int]) -> bool:
+        sample_points = get_sample_points(p,q)
+
+        for sp in sample_points:
+            if not point_in_rectilinear_polygon(sp):
+                return False
+        return True
+
+    def get_valid_rectangle_sizes_using_opposing_corner_points():
+        valid_rectangle_sizes = list()
+        p_size = len(points)
+        for i, p in enumerate(points):
+            #for j, q in enumerate(points):
+            for j in range(i+1, p_size):      
+                q = points[j]
+
+                # Ignore if same point
+                if p == q:
+                    continue
+
+                # Ignore p and q if on same lines (horizontal or vertical)
+                #if p[0] == q[0] or p[1] == q[1]:
+                #    continue
+
+                a = (p[0], q[1])
+                b = (q[0], p[1])
+
+                if a in points and b not in points:
+                    if not point_in_rectilinear_polygon(b):
+                        continue
+
+                elif b in points and a not in points:
+                    if not point_in_rectilinear_polygon(a):
+                        continue
+
+                # Attempt to use some sample points to spot invalid rectangles earlier               
+                if not has_valid_enclosed_sample_points(p,q):
+                    continue
+
+                # Use a brute force approach to validate the rectangle 
+                if is_valid_sub_rectangle(p, q):
+                    r_size = get_rectangle_size(p,q)    
+                    #logger.debug(f"p={p} q={q} a={a} b={b} r_size={r_size}")            
+                    valid_rectangle_sizes.append(r_size)                
+
+        return valid_rectangle_sizes
+
+
+    valid_rectangle_sizes = get_valid_rectangle_sizes_using_opposing_corner_points()
+    
+    #logger.debug(sorted(valid_rectangle_sizes))
+
+    return max(valid_rectangle_sizes)
+
+
+def solve_part2(filename:str) -> int:
+    return "TODO!"
--- aoc-2025/aoc-2025-day-09/solution-in-python3/test_solution.py
+++ aoc-2025/aoc-2025-day-09/solution-in-python3/test_solution.py
@@ -23,15 +23,36 @@ def test_solve_part1(filename, expected):
     assert expected == value
 
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+input_example_other = "AOC-2025-Day-09_Puzzle-Input-Example-Other.txt"
+
+"""
+.X#X............X#X.
+.###............###.
+.###............###.
+.###............###.
+.###............###.
+.###............###.
+.##X############X##.
+.#####X######X#####.
+.######......######.
+.X####X......X####X.
+....................
+"""
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 24),
+        pytest.param(input_example_other, 30), # Not 98!
+        #pytest.param(input_full, 1452422268), # 173060152 too low! , 172842768 too low, not 4602912072, not 4603255632, not 4602912072, not 4761736832, 3143017900, not 4602534508
     ],    
 )
 def test_solve_part2(filename, expected):
-    value = solution.solve_part2(filename)
-    
+    # Slow! (~ 15 mins)
+    value = solution.solve_part2_using_brute_force_approach(filename)
     assert expected == value
+
+    # TODO: find a better (quicker) way!
+    #value = solution.solve_part2(filename)    
+    #assert expected == value
