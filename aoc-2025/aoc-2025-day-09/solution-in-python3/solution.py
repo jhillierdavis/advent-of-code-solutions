d18777b73e503cd9c3dbd2f84312fc2bdcdff8af
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-12-09 05:26:30 +0000

    AOC 2025: Day 09: Solved part 1

--- aoc-2025/aoc-2025-day-09/solution-in-python3/solution.py
+++ aoc-2025/aoc-2025-day-09/solution-in-python3/solution.py
@@ -10,14 +10,46 @@ logging.config.fileConfig('logging.conf')
 logger = logging.getLogger(__name__)
 logger = logging.getLogger('simpleLogger')
 
+
+def get_2d_points_from_lines(lines:str) -> list[tuple[int,int]]:
+    points = [tuple(map(int, l.split(','))) for l in lines]
+    #logger.debug(f"points={points}")
+    return points
+
+
+def get_rectangle_size(p, q) -> int:
+    width = 1 + abs(p[0] - q[0])
+    height = 1 + abs(p[1] - q[1])
+    rectange = width * height
+    return rectange
+
+
+def get_rectangles(points:list[tuple[int,int]]):
+    rectangles = list()
+
+    size = len(points)
+    for i, p in enumerate(points):
+        for j in range(i+1, size):      
+            q = points[j]    
+
+            rectangle = get_rectangle_size(p,q)
+            rectangles.append(rectangle)
+
+    #logger.debug(f"rectangles={rectangles}")
+    return rectangles
+
+
 def solve_part1(filename):
-    logger.debug("TODO: Implement Part 1")
     lines = fileutils.get_file_lines_from(filename)
 
-    return "TODO"
+    points = get_2d_points_from_lines(lines)
+    
+    rectangles = get_rectangles(points)
+
+    return max(rectangles)
+
 
 def solve_part2(filename):
     logger.debug("TODO: Implement Part 2")
     lines = fileutils.get_file_lines_from(filename)
-
     return "TODO"
--- aoc-2025/aoc-2025-day-09/solution-in-python3/test_solution.py
+++ aoc-2025/aoc-2025-day-09/solution-in-python3/test_solution.py
@@ -6,17 +6,20 @@ input_example = "AOC-2025-Day-09_Puzzle-Input-Example.txt"
 input_full = "AOC-2025-Day-09_Puzzle-Input-Full.txt"
 
 
+def test_get_rectange_size():
+    assert 50 == solution.get_rectangle_size((2,5), (11,1))
+
+
 #@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 50),
+        pytest.param(input_full, 4761736832),
     ],    
 )
 def test_solve_part1(filename, expected):
-    value = solution.solve_part1(filename)
-    
+    value = solution.solve_part1(filename)    
     assert expected == value
 
 
