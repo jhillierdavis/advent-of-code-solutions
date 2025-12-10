d92deb63dd061a580a71f0bd8b26282ae5ebb2aa
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-12-10 09:31:52 +0000

    AOC 2025: Day 10: Solved part 1

--- aoc-2025/aoc-2025-day-10/solution-in-python3/solution.py
+++ aoc-2025/aoc-2025-day-10/solution-in-python3/solution.py
@@ -5,19 +5,126 @@ from helpers import fileutils
 import logging
 import logging.config
 
-# create logger
+# Create logger
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger(__name__)
 logger = logging.getLogger('simpleLogger')
 
-def solve_part1(filename):
-    logger.debug("TODO: Implement Part 1")
-    lines = fileutils.get_file_lines_from(filename)
 
-    return "TODO"
+def process_config(cfg):
+    vals = cfg.split()
+    indicators = vals[0]
+    logger.debug(f"indicators={indicators}")
+    wirings = vals[1:-1]
+    logger.debug(f"wirings={wirings}")
+    joltages = vals[-1]
+    logger.debug(f"joltages={joltages}")
+
+    return indicators, wirings, joltages
+
+
+def toggle_light(status):
+    if status == '.':
+        return '#'
+    if status == '#':
+        return '.'
+    raise Exception(f"Unrecognised status={status}!")
+
+
+def get_pressed_count(press_map):
+    total_count = 0
+    for v in press_map.values():
+        total_count += v
+    return total_count
+
+def get_unpressed_count(press_map):
+    total_count = 0
+    for v in press_map.values():
+        if v == 0:
+            total_count += 1
+    return total_count
+
+
+def press_buttons_at_most_once(button_press_idx, button_map, current_state, target_state, press_map, totals):
+    new_state = current_state.copy()
+    press_map = press_map.copy()
+
+    wiring = button_map[button_press_idx]
+    for wi in wiring:
+        status = current_state[wi]
+        new_state[wi] = toggle_light(status)
+
+    press_map[button_press_idx] += 1
+    #logger.debug(f"button_press_idx={button_press_idx} press_map={press_map} new_state={new_state} target_state={target_state}")
+
+    # Abort if already found solution with same or fewer presses
+    total_count = get_pressed_count(press_map)
+    if len(totals) > 0:
+        for t in totals:
+            if total_count >= t:
+                return
+
+    if new_state == target_state:        
+        logger.debug(f"Matched: total_count={total_count} press_map={press_map} new_state={new_state} target_state={target_state}")   
+        totals.add(total_count)
+        return # Matched
+    
+    for bk in button_map.keys():
+        if press_map[bk] == 0: # Unpressed
+            press_buttons_at_most_once(bk, button_map, new_state, target_state, press_map, totals)
+
+
+def calculate_fewest_button_presses(cfg):
+   
+    indicators, wirings, _ = process_config(cfg)
+
+
+    i_size = len(indicators) - 2
+    current_lights = list()
+
+    for _ in range(i_size):
+        current_lights.append('.')
+    logger.debug(f"current_lights={current_lights}")
+
+    target_lights = list()
+    for i in range(i_size):
+        target_lights.append(indicators[1+i])
+    logger.debug(f"target_lights={target_lights}")
+
+    button_map = dict()
+    press_map = dict()
+
+    for i, w in enumerate(wirings):
+        wl = list()
+        vals = w[1:-1].split(',')
+        for v in vals:
+            idx = int(v)
+            if idx < len(target_lights):
+                wl.append(idx)
+        button_map[i] = wl
+        press_map[i] = 0
+
+    logger.debug(f"button_map={button_map}")
+    logger.debug(f"press_map={press_map}")
+
+    totals = set()
+
+    for bk in button_map.keys():
+        press_buttons_at_most_once(bk, button_map, current_lights, target_lights, press_map, totals)
+
+    logger.debug(f"totals={totals}")
+    return min(totals)
+
+
+def solve_part1(filename):
+    lines = fileutils.get_file_lines_from(filename)
+    ans = 0
+    for l in lines:
+        ans += calculate_fewest_button_presses(l)
+    return ans
+
 
 def solve_part2(filename):
     logger.debug("TODO: Implement Part 2")
     lines = fileutils.get_file_lines_from(filename)
-
     return "TODO"
--- aoc-2025/aoc-2025-day-10/solution-in-python3/test_solution.py
+++ aoc-2025/aoc-2025-day-10/solution-in-python3/test_solution.py
@@ -2,21 +2,35 @@ import pytest
 
 import solution
 
-input_example = "AOC-2025-Day-01_Puzzle-Input-Example.txt"
-input_full = "AOC-2025-Day-01_Puzzle-Input-Full.txt"
+input_example = "AOC-2025-Day-10_Puzzle-Input-Example.txt"
+input_full = "AOC-2025-Day-10_Puzzle-Input-Full.txt"
+
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        pytest.param("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}", 2),
+        pytest.param("[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}", 3),
+        pytest.param("[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}", 2),
+    ],    
+)
+def test_calculate_fewest_button_presses(input, expected):
+    value = solution.calculate_fewest_button_presses(input)    
+    assert expected == value
+
 
 
 #@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 7),
+        pytest.param(input_full, 473),
     ],    
 )
 def test_solve_part1(filename, expected):
-    value = solution.solve_part1(filename)
-    
+    value = solution.solve_part1(filename)    
     assert expected == value
 
 
@@ -29,6 +43,5 @@ def test_solve_part1(filename, expected):
     ],    
 )
 def test_solve_part2(filename, expected):
-    value = solution.solve_part2(filename)
-    
+    value = solution.solve_part2(filename)    
     assert expected == value
