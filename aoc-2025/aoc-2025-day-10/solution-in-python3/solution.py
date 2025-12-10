32e535ef9bca5a4ab1b5852fa9a8c84fc3c09801
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-12-10 13:25:46 +0000

    AOC 2025: Day 10: Part 2: WIP: Example unit tests working, but slow

--- aoc-2025/aoc-2025-day-10/solution-in-python3/solution.py
+++ aoc-2025/aoc-2025-day-10/solution-in-python3/solution.py
@@ -74,10 +74,8 @@ def press_buttons_at_most_once(button_press_idx, button_map, current_state, targ
             press_buttons_at_most_once(bk, button_map, new_state, target_state, press_map, totals)
 
 
-def calculate_fewest_button_presses(cfg):
-   
+def calculate_fewest_button_presses_for_indicators(cfg):   
     indicators, wirings, _ = process_config(cfg)
-
 
     i_size = len(indicators) - 2
     current_lights = list()
@@ -120,11 +118,165 @@ def solve_part1(filename):
     lines = fileutils.get_file_lines_from(filename)
     ans = 0
     for l in lines:
-        ans += calculate_fewest_button_presses(l)
+        ans += calculate_fewest_button_presses_for_indicators(l)
     return ans
+
+def determine_new_joltage_state(button_press_idx, button_map, new_state):
+    wiring = button_map[button_press_idx]
+    for wi in wiring:
+        new_state[wi] += 1
+    return new_state
+
+
+def press_joltage_buttons(button_press_idx, button_map, current_state, target_state, press_map, totals):
+    new_state = current_state.copy()
+    press_map = press_map.copy()
+
+    """
+    wiring = button_map[button_press_idx]
+    for wi in wiring:
+        new_state[wi] += 1
+    """
+    #new_state = determine_new_joltage_state(button_press_idx, button_map, new_state)
+    #hashed_button_map = make_hashable(button_map)
+    #hashed_new_state = make_hashable(new_state)
+    #new_state = determine_new_joltage_state_cachable(button_press_idx, hashed_button_map, hashed_new_state)
+    new_state = determine_new_joltage_state(button_press_idx, button_map, new_state)
+
+    press_map[button_press_idx] += 1
+    #logger.debug(f"button_press_idx={button_press_idx} press_map={press_map} new_state={new_state} target_state={target_state}")
+
+    # Abort if already found solution with same or fewer presses
+    total_count = get_pressed_count(press_map)
+    if len(totals) > 0:
+        for t in totals:
+            if total_count >= t:
+                return
+
+    if new_state == target_state:        
+        logger.debug(f"Matched: total_count={total_count} press_map={press_map} new_state={new_state} target_state={target_state}")   
+        totals.add(total_count)
+        return # Matched
+    
+    for i, nv in enumerate(new_state):
+        if nv > target_state[i]:
+            return
+    
+    for bk in button_map.keys():
+        press_joltage_buttons(bk, button_map, new_state, target_state, press_map, totals)
+
+    # Clean-up
+    press_map = None
+    target_state = None
+
+
+def calculate_fewest_button_presses_for_joltages(cfg):
+    _, wirings, joltages = process_config(cfg)
+    
+    current_joltages = list()
+    logger.debug(f"current_joltages={current_joltages}")
+
+    target_joltages = [int(j) for j in joltages[1:-1].split(',')]
+    logger.debug(f"target_joltages={target_joltages}")
+
+    current_joltages = [0 for _ in target_joltages]
+    logger.debug(f"current_joltages={current_joltages}")
+
+    button_map = dict()
+    press_map = dict()
+
+    for i, w in enumerate(wirings):
+        wl = list()
+        vals = w[1:-1].split(',')
+        for v in vals:
+            idx = int(v)
+            if idx < len(target_joltages):
+                wl.append(idx)
+        button_map[i] = wl
+        press_map[i] = 0
+
+    logger.debug(f"button_map={button_map}")
+    logger.debug(f"press_map={press_map}")
+
+    """
+    totals = set()
+
+    for bk in button_map.keys():
+        press_joltage_buttons(bk, button_map, current_joltages, target_joltages, press_map, totals)
+    """
+
+    totals = bfs(button_map, target_joltages, current_joltages, press_map)
+    logger.debug(f"totals={totals}")
+
+
+    return min(totals)
 
 
 def solve_part2(filename):
     logger.debug("TODO: Implement Part 2")
+
     lines = fileutils.get_file_lines_from(filename)
-    return "TODO"
+    ans = 0
+    for i, l in enumerate(lines):
+        if i > 3:
+            break
+
+        ans += calculate_fewest_button_presses_for_joltages(l)
+    return ans
+
+
+from collections import deque
+
+def process(button_press_idx, button_map, current_state, target_state, press_map, totals):
+    new_state = current_state.copy()
+    press_map = press_map.copy()
+
+    new_state = determine_new_joltage_state(button_press_idx, button_map, new_state)
+
+    press_map[button_press_idx] += 1
+
+    # Abort if already found solution with same or fewer presses
+    total_count = get_pressed_count(press_map)
+    if len(totals) > 0:
+        for t in totals:
+            if total_count >= t:
+                return None, None
+
+    if new_state == target_state:        
+        logger.debug(f"Matched: total_count={total_count} press_map={press_map} new_state={new_state} target_state={target_state}")   
+        totals.add(total_count)
+        return None, None
+    
+    # Exceeded target
+    for i, nv in enumerate(new_state):
+        if nv > target_state[i]:
+            return None, None
+    
+    #for bk in button_map.keys():
+    #    press_joltage_buttons(bk, button_map, new_state, target_state, press_map, totals)
+
+    return new_state, press_map
+
+
+def bfs(button_map, target_joltages, current_joltages, press_map):
+    totals = set()
+
+    # Step 1: Initialize queue and visited set
+    queue = deque()
+    for bk in button_map.keys():
+        node = (bk, current_joltages, press_map)
+        queue.append(node)
+
+    # Step 2: Loop until queue is empty
+    while queue:
+        # Step 3: Dequeue a node
+        node = queue.pop()    
+        button_press_idx, current_joltages, press_map = node
+        
+        current_joltages, press_map = process(button_press_idx, button_map, current_joltages, target_joltages, press_map, totals)
+        if current_joltages:
+            for bk in button_map.keys():
+                node = (bk, current_joltages, press_map)
+                queue.append(node)
+            
+    return totals
--- aoc-2025/aoc-2025-day-10/solution-in-python3/test_solution.py
+++ aoc-2025/aoc-2025-day-10/solution-in-python3/test_solution.py
@@ -6,7 +6,6 @@ input_example = "AOC-2025-Day-10_Puzzle-Input-Example.txt"
 input_full = "AOC-2025-Day-10_Puzzle-Input-Full.txt"
 
 
-
 @pytest.mark.parametrize(
     "input, expected",
     [
@@ -15,10 +14,9 @@ input_full = "AOC-2025-Day-10_Puzzle-Input-Full.txt"
         pytest.param("[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}", 2),
     ],    
 )
-def test_calculate_fewest_button_presses(input, expected):
-    value = solution.calculate_fewest_button_presses(input)    
+def test_calculate_fewest_button_presses_for_indicators(input, expected):
+    value = solution.calculate_fewest_button_presses_for_indicators(input)    
     assert expected == value
-
 
 
 #@pytest.mark.skip(reason="TODO: Ignore until implemented")
@@ -26,20 +24,35 @@ def test_calculate_fewest_button_presses(input, expected):
     "filename, expected",
     [
         pytest.param(input_example, 7),
-        pytest.param(input_full, 473),
+        #pytest.param(input_full, 473), # A bit slow (~ 3 mins) -> TODO: Optimise / improve
     ],    
 )
 def test_solve_part1(filename, expected):
     value = solution.solve_part1(filename)    
     assert expected == value
 
+#@pytest.mark.skip(reason="Covered by 'test solve_part2'")
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        # From instructions / example data set
+        pytest.param("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}", 10),
+        pytest.param("[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}", 12),
+        pytest.param("[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}", 11),
+        # From full data set
+        #pytest.param("[##..#.] (2) (4,5) (3,5) (0,5) (1,5) (1,2,4,5) {4,135,127,8,135,155}", -1),
+    ],    
+)
+def test_calculate_fewest_button_presses_for_joltages(input, expected):
+    value = solution.calculate_fewest_button_presses_for_joltages(input)    
+    assert expected == value
 
 @pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 33),
+        pytest.param(input_full, -1),
     ],    
 )
 def test_solve_part2(filename, expected):
