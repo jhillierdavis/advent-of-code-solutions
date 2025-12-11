3f47b5d7e5e05ffd5840e33823bf9ab9a7bde26f
jhillierdavis <jhillierdavis@users.noreply.github.com>
2025-12-11 21:44:41 +0000

    AOC 2025: Day 11: Solved parts 1 & 2

--- aoc-2025/aoc-2025-day-11/solution-in-python3/solution.py
+++ aoc-2025/aoc-2025-day-11/solution-in-python3/solution.py
@@ -5,19 +5,107 @@ from helpers import fileutils
 import logging
 import logging.config
 
-# create logger
+# Create logger
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger(__name__)
 logger = logging.getLogger('simpleLogger')
 
-def solve_part1(filename):
-    logger.debug("TODO: Implement Part 1")
+
+def find_paths(graph:dict, start:str, stop:str, current_path:list=list()) -> list[list[str]]:
+    # Local path copy (for cycles detection)
+    path = current_path.copy()
+    path.append(start)    
+
+    if start == stop: # Found path (from 'start') to target ('stop')
+        return [path] # List of lists
+
+    if start not in graph: # Childless
+        return list() # Empty path
+
+    child_paths = list()
+    for child in graph[start]:
+        
+        if child in path: # Avoid cycles
+            continue
+
+        new_paths = find_paths(graph, child, stop, path)
+        for p in new_paths:
+            child_paths.append(p)
+
+    return child_paths
+
+
+def get_path_map_from_lines(lines:str) -> dict:
+    path_map = dict()
+    for l in lines:
+        vals = l.split()
+        path_map[vals[0][:-1]] = vals[1:]
+
+    #logger.debug(f"path_map={path_map}")
+    return path_map
+
+
+def find_path_count(path_map:dict, start:str, stop:str) -> int:
+    from functools import cache
+
+    # Cannot cache collections (dict, list etc.), so use inner function:
+
+    @cache
+    def cachable_find_path_count(start, stop):
+        if start == stop:
+            return 1
+        
+        if start not in path_map: # Childless
+            return 0   
+
+        count = 0
+        for child in path_map[start]:  
+            count += cachable_find_path_count(child, stop)
+
+        return count
+            
+    return cachable_find_path_count(start, stop)
+
+
+def get_path_map_from_from_file(filename:str) -> dict:
     lines = fileutils.get_file_lines_from(filename)
 
-    return "TODO"
+    path_map = get_path_map_from_lines(lines)
+    #logger.debug(f"path_map={path_map}")
+    return path_map
 
-def solve_part2(filename):
-    logger.debug("TODO: Implement Part 2")
-    lines = fileutils.get_file_lines_from(filename)
 
-    return "TODO"
+def solve_part1(filename:str) -> int:
+    path_map = get_path_map_from_from_file(filename)
+
+    start, stop = 'you', 'out'
+
+    paths = find_paths(path_map, start, stop)
+
+    number_of_paths = len(paths)
+
+    return number_of_paths
+
+
+def solve_part2(filename:str) -> int:
+    path_map = get_path_map_from_from_file(filename)
+
+    svr_to_dac = find_path_count(path_map, 'svr', 'dac')
+    dac_to_fft = find_path_count(path_map, 'dac', 'fft')
+    fft_to_out = find_path_count(path_map, 'fft', 'out')
+
+    svr_to_dac_to_fft_to_out = svr_to_dac * dac_to_fft * fft_to_out
+
+    svr_to_fft = find_path_count(path_map, 'svr', 'fft')
+    fft_to_dac = find_path_count(path_map, 'fft', 'dac')
+    dac_to_out = find_path_count(path_map, 'dac', 'out')
+
+    svr_to_fft_to_dac_to_out = svr_to_fft * fft_to_dac * dac_to_out
+
+    return svr_to_dac_to_fft_to_out + svr_to_fft_to_dac_to_out
+
+
+def solve_part1_using_part2_approach(filename:str) -> int:
+    path_map = get_path_map_from_from_file(filename)
+
+    return find_path_count(path_map, 'you', 'out')
--- aoc-2025/aoc-2025-day-11/solution-in-python3/test_solution.py
+++ aoc-2025/aoc-2025-day-11/solution-in-python3/test_solution.py
@@ -10,25 +10,61 @@ input_full = "AOC-2025-Day-11_Puzzle-Input-Full.txt"
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example, 5),
+        pytest.param(input_full, 699),
     ],    
 )
 def test_solve_part1(filename, expected):
-    value = solution.solve_part1(filename)
-    
+    value = solution.solve_part1(filename)   
     assert expected == value
 
+    # Re-solve using part 2 approach (caching)
+    num = solution.solve_part1_using_part2_approach(filename)
+    assert expected == num
 
-@pytest.mark.skip(reason="TODO: Ignore until implemented")
+
+input_example_part_2 = "AOC-2025-Day-11_Puzzle-Input-Example-Part-2.txt"
+
+
+#@pytest.mark.skip(reason="TODO: Ignore until implemented")
 @pytest.mark.parametrize(
     "filename, expected",
     [
-        pytest.param(input_example, -1),
-        #pytest.param(input_full, -1),
+        pytest.param(input_example_part_2, 2),
+        pytest.param(input_full, 388893655378800), # not 791848918299150, not 32932516744905217
     ],    
 )
 def test_solve_part2(filename, expected):
-    value = solution.solve_part2(filename)
-    
+    value = solution.solve_part2(filename)    
     assert expected == value
+
+
+@pytest.mark.parametrize(
+    "filename, start, stop, expected",
+    [
+        pytest.param(input_example, 'you', 'out', 5),        
+        pytest.param(input_full, 'you', 'out', 699),
+        pytest.param(input_example_part_2, 'you', 'out', 0),
+        pytest.param(input_example_part_2, 'svr', 'out', 8),
+        pytest.param(input_example_part_2, 'svr', 'dac', 2),
+        pytest.param(input_example_part_2, 'svr', 'fft', 1),
+        pytest.param(input_example_part_2, 'fft', 'dac', 1),
+        pytest.param(input_example_part_2, 'dac', 'fft', 0),
+        pytest.param(input_example_part_2, 'dac', 'out', 2),
+        pytest.param(input_example_part_2, 'fft', 'out', 4),
+        pytest.param(input_full, 'svr', 'out', 269728341652862491),
+        pytest.param(input_full, 'svr', 'dac', 3041933305555),
+        pytest.param(input_full, 'svr', 'fft', 4275),
+        pytest.param(input_full, 'fft', 'dac', 17109136),
+        pytest.param(input_full, 'dac', 'fft', 0),
+        pytest.param(input_full, 'dac', 'out', 5317),
+        pytest.param(input_full, 'fft', 'out', 1517071963739),
+    ],    
+)
+def test_find_path_count(filename, start, stop, expected):
+    # Given: a graph
+    path_map = solution.get_path_map_from_from_file(filename)
+
+    # Then: count distinct paths from start to stop
+    num = solution.find_path_count(path_map, start, stop)    
+    assert expected == num
